f13bde311e7d2e375ae56fcb5b44fc6f62e523c3
Chris Bue Hansen <chrisbuehansen@gmail.com>
2022-03-14 12:31:05 +0100

    Added HelloWorldController and a more fitting gitignore

---
+++ services/API/API/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataSync.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Pasta")]
+        public string GetPasta()
+        {
+            return "Pasta";
+        }
+
+        [HttpGet]
+        [Route("Fries")]
+        public string GetFries()
+        {
+            return "Fries";
+        }
+    }
+}
