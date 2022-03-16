f2fd6ef7947cebd757f33fdc516f592a32b7b86f
Chris Bue Hansen <chrisbuehansen@gmail.com>
2022-03-16 13:28:55 +0100

    HealthController added

---
+++ services/API/API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetStatus()
+        {
+            return StatusCode(200);
+        }
+    }
+}
