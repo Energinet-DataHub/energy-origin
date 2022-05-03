03891a65232df98c94b522442e28a29bc63e9aca
Claus <claus.christiansen1987@gmail.com>
2022-05-03 14:02:32 +0200

    Remove old TODO

--- domains/emissions/Emissions.API/API/Models/Emissions.cs
+++ domains/emissions/Emissions.API/API/Models/Emissions.cs
@@ -2,7 +2,6 @@
 {
     public class Emissions
     {
-        //TODO Should this be a string? The docs say the response is a string, but can we expect it to be a number?
         public string GSRN { get; set; }
     }
 }
