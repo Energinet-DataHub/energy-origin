47ef26274611b42166c842fef09e55025c2908d6
Claus <claus.christiansen1987@gmail.com>
2022-05-03 13:36:06 +0200

    Add Emissions to domain.

---
+++ domains/emssions/Emissions.API/API/Models/Emissions.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models
+{
+    public class Emissions
+    {
+        //TODO Should this be a string? The docs say the response is a string, but can we expect it to be a number?
+        public string GSRN { get; set; }
+    }
+}
---
+++ domains/emssions/Emissions.API/Readme.md
@@ -0,0 +1,2 @@
+# eo-emissions
+This repository contains the codebase for the Emissions domain which is a part of [Energy Origin](https://github.com/Energinet-DataHub/energy-origin).
