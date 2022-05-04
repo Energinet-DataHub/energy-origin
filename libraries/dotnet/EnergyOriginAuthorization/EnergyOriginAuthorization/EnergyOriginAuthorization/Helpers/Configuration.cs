4ec0442938e2329da6fca7dda2e7750b3bb27ee2
Claus <claus.christiansen1987@gmail.com>
2022-05-04 15:07:18 +0200

    Add library to domain

---
+++ libraries/dotnet/EnergyOriginAuthorization/EnergyOriginAuthorization/EnergyOriginAuthorization/Helpers/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace EnergyOriginAuthorization.Helpers
+{
+    public static class Configuration
+    {
+        private const string DotNetEnvironment = "ASPNETCORE_ENVIRONMENT";
+
+        public static bool IsDevelopment()
+        {
+            return "Development".Equals(Environment.GetEnvironmentVariable(DotNetEnvironment), StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
