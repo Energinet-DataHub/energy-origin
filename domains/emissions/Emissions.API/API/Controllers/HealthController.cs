8e6b62684c16cf82b043cc94bc2ad0f107b08269
Claus <claus.christiansen1987@gmail.com>
2022-05-09 09:12:31 +0200

    Change health back with new env variable

--- domains/emissions/Emissions.API/API/Controllers/HealthController.cs
+++ domains/emissions/Emissions.API/API/Controllers/HealthController.cs
@@ -1,4 +1,5 @@
-﻿using EnergyOriginAuthorization;
+﻿using API.Helpers;
+using EnergyOriginAuthorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
@@ -7,26 +8,21 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    [Authorization.Authorize]
-    public class HealthController : ControllerBase
+    public class HealthController : Controller
     {
-        private readonly ILogger<HealthController> _logger;
-        private readonly HealthCheckService _healthCheckService;
-        public HealthController(ILogger<HealthController> logger,
-            HealthCheckService healthCheckService)
-        {
-            _healthCheckService = healthCheckService;
-            _logger = logger;
-        }
-
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public IActionResult GetStatus()
         {
-            var report = await _healthCheckService.CheckHealthAsync();
-
-            return report.Status == HealthStatus.Healthy ? Ok(report) :
-                StatusCode((int)System.Net.HttpStatusCode.ServiceUnavailable, report);
+            try
+            {
+                Configuration.GetDataSyncEndpoint();
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
-
