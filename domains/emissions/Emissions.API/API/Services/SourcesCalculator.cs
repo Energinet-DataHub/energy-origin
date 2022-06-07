fcb1d04cdd5e7ce49528e3ef419c9f052453d0ba
Robert Rosborg <ror@systemate.dk>
2022-06-07 08:57:59 +0200

    Calculate total share

--- domains/emissions/Emissions.API/API/Services/SourcesCalculator.cs
+++ domains/emissions/Emissions.API/API/Services/SourcesCalculator.cs
@@ -5,46 +5,68 @@ namespace API.Services
 {
     public class SourcesCalculator : ISourcesCalculator
     {
-        public EnergySourceResponse CalculateSourceEmissions(IEnumerable<TimeSeries> measurements, DeclarationProduction declaration,
+        public EnergySourceResponse CalculateSourceEmissions(IEnumerable<TimeSeries> timeSeries, DeclarationProduction declaration,
             long dateFrom, long dateTo, Aggregation aggregation)
         {
-            EnergySourceResponse result;
+            EnergySourceResponse result = new EnergySourceResponse(new List<EnergySourceDeclaration>());
 
             IEnumerable<IGrouping<string, Record>> groupedDeclarations = GetGroupedDeclarations(aggregation, declaration.Result.Records);
 
-            var consumptionResult = new Dictionary<string, List<ConsumptionShare>>();
-            foreach (var timeSeries in measurements)
+            var consumptionResult = new Dictionary<string, Dictionary<string, ConsumptionShare>>();
+            foreach (var meteringPointTimeSeries in timeSeries)
             {
-                foreach (var measurement in timeSeries.Measurements)
+                foreach (var measurement in meteringPointTimeSeries.Measurements)
                 {
                     var utcDateTime = GetDateAsString(measurement.DateFrom.ToUtcDateTime(), aggregation);
-                    var gridArea = timeSeries.MeteringPoint.GridArea;
-                    var totalShares = groupedDeclarations.First(_ =>
-                        _.Key == utcDateTime + gridArea
+                    var gridArea = meteringPointTimeSeries.MeteringPoint.GridArea;
+                    var key = utcDateTime + gridArea;
+                    var totalShares = groupedDeclarations.Single(_ =>
+                        _.Key == key
                     );
-                    consumptionResult.TryGetValue(utcDateTime + gridArea, out var shares);
+                    consumptionResult.TryGetValue(key, out var shares);
                     if (shares == null)
                     {
-                        shares = new List<ConsumptionShare>();
-                        consumptionResult.Add(utcDateTime + gridArea, shares);
+                        shares = new Dictionary<string,ConsumptionShare>();
+                        consumptionResult.Add(key, shares);
                     }
 
                     foreach (var totalShare in totalShares)
                     {
-                        shares.Add(new ConsumptionShare((float)totalShare.ShareTotal * measurement.Quantity, measurement.DateFrom, totalShare.ProductionType));
-
+                        if (!shares.TryGetValue(totalShare.ProductionType, out var share))
+                        {
+                            share = new ConsumptionShare {
+                                Value = 0, 
+                                Date = measurement.DateFrom, 
+                                ProductionType = totalShare.ProductionType};
+                            shares.Add(totalShare.ProductionType, share);
+                        }
+                        share.Value += (float)totalShare.ShareTotal * measurement.Quantity;
                     }
                 }
             }
 
+            IEnumerable<IGrouping<string, Measurement>> groupedMeasurements = GetGroupedMeasurements(aggregation, timeSeries);
 
             return result;
+        }
+
+        private IEnumerable<IGrouping<string, Measurement>> GetGroupedMeasurements(Aggregation aggregation, IEnumerable<TimeSeries> timeSeries)
+        {
+            if (aggregation == Aggregation.Total)
+            {
+                return timeSeries.SelectMany(_ => _.Measurements).GroupBy(x => "total");
+            }
+
+            return timeSeries.SelectMany(y => y.Measurements)
+                .GroupBy(x => GetDateAsString(x.DateFrom.ToUtcDateTime(), aggregation));
         }
 
         private IEnumerable<IGrouping<string, Record>> GetGroupedDeclarations(Aggregation aggregation, List<Record> declaration)
         {
             if (aggregation == Aggregation.Total)
-                return declaration.GroupBy(_ => "total");
+            {
+                return declaration.GroupBy(_ => "total" + _.PriceArea);
+            }
 
             return declaration.GroupBy(_ => GetDateAsString(_.HourUTC, aggregation) + _.PriceArea);
 
@@ -66,6 +88,8 @@ namespace API.Services
                     return date.ToString("yyyy/MM/dd/HH/mm");
                 case Aggregation.Actual:
                     return date.ToString("yyyy/MM/dd/HH");
+                case Aggregation.Total:
+                    return "total";
                 default:
                     throw new ArgumentOutOfRangeException(nameof(aggregation), aggregation, null);
             }
@@ -75,18 +99,12 @@ namespace API.Services
 
         class ConsumptionShare
         {
-            public ConsumptionShare(float value, long measurementTime, string productionType)
-            {
-                Value = value;
-                Date = measurementTime;
-                ProductionType = productionType;
-            }
+            public float Value { get; set; }
 
-            public float Value { get; }
+            public long Date { get; set; }
 
-            public long Date { get; }
+            public string ProductionType { get; set; }
 
-            public string ProductionType { get; }
         }
     }
 }
