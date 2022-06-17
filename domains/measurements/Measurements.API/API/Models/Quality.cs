bfb441bd951afac7cb39b88616ac03b71ec40d73
Claus <45032018+C-Christiansen@users.noreply.github.com>
2022-06-17 14:08:50 +0200

    Update quality

--- domains/measurements/Measurements.API/API/Models/Quality.cs
+++ domains/measurements/Measurements.API/API/Models/Quality.cs
@@ -7,5 +7,9 @@ public enum Quality
     [Description("Measured")]
     Measured = 1,
     [Description("Estimated")]
-    Estimated = 2
+    Estimated = 2,
+    [Description("Calculated")]
+    Calculated = 3,
+    [Description("Revised")]
+    Revised = 4,
 }
