b6b8ac3d143ae1b97fbee94881b5cf237cf7e0f3
Martin Schmidt <martin@scoding.dk>
2022-06-17 14:42:14 +0200

    fixes and formatting

--- domains/emissions/Emissions.API/API/Helpers/DateTimeUtil.cs
+++ domains/emissions/Emissions.API/API/Helpers/DateTimeUtil.cs
@@ -2,9 +2,9 @@ namespace API.Helpers;
 
 public static class DateTimeUtil
 {
-    public static DateTime ToUtcDateTime(this long unixDateTimeSeconds) => 
+    public static DateTime ToUtcDateTime(this long unixDateTimeSeconds) =>
         DateTimeOffset.FromUnixTimeSeconds(unixDateTimeSeconds).UtcDateTime;
 
-    public static long ToUnixTime(this DateTime dateTime) => 
+    public static long ToUnixTime(this DateTime dateTime) =>
         ((DateTimeOffset)DateTime.SpecifyKind(dateTime, DateTimeKind.Utc)).ToUnixTimeSeconds();
 }
