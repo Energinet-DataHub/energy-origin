df9ecbaeb4e71958d1955def09b7be6f2ec4578b
Paul Toft Duizer <paul@duizer.dk>
2022-07-12 09:23:02 +0200

    Simplify program

--- domains/mock-oidc/Mock.Oidc/Mock/Program.cs
+++ domains/mock-oidc/Mock.Oidc/Mock/Program.cs
@@ -15,22 +15,9 @@ builder.Services.AddSingleton(_ =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.MapRazorPages();
 app.MapControllers();
 
