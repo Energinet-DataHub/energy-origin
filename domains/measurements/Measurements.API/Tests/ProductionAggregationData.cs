0b57c0eab096b16dc9a3bb2bca7a0f83f25324f5
Danni Petersen <dannip1123@outlook.com>
2022-07-14 12:52:29 +0200

    gg

--- domains/measurements/Measurements.API/Tests/ProductionAggregationData.cs
+++ domains/measurements/Measurements.API/Tests/ProductionAggregationData.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 
 namespace Tests;
+
 internal class ProductionAggregationData : MeasurementAggregationDataSetFactory
 {
     public List<TimeSeries> CreateTimeSeries()
     {
         return new List<TimeSeries>
-    {
-        new TimeSeries(
-            new MeteringPoint(
-                gsrn: "571313121223234323",
-                gridArea: "DK1",
-                type: MeterType.Production),
-            CreateMeasurements()
-        )
-    };
+        {
+            new TimeSeries(
+                new MeteringPoint(
+                    gsrn: "571313121223234323",
+                    gridArea: "DK1",
+                    type: MeterType.Production),
+                CreateMeasurements()
+            )
+        };
     }
 }
 
