7794931ebbf378047626bc73a00fd549cce4220b
Danni Petersen <47751775+Exobitt@users.noreply.github.com>
2022-07-14 13:09:27 +0200

    Add production to measurements (#86)

---
+++ domains/measurements/Measurements.API/Tests/ProductionAggregationData.cs
@@ -0,0 +1,22 @@
+﻿using API.Models;
+using System.Collections.Generic;
+
+namespace Tests;
+
+internal class ProductionAggregationData : MeasurementAggregationDataSetFactory
+{
+    public List<TimeSeries> CreateTimeSeries()
+    {
+        return new List<TimeSeries>
+        {
+            new TimeSeries(
+                new MeteringPoint(
+                    gsrn: "571313121223234323",
+                    gridArea: "DK1",
+                    type: MeterType.Production),
+                CreateMeasurements()
+            )
+        };
+    }
+}
+
