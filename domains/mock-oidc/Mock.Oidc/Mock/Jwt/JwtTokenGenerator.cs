b700697c3e6c0e6e074388bb34b7593b8aa1e474
Paul Toft Duizer <paul@duizer.dk>
2022-07-15 15:44:08 +0200

    revert keyid

--- domains/mock-oidc/Mock.Oidc/Mock/Jwt/JwtTokenGenerator.cs
+++ domains/mock-oidc/Mock.Oidc/Mock/Jwt/JwtTokenGenerator.cs
@@ -6,11 +6,10 @@ public class JwtTokenGenerator : IJwtTokenGenerator
 {
     private readonly Jwk _jwk;
 
-
     public JwtTokenGenerator()
     {
         byte[] secretKey = { 71, 169, 0, 146, 29, 249, 85, 175, 83, 69, 213, 13, 6, 171, 190, 35, 10, 94, 255, 61, 57, 82, 91, 182, 100, 167, 217, 111, 54, 75, 193, 160 };
-        _jwk = new Jwk(secretKey) { KeyId = "irrelevantKeyId" };
+        _jwk = new Jwk(secretKey);
     }
 
     public string Generate(Dictionary<string, object> claims) => JWT.Encode(claims, _jwk, JwsAlgorithm.HS256);
