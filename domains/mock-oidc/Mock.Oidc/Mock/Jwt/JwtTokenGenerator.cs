6b64eb6b4c19a52b7094603fdb07bc6b55e86a03
Paul Toft Duizer <paul@duizer.dk>
2022-07-15 15:17:35 +0200

    Add keyid

--- domains/mock-oidc/Mock.Oidc/Mock/Jwt/JwtTokenGenerator.cs
+++ domains/mock-oidc/Mock.Oidc/Mock/Jwt/JwtTokenGenerator.cs
@@ -10,7 +10,7 @@ public class JwtTokenGenerator : IJwtTokenGenerator
     public JwtTokenGenerator()
     {
         byte[] secretKey = { 71, 169, 0, 146, 29, 249, 85, 175, 83, 69, 213, 13, 6, 171, 190, 35, 10, 94, 255, 61, 57, 82, 91, 182, 100, 167, 217, 111, 54, 75, 193, 160 };
-        _jwk = new Jwk(secretKey);
+        _jwk = new Jwk(secretKey) { KeyId = "irrelevantKeyId" };
     }
 
     public string Generate(Dictionary<string, object> claims) => JWT.Encode(claims, _jwk, JwsAlgorithm.HS256);
