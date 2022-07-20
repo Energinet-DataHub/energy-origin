b2aff34bc6f55f50094f31cbd20229f5b1984f06
Paul Toft Duizer <paul@duizer.dk>
2022-07-20 10:21:11 +0200

    Serialization

--- libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Serialization/EventModel.cs
+++ libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Serialization/EventModel.cs
@@ -8,25 +8,13 @@ public abstract class EventModel
     private EventModelVersionAttribute attribute;
 
     [JsonIgnore]
-    public string Type
-    {
-        get
-        {
-            return this.attribute.Type;
-        }
-    }
+    public string Type => this.attribute.Type;
 
     [JsonIgnore]
-    public int Version
-    {
-        get
-        {
-            return this.attribute.Version;
-        }
-    }
+    public int Version => this.attribute.Version;
 
     [JsonIgnore]
-    public string Data { get { return JsonConvert.SerializeObject(this); } }
+    public string Data => JsonConvert.SerializeObject(this);
 
     public EventModel()
     {
