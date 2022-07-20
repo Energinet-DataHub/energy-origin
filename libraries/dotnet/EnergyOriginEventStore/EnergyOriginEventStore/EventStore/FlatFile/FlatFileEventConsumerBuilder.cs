6bd199cc228111deacd3a1714b2355f03120b5cc
Paul Toft Duizer <paul@duizer.dk>
2022-07-20 10:05:12 +0200

    FlatFileEventConsumerBuilder

--- libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/FlatFile/FlatFileEventConsumerBuilder.cs
+++ libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/FlatFile/FlatFileEventConsumerBuilder.cs
@@ -5,25 +5,24 @@ namespace EventStore.FlatFile;
 
 internal class FlatFileEventConsumerBuilder : IEventConsumerBuilder
 {
+    private readonly Dictionary<Type, IEnumerable<Action<Event<EventModel>>>> _handlers = new();
 
-    private Dictionary<Type, IEnumerable<Action<Event<EventModel>>>> handlers = new Dictionary<Type, IEnumerable<Action<Event<EventModel>>>>();
-
-    private FlatFileEventStore fileStore;
-    private string topicPrefix;
-    private string? pointer = null;
+    private readonly FlatFileEventStore _fileStore;
+    private readonly string _topicPrefix;
+    private string? _pointer;
 
     public FlatFileEventConsumerBuilder(FlatFileEventStore fileStore, string topicPrefix)
     {
-        this.fileStore = fileStore;
-        this.topicPrefix = topicPrefix;
+        _fileStore = fileStore;
+        _topicPrefix = topicPrefix;
     }
 
     public IEventConsumer Build()
     {
         var unpacker = new Unpacker();
-        var consumer = new FlatFileEventConsumer(unpacker, handlers, fileStore, topicPrefix, pointer);
+        var consumer = new FlatFileEventConsumer(unpacker, _handlers, _fileStore, _topicPrefix, _pointer);
 
-        fileStore.disposeEvent += consumer.Dispose;
+        _fileStore.disposeEvent += consumer.Dispose;
 
         return consumer;
     }
@@ -32,21 +31,17 @@ internal class FlatFileEventConsumerBuilder : IEventConsumerBuilder
     {
         Type t = typeof(T);
 
-        var list = handlers.GetValueOrDefault(t);
-        if (list is null)
-        {
-            list = new List<Action<Event<EventModel>>>();
-        }
+        var list = _handlers.GetValueOrDefault(t) ?? new List<Action<Event<EventModel>>>();
 
         Action<Event<EventModel>> casted_handler = (e) => handler(new Event<T>((T)e.EventModel, e.Pointer));
-        handlers[t] = list.Append(casted_handler);
+        _handlers[t] = list.Append(casted_handler);
 
         return this;
     }
 
     public IEventConsumerBuilder ContinueFrom(string pointer)
     {
-        this.pointer = pointer;
+        _pointer = pointer;
         return this;
     }
 }
