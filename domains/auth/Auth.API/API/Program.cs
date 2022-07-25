e48e04d8f086e1a2b7262d012382566ef34dec3f
Danni Petersen <dannip1123@outlook.com>
2022-07-25 16:43:31 +0200

    logging

--- domains/auth/Auth.API/API/Program.cs
+++ domains/auth/Auth.API/API/Program.cs
@@ -1,8 +1,29 @@
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Serilog;
+using Serilog.Formatting.Json;
+
+
+var logger = new LoggerConfiguration()
+    .WriteTo.Console(new JsonFormatter())
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
 
 var app = builder.Build();
 
