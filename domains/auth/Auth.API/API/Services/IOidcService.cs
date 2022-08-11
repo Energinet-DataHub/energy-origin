a2fefe707885f515f0bca2bbbbbd04ea39217630
Exobitt <dannip1123@outlook.com>
2022-08-11 14:38:52 +0200

    gg

--- domains/auth/Auth.API/API/Services/IOidcService.cs
+++ domains/auth/Auth.API/API/Services/IOidcService.cs
@@ -4,7 +4,5 @@ using Microsoft.AspNetCore.Http.Extensions;
 namespace API.Services;
 public interface IOidcService
 {
-    string EncodeJwtToken(AuthState state);
-
     QueryBuilder CreateAuthorizationRedirectUrl(string responseType, AuthState state, string lang);
 }
