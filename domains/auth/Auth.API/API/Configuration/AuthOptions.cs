8df0aa9da4cbb1633beb0f6fe7eef2e20d745d84
Paul Toft Duizer <paul@duizer.dk>
2022-08-17 13:21:55 +0200

    Use options pattern for auth domain (#112)
    Co-authored-by: Danni Petersen <dannip1123@outlook.com>

---
+++ domains/auth/Auth.API/API/Configuration/AuthOptions.cs
@@ -0,0 +1,21 @@
+namespace API.Configuration;
+
+public class AuthOptions
+{
+    // Token related
+    public string InternalTokenSecret { get; set; }
+    public string TokenExpiryTime { get; set; }
+
+    // OIDC Related
+    public string Scope { get; set; }
+    public string AmrValues { get; set; }
+    public string OidcUrl { get; set; }
+    public string OidcClientId { get; set; }
+    public string OidcClientSecret { get; set; }
+
+    // Base url
+    public string BaseUrl { get; set; }
+
+    // Cryptography
+    public string SecretKey { get; set; }
+}
