32146ea89c584547ba47a5506e9d3019acd8a8b7
Paul Toft Duizer <paul@duizer.dk>
2022-08-18 16:11:29 +0200

    refactor test

--- domains/auth/Auth.API/Tests/TestRedirectLogin.cs
+++ domains/auth/Auth.API/Tests/TestRedirectLogin.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using API.Configuration;
 using API.Models;
 using API.Services;
 using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
 using Xunit.Categories;
@@ -10,75 +14,63 @@ namespace Tests;
 [UnitTest]
 public sealed class TestRedirectLogin
 {
-    private readonly Mock<IOidcProviders> _moqSignaturGruppen;
-    private readonly Mock<IOidcService> _moqService;
-
-    public TestRedirectLogin()
-    {
-        _moqSignaturGruppen = new Mock<IOidcProviders>();
-        _moqService = new Mock<IOidcService>();
-    }
-
     [Fact]
     public void Oidc_Redirect_success()
     {
         //Arrange
+        const string expectedNextUrl = "?response_type=code&client_id=OIDCCLIENTID&redirect_uri=http%3A%2F%2Ftest.energioprindelse.dk%2Fapi%2Fauth%2Foidc%2Flogin%2Fcallback&scope=SCOPE,%20SCOPE1&state=foo&language=en";
 
-        var state = new AuthState()
+        var state = new AuthState
         {
             FeUrl = "http://test.energioprindelse.dk",
             ReturnUrl = "https://demo.energioprindelse.dk/dashboard"
         };
 
-        var expectedNextUrl = "?response_type=code&client_id=OIDCCLIENTID&redirect_uri=dfsgdsf%2Fapi%2Fauth%2Foidc%2Flogin%2Fcallback&scope=SCOPE%2BSCOPE1&state=uebnksIeIK0VgZl%2F7ze9XTCpUiCuuLG%2BhcNGTGSWICA9vaxyRQXllRdI%2FXMsYDuOhzV1Tc2I9C1Ayydb2vB%2F7R1cUfMvhzZnyHXFTpNhUI84LZ73ntOUCXkdS%2FGciMO%2BBJ5U9wbDAWzuy8lbQnVVXlnRggEsNUb9Qpjk6Bu4AGk83yIZF8Xke23U4%2BUvKpf8JxCyiWSlWsu1Iw0ui79fuE1BVWhqfY%2F0NCA1t32fi8Q%3D&language=en";
+        var cryptographyServiceMock = new Mock<ICryptographyService>();
+        cryptographyServiceMock
+            .Setup(x => x.Encrypt(It.IsAny<string>()))
+            .Returns("foo");
+
+        var authOptionsMock = new Mock<IOptions<AuthOptions>>();
+        authOptionsMock.Setup(x => x.Value).Returns(new AuthOptions { OidcClientId = "OIDCCLIENTID", Scope = "SCOPE, SCOPE1" });
 
         //Act
-        //_moqService.SetReturnsDefault(expectedNextUrl);
-        _moqService.Setup(x => x.CreateAuthorizationRedirectUrl("code", state, "en"))
-            .Returns(new QueryBuilder {
-                { expectedNextUrl, "" }
-            });
-
-        var res = _moqService.Object.CreateAuthorizationRedirectUrl("code", state, "en");
-
+        var oidcService = new OidcService(new Mock<ILogger<OidcService>>().Object, cryptographyServiceMock.Object, authOptionsMock.Object);
+        var res = oidcService.CreateAuthorizationRedirectUrl("code", state, "en");
+        
         //Assert
-        Assert.NotNull(res);
-        Assert.NotEmpty(res);
-        Assert.IsType<QueryBuilder>(res);
-
-        // Somehow can't decode the html output the querybuilder creates
-        // Assert.Equal(expectedNextUrl, res.ToString());
+        Assert.Equal(expectedNextUrl, res.ToQueryString().Value);
+        
     }
 
     [Fact]
     public void SignaturGruppen_Redirect_success()
     {
         //Arrange
+        const string expectedNextUrl = "?foo=42&idp_params=%7B%22nemid%22%3A%7B%22amr_values%22%3A%22AMRVALUES%22%7D%7D";
 
-        var feUrl = "http://test.energioprindelse.dk";
-        var returnUrl = "https://demo.energioprindelse.dk/dashboard";
-
-        var state = new AuthState()
+        var state = new AuthState
         {
-            FeUrl = feUrl,
-            ReturnUrl = returnUrl
+            FeUrl = "http://test.energioprindelse.dk",
+            ReturnUrl = "https://demo.energioprindelse.dk/dashboard"
         };
 
-        var expectedNextUrl = "?response_type=code&client_id=OIDCCLIENTID&redirect_uri=http%3A%2F%2Ftest.energioprindelse.dk%2Fapi%2Fauth%2Foidc%2Flogin%2Fcallback&scope=SCOPE%2BSCOPE1&state=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmZV91cmwiOiJodHRwOi8vdGVzdC5lbmVyZ2lvcHJpbmRlbHNlLmRrIiwicmV0dXJuX3VybCI6Imh0dHBzOi8vZGVtby5lbmVyZ2lvcHJpbmRlbHNlLmRrL2Rhc2hib2FyZCIsInRlcm1zX2FjY2VwdGVkIjoiRmFsc2UiLCJ0ZXJtc192ZXJzaW9uIjoiMCIsImlkX3Rva2VuIjoiIiwidGluIjoiIiwiaWRlbnRpdHlfcHJvdmlkZXIiOiIiLCJleHRlcm5hbF9zdWJqZWN0IjoiIn0.0QgNo_mOGnthS5R4lYTONYC7xmOtywdldWrDXCma8uQ&language=en&idp_params=%7B%22nemid%22%3A%7B%22amr_values%22%3A%22AMRVALUES%22%7D%7D";
+        var parameters = new[] { new KeyValuePair<string, string>("foo", "42") };
 
-        _moqSignaturGruppen.Setup(x => x.CreateAuthorizationUri(state))
-            .Returns(new NextStep()
-            {
-                NextUrl = expectedNextUrl
-            });
+        var oidcServiceMock = new Mock<IOidcService>();
+        oidcServiceMock
+            .Setup(x => x.CreateAuthorizationRedirectUrl(It.IsAny<string>(), It.IsAny<AuthState>(), It.IsAny<string>()))
+            .Returns(new QueryBuilder(parameters));
+
+        var authOptionsMock = new Mock<IOptions<AuthOptions>>();
+        authOptionsMock.Setup(x => x.Value).Returns(new AuthOptions { AmrValues = "AMRVALUES" });
 
         //Act
-        var res = _moqSignaturGruppen.Object.CreateAuthorizationUri(state);
+        var signaturGruppen = new SignaturGruppen(new Mock<ILogger<SignaturGruppen>>().Object, oidcServiceMock.Object, authOptionsMock.Object);
+
+        var res = signaturGruppen.CreateAuthorizationUri(state);
 
         //Assert
-        Assert.NotNull(res.NextUrl);
-        Assert.NotEmpty(res.NextUrl);
-        Assert.IsType<NextStep>(res);
         Assert.Equal(expectedNextUrl, res.NextUrl);
     }
 }
