6217e3f6b0059b605833ce6d733716db9a70b7dc
Claus Christiansen <claus.christiansen1987@gmail.com>
2022-08-19 09:13:57 +0200

    Test done

---
+++ domains/auth/Auth.API/Tests/TestLogout.cs
@@ -0,0 +1,61 @@
+using API.Services;
+using API.Configuration;
+using Microsoft.Extensions.Options;
+using Xunit;
+using Xunit.Categories;
+using Moq;
+using API.Controllers;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace Tests;
+
+
+[UnitTest]
+public sealed class TestLogout
+{
+
+    [Fact]
+    public void Logout_success()
+    {
+        //Arrange
+        var logger = new Mock<ILogger<AuthController>>();
+        var odieProvider = new Mock<IOidcProviders>();
+
+        var authOptionsMock = new Mock<IOptions<AuthOptions>>();
+        authOptionsMock.Setup(x => x.Value).Returns(new AuthOptions
+        {
+            CookieName = "Authorization",
+            CookieDomain = "energioprindelse.dk",
+            CookieHttpOnly = "true",
+            CookieSameSite = "Strict",
+            CookieSecure = "true",
+            CookieCreateExpires = 6, 
+        });
+
+        var cookieService = new CookieService(authOptionsMock.Object);
+
+        var testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3N1ZWQiOiIyMDIyLTA1LTE4VDEzOjEzOjQxLjY2MTI2NSswMDowMCIsImV4cGlyZXMiOiIyMDIyLTA1LTE5VDEzOjEzOjQxLjY2MDk4NyswMDowMCIsImFjdG9yIjoiYWN0b3IiLCJzdWJqZWN0Ijoic3ViamVjdCIsInNjb3BlIjpbInNjb3BlMSIsInNjb3BlMiJdfQ.W1C1xKiEYPDeuo1OfRFpm6L3j7YGQJTGmegIgLu2UIQ";
+
+        //Act
+        var cookieOptions = cookieService.CreateCookieOptions(authOptionsMock.Object.Value.CookieCreateExpires);
+        
+        var AuthController = new AuthController(logger.Object, odieProvider.Object, cookieService, authOptionsMock.Object) { ControllerContext = new ControllerContext() { HttpContext = new DefaultHttpContext() }};
+
+        var opaque_token = "Test_opaque_token";
+        AuthController.HttpContext.Response.Cookies.Append($"{authOptionsMock.Object.Value.CookieName}", $"{opaque_token}", cookieOptions);
+        AuthController.HttpContext.Request.Headers.Add(authOptionsMock.Object.Value.CookieName, "Bearer " + testToken);
+
+        var op = AuthController.HttpContext.Response.Headers.Values.Count;
+
+        AuthController.Logout();
+
+        var expectedCookie = "Authorization=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+
+        //Assert
+        Assert.NotNull(cookieOptions);
+        Assert.Equal(expectedCookie, AuthController.HttpContext.Response.Headers.Values.Single());
+    }
+}
