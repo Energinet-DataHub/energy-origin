739392e5d1017d68e82d7c2a62b309828e96b5bc
Jakob Jensen <jakobj@jakobj.dk>
2022-08-26 11:08:54 +0200

    A working test

--- libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Database/DatabaseEventConsumer.cs
+++ libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Database/DatabaseEventConsumer.cs
@@ -9,9 +9,9 @@ internal class DatabaseEventConsumer : IEventConsumer
     private readonly IUnpacker _unpacker;
     private readonly Dictionary<Type, IEnumerable<Action<Event<EventModel>>>> _handlers;
     private readonly Action<string, Exception>? _exceptionHandler;
-    private readonly string? _pointer;
     private readonly string _topicPrefix;
     private readonly PeriodicTimer timer;
+    private string? _pointer;
 
     public DatabaseEventConsumer(IUnpacker unpacker, Dictionary<Type, IEnumerable<Action<Event<EventModel>>>> handlers, Action<string, Exception>? exceptionHandler, DatabaseEventContext context, string topicPrefix, string? pointer)
     {
@@ -23,50 +23,56 @@ internal class DatabaseEventConsumer : IEventConsumer
         _exceptionHandler = exceptionHandler;
 
         timer = new PeriodicTimer(TimeSpan.FromMilliseconds(100));
-    }
 
-    internal async Task Begin()
-    {
-        while (await timer.WaitForNextTickAsync())
+        Task.Run(async () =>
         {
-            var query = _context.Messages.OrderBy(it => it.Id);
-            if (_pointer != null)
+            while (await timer.WaitForNextTickAsync())
             {
-                var pointer = Int64.Parse(_pointer ?? "");
-                query.Where(it => it.Id > pointer);
-            }
-            query.Where(it => it.Topic.StartsWith(_topicPrefix));
-
-            foreach (var message in query)
-            {
-                if (message == null) continue;
-
-                var reconstructedEvent = _unpacker.UnpackEvent(message.Payload);
-                var reconstructed = _unpacker.UnpackModel(reconstructedEvent);
-
-                var type = reconstructed.GetType();
-                var typeString = type.ToString();
-
-                var handlers = _handlers.GetValueOrDefault(type);
-                if (handlers == null)
+                IQueryable<Message> query = _context.Messages.OrderBy(it => it.Id);
+                if (_pointer != null)
                 {
-                    _exceptionHandler?.Invoke(typeString, new NotImplementedException($"No handler for event of type {type.ToString()}"));
+                    var pointer = Int64.Parse(_pointer ?? "");
+                    query = query.Where(it => it.Id > pointer);
                 }
+                query = query.Where(it => it.Topic.StartsWith(_topicPrefix));
 
-                (handlers ?? Enumerable.Empty<Action<Event<EventModel>>>()).AsParallel().ForAll(x =>
+                foreach (var message in query)
                 {
-                    try
+                    if (message == null) continue;
+
+                    var reconstructedEvent = _unpacker.UnpackEvent(message.Payload);
+                    var reconstructed = _unpacker.UnpackModel(reconstructedEvent);
+
+                    var type = reconstructed.GetType();
+                    var typeString = type.ToString();
+
+                    var handlers = _handlers.GetValueOrDefault(type);
+                    if (handlers == null)
                     {
-                        x.Invoke(new Event<EventModel>(reconstructed, $"{message.Id}"));
+                        _exceptionHandler?.Invoke(typeString, new NotImplementedException($"No handler for event of type {type.ToString()}"));
                     }
-                    catch (Exception exception)
+
+                    var pointer = $"{message.Id}";
+                    _pointer = pointer;
+
+                    (handlers ?? Enumerable.Empty<Action<Event<EventModel>>>()).AsParallel().ForAll(x =>
                     {
-                        _exceptionHandler?.Invoke(typeString, exception);
-                    }
-                });
+                        try
+                        {
+                            x.Invoke(new Event<EventModel>(reconstructed, pointer));
+                        }
+                        catch (Exception exception)
+                        {
+                            _exceptionHandler?.Invoke(typeString, exception);
+                        }
+                    });
+                }
             }
-        }
+        });
     }
 
-    public void Dispose() { }
+    public void Dispose()
+    {
+        timer.Dispose();
+    }
 }
