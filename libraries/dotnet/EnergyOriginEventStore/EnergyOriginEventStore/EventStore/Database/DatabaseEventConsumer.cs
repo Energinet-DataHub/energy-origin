93b68330c909e732c2d9ec1a895a55831d5c5d2b
Jakob Jensen <jakobj@jakobj.dk>
2022-08-26 15:03:30 +0200

    work work

--- libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Database/DatabaseEventConsumer.cs
+++ libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Database/DatabaseEventConsumer.cs
@@ -12,6 +12,7 @@ internal class DatabaseEventConsumer : IEventConsumer
     private readonly string _topicPrefix;
     private readonly PeriodicTimer timer;
     private string? _pointer;
+    private SemaphoreSlim semaphore = new(0, 1);
 
     public DatabaseEventConsumer(IUnpacker unpacker, Dictionary<Type, IEnumerable<Action<Event<EventModel>>>> handlers, Action<string, Exception>? exceptionHandler, DatabaseEventContext context, string topicPrefix, string? pointer)
     {
@@ -26,53 +27,65 @@ internal class DatabaseEventConsumer : IEventConsumer
 
         Task.Run(async () =>
         {
-            while (await timer.WaitForNextTickAsync())
+            do
             {
-                IQueryable<Message> query = _context.Messages.OrderBy(it => it.Id);
-                if (_pointer != null)
-                {
-                    var pointer = Int64.Parse(_pointer ?? "");
-                    query = query.Where(it => it.Id > pointer);
-                }
-                query = query.Where(it => it.Topic.StartsWith(_topicPrefix));
-
-                foreach (var message in query)
-                {
-                    if (message == null) continue;
-
-                    var reconstructedEvent = _unpacker.UnpackEvent(message.Payload);
-                    var reconstructed = _unpacker.UnpackModel(reconstructedEvent);
-
-                    var type = reconstructed.GetType();
-                    var typeString = type.ToString();
-
-                    var handlers = _handlers.GetValueOrDefault(type);
-                    if (handlers == null)
-                    {
-                        _exceptionHandler?.Invoke(typeString, new NotImplementedException($"No handler for event of type {type.ToString()}"));
-                    }
-
-                    var pointer = $"{message.Id}";
-                    _pointer = pointer;
-
-                    (handlers ?? Enumerable.Empty<Action<Event<EventModel>>>()).AsParallel().ForAll(x =>
-                    {
-                        try
-                        {
-                            x.Invoke(new Event<EventModel>(reconstructed, pointer));
-                        }
-                        catch (Exception exception)
-                        {
-                            _exceptionHandler?.Invoke(typeString, exception);
-                        }
-                    });
-                }
-            }
+                await Work();
+            } while (await timer.WaitForNextTickAsync());
         });
     }
 
     public void Dispose()
     {
         timer.Dispose();
+    }
+
+    async Task Work() // FIXME: to be called on produce
+    {
+        await semaphore.WaitAsync();
+
+        IQueryable<Message> query = _context.Messages.OrderBy(it => it.Id);
+        if (_pointer != null)
+        {
+            var pointer = Int64.Parse(_pointer ?? "");
+            query = query.Where(it => it.Id > pointer);
+        }
+        query = query.Where(it => it.Topic.StartsWith(_topicPrefix));
+
+        foreach (var message in query)
+        {
+            if (message == null) continue;
+
+            var reconstructedEvent = _unpacker.UnpackEvent(message.Payload);
+            var reconstructed = _unpacker.UnpackModel(reconstructedEvent);
+
+            var type = reconstructed.GetType();
+            var typeString = type.ToString();
+
+            var handlers = _handlers.GetValueOrDefault(type);
+            if (handlers == null)
+            {
+                _exceptionHandler?.Invoke(typeString, new NotImplementedException($"No handler for event of type {type.ToString()}"));
+            }
+
+            var pointer = $"{message.Id}";
+            _pointer = pointer;
+
+            var ghasdj = new Event<EventModel>(reconstructed, pointer);
+            var items = (handlers ?? Enumerable.Empty<Action<Event<EventModel>>>());
+
+            await Task.WhenAll(items.Select(it => Task.Run(() =>
+            {
+                try
+                {
+                    it.Invoke(ghasdj);
+                }
+                catch (Exception exception)
+                {
+                    _exceptionHandler?.Invoke(typeString, exception);
+                }
+            })));
+
+            semaphore.Release();
+        }
     }
 }
