e47313671aa28f29d54908a678006fee979a892d
Martin Schmidt <martin@scoding.dk>
2022-08-31 09:04:02 +0000

    formatting

--- domains/emissions/Emissions.API/Tests/SourceEmissionShareDataSetFactory.cs
+++ domains/emissions/Emissions.API/Tests/SourceEmissionShareDataSetFactory.cs
@@ -11,29 +11,29 @@ internal static class SourceEmissionShareDataSetFactory
     {
         new Measurement(
             GSRN: MP,
-            DateFrom: new DateTime(2021, 1, 1, 22,0,0, DateTimeKind.Utc).ToUnixTime(),
-            DateTo: new DateTime(2021, 1, 1, 23,0,0, DateTimeKind.Utc).ToUnixTime(),
+            DateFrom: new DateTime(2021, 1, 1, 22, 0, 0, DateTimeKind.Utc).ToUnixTime(),
+            DateTo: new DateTime(2021, 1, 1, 23, 0, 0, DateTimeKind.Utc).ToUnixTime(),
             Quantity: 1000,
             Quality: Quality.Measured
             ),
         new Measurement(
             GSRN: MP,
-            DateFrom: new DateTime(2021, 1, 1, 23,0,0, DateTimeKind.Utc).ToUnixTime(),
-            DateTo: new DateTime(2021, 1, 2, 0,0, 0, DateTimeKind.Utc).ToUnixTime(),
+            DateFrom: new DateTime(2021, 1, 1, 23, 0, 0, DateTimeKind.Utc).ToUnixTime(),
+            DateTo: new DateTime(2021, 1, 2, 0, 0, 0, DateTimeKind.Utc).ToUnixTime(),
             Quantity: 2000,
             Quality: Quality.Measured
             ),
         new Measurement(
             GSRN: MP,
-            DateFrom: new DateTime(2021, 1, 2, 0,0,0, DateTimeKind.Utc).ToUnixTime(),
-            DateTo: new DateTime(2021, 1, 2, 1,0,0, DateTimeKind.Utc).ToUnixTime(),
+            DateFrom: new DateTime(2021, 1, 2, 0, 0, 0, DateTimeKind.Utc).ToUnixTime(),
+            DateTo: new DateTime(2021, 1, 2, 1, 0, 0, DateTimeKind.Utc).ToUnixTime(),
             Quantity: 3000,
             Quality: Quality.Measured
             ),
         new Measurement(
             GSRN: MP,
-            DateFrom: new DateTime(2021, 1, 2, 1,0,0, DateTimeKind.Utc).ToUnixTime(),
-            DateTo: new DateTime(2021, 1, 2, 2,0,0, DateTimeKind.Utc).ToUnixTime(),
+            DateFrom: new DateTime(2021, 1, 2, 1, 0, 0, DateTimeKind.Utc).ToUnixTime(),
+            DateTo: new DateTime(2021, 1, 2, 2, 0, 0, DateTimeKind.Utc).ToUnixTime(),
             Quantity: 4000,
             Quality: Quality.Measured
         )
@@ -61,17 +61,17 @@ internal static class SourceEmissionShareDataSetFactory
         new MixRecord(30, new DateTime(2021, 1, 1, 22, 0, 0, DateTimeKind.Utc), "Final", "DK1", "WindOnshore"),
         new MixRecord(20, new DateTime(2021, 1, 1, 22, 0, 0, DateTimeKind.Utc), "Final", "DK1", "BioGas"),
 
-        new MixRecord(40, new DateTime(2021, 1, 1, 23,0,0, DateTimeKind.Utc), "Final", "DK1", "Solar"),
-        new MixRecord(50, new DateTime(2021, 1, 1, 23,0,0, DateTimeKind.Utc), "Final", "DK1", "WindOnshore"),
-        new MixRecord(10, new DateTime(2021, 1, 1, 23,0,0, DateTimeKind.Utc), "Final", "DK1", "BioGas"),
+        new MixRecord(40, new DateTime(2021, 1, 1, 23, 0, 0, DateTimeKind.Utc), "Final", "DK1", "Solar"),
+        new MixRecord(50, new DateTime(2021, 1, 1, 23, 0, 0, DateTimeKind.Utc), "Final", "DK1", "WindOnshore"),
+        new MixRecord(10, new DateTime(2021, 1, 1, 23, 0, 0, DateTimeKind.Utc), "Final", "DK1", "BioGas"),
 
-        new MixRecord(30, new DateTime(2021, 1, 2, 0,0,0, DateTimeKind.Utc), "Final", "DK1", "Solar"),
-        new MixRecord(30, new DateTime(2021, 1, 2, 0,0,0, DateTimeKind.Utc), "Final", "DK1", "WindOnshore"),
-        new MixRecord(40, new DateTime(2021, 1, 2, 0,0,0, DateTimeKind.Utc), "Final", "DK1", "BioGas"),
+        new MixRecord(30, new DateTime(2021, 1, 2, 0, 0, 0, DateTimeKind.Utc), "Final", "DK1", "Solar"),
+        new MixRecord(30, new DateTime(2021, 1, 2, 0, 0, 0, DateTimeKind.Utc), "Final", "DK1", "WindOnshore"),
+        new MixRecord(40, new DateTime(2021, 1, 2, 0, 0, 0, DateTimeKind.Utc), "Final", "DK1", "BioGas"),
 
-        new MixRecord(20, new DateTime(2021, 1, 2, 1,0,0, DateTimeKind.Utc), "Final", "DK1", "Solar"),
-        new MixRecord(40, new DateTime(2021, 1, 2, 1,0,0, DateTimeKind.Utc), "Final", "DK1", "WindOnshore"),
-        new MixRecord(40, new DateTime(2021, 1, 2, 1,0,0, DateTimeKind.Utc), "Final", "DK1", "BioGas"),
+        new MixRecord(20, new DateTime(2021, 1, 2, 1, 0, 0, DateTimeKind.Utc), "Final", "DK1", "Solar"),
+        new MixRecord(40, new DateTime(2021, 1, 2, 1, 0, 0, DateTimeKind.Utc), "Final", "DK1", "WindOnshore"),
+        new MixRecord(40, new DateTime(2021, 1, 2, 1, 0, 0, DateTimeKind.Utc), "Final", "DK1", "BioGas"),
 
     };
 }
