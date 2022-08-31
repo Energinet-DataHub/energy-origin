03b55fa068a4fce2fe6c49e9e1829828847ddbc6
Christoffer Frydkjær <christofferfk@gmail.com>
2022-08-31 13:08:08 +0200

    add get or create

--- domains/auth/Auth.API/API/Repository/ITokenStorage.cs
+++ domains/auth/Auth.API/API/Repository/ITokenStorage.cs
@@ -4,6 +4,7 @@ namespace API.Repository;
 
 public interface ITokenStorage
 {
+    Task<InternalToken> GetOrCreateInternalToken(InternalToken internalToken);
     void DeleteByOpaqueToken(string token);
     string GetIdTokenByOpaqueToken(string token);
     InternalToken? GetInteralTokenByOpaqueToken(string token);
