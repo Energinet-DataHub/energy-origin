b623cf2b3baf61bbe1d4c04fe8b15eceec79d4d8
Jakob Jensen <jakobj@jakobj.dk>
2022-09-08 10:31:01 +0200

    Snyk clean up (#135)

--- libraries/dotnet/EnergyOriginAuthorization/EnergyOriginAuthorization/EnergyOriginAuthorization/AuthorizeAttribute.cs
+++ libraries/dotnet/EnergyOriginAuthorization/EnergyOriginAuthorization/EnergyOriginAuthorization/AuthorizeAttribute.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -7,23 +6,14 @@ namespace EnergyOriginAuthorization
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
     public class AuthorizeAttribute : Attribute, IAuthorizationFilter
     {
-        private IEnumerable<string> requiredScopes;
+        private readonly IEnumerable<string> requiredScopes;
         public AuthorizationContext? Context { get; private set; }
 
-        public AuthorizeAttribute()
-        {
-            requiredScopes = new List<string>();
-        }
+        public AuthorizeAttribute() => requiredScopes = new List<string>();
 
-        public AuthorizeAttribute(string requiredScope)
-        {
-            requiredScopes = new List<string>() { requiredScope };
-        }
+        public AuthorizeAttribute(string requiredScope) => requiredScopes = new List<string>() { requiredScope };
 
-        public AuthorizeAttribute(IEnumerable<string> requiredScopes)
-        {
-            this.requiredScopes = requiredScopes;
-        }
+        public AuthorizeAttribute(IEnumerable<string> requiredScopes) => this.requiredScopes = requiredScopes;
 
         public void OnAuthorization(AuthorizationFilterContext context)
         {
@@ -31,13 +21,10 @@ namespace EnergyOriginAuthorization
 
             if (!ValidateToken(token, requiredScopes))
             {
-                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+                context.Result = new UnauthorizedObjectResult(new { message = "Unauthorized" });
             }
         }
 
-        public bool ValidateToken(string token, IEnumerable<string> requiredScopes)
-        {
-            return AuthorizationContext.decode(token, requiredScopes) != null;
-        }
+        public static bool ValidateToken(string token, IEnumerable<string> requiredScopes) => AuthorizationContext.decode(token, requiredScopes) != null;
     }
 }
--- libraries/dotnet/EnergyOriginAuthorization/EnergyOriginAuthorization/EnergyOriginAuthorization/HttpClientExtension.cs
+++ libraries/dotnet/EnergyOriginAuthorization/EnergyOriginAuthorization/EnergyOriginAuthorization/HttpClientExtension.cs
@@ -4,9 +4,6 @@ namespace EnergyOriginAuthorization
 {
     public static class HttpClientExtension
     {
-        public static void AddAuthorizationToken(this HttpClient client, AuthorizationContext context)
-        {
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", context.Token);
-        }
+        public static void AddAuthorizationToken(this HttpClient client, AuthorizationContext context) => client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", context.Token);
     }
 }
