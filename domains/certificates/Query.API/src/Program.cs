aea0d6d29b4d22dca321dfb31e8f8e096e3f9327
Paul Toft Duizer <paul@duizer.dk>
2022-10-19 14:52:55 +0200

    description

--- domains/certificates/Query.API/src/Program.cs
+++ domains/certificates/Query.API/src/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(o => o.SupportNonNullableReferenceTypes());
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SupportNonNullableReferenceTypes();
+    o.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Certificates Query API"
+    });
+});
 
 var app = builder.Build();
 
