a6289bd3715e27b3a2ccd54f5bf3e5eefe85969b
Paul Toft Duizer <paul@duizer.dk>
2022-10-19 20:32:19 +0200

    health checks

--- domains/certificates/Query.API/src/Program.cs
+++ domains/certificates/Query.API/src/Program.cs
@@ -6,10 +6,8 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(o =>
 {
@@ -22,9 +20,12 @@ builder.Services.AddSwaggerGen(o =>
     });
 });
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapHealthChecks("/health");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
