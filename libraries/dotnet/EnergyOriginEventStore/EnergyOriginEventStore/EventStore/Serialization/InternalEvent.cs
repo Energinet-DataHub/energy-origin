c5ac62b670733284ced9d40f86ae4e203ba14d9b
Paul Toft Duizer <paul@duizer.dk>
2022-10-20 14:08:28 +0200

    remove from code

--- libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Serialization/InternalEvent.cs
+++ libraries/dotnet/EnergyOriginEventStore/EnergyOriginEventStore/EventStore/Serialization/InternalEvent.cs
@@ -1,16 +1,14 @@
-using EnergyOriginDateTimeExtension;
-
 namespace EnergyOriginEventStore.EventStore.Serialization;
 
 internal record InternalEvent(string Id, long Issued, long IssuedFraction, string ModelType, int ModelVersion, string Data)
 {
     internal static InternalEvent From(EventModel model)
     {
-        var now = DateTime.Now;
+        var now = DateTimeOffset.UtcNow;
 
         return new InternalEvent(
             Guid.NewGuid().ToString(),
-            now.ToUnixTime(),
+            now.ToUnixTimeSeconds(),
             now.Ticks,
             model.Type,
             model.Version,
