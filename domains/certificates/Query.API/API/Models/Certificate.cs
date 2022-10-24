11b0a267ad78e2b49de99021bd35dc23c8d1775e
Paul Toft Duizer <paul@duizer.dk>
2022-10-24 13:42:05 +0200

    Certificates Query.Api mock (#142)
    Co-authored-by: Jakob Jensen <jakobj@jakobj.dk>

---
+++ domains/certificates/Query.API/API/Models/Certificate.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+
+namespace API.Models;
+
+public class Certificate
+{
+    /// <summary>
+    /// Start timestamp for the certificate in Unix time
+    /// </summary>
+    public long DateFrom { get; set; }
+
+    /// <summary>
+    /// End timestamp for the certificate in Unix time
+    /// </summary>
+    public long DateTo { get; set; }
+
+    /// <summary>
+    /// Quantity of energy measured in Wh
+    /// </summary>
+    public long Quantity { get; set; }
+
+    /// <summary>
+    /// Global Service Relation Number (GSRN) for the metering point
+    /// </summary>
+    [JsonPropertyName("gsrn")]
+    public string GSRN { get; set; } = "";
+}
