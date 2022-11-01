f682582203ad00585cd033d84946a01a167568c9
Paul Toft Duizer <paul@duizer.dk>
2022-11-01 16:58:02 +0100

    remove health check as it makes tests fail

--- domains/certificates/Query.API/API/RegistryConnector/Startup.cs
+++ domains/certificates/Query.API/API/RegistryConnector/Startup.cs
@@ -1,4 +1,3 @@
-using Issuer.Worker.RegistryConnector.Health;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace API.RegistryConnector;
@@ -7,7 +6,6 @@ public static class Startup
 {
     public static void AddRegistryConnector(this IServiceCollection services)
     {
-        services.AddHealthChecks().AddCheck<HealthCheckWorker>("HealthCheckWorker");
         services.AddHostedService<RegistryConnectorWorker>();
     }
 }
