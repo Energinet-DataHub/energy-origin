a76cdaaa80e8154d52eb0776db3384fd34529209
Paul Toft Duizer <paul@duizer.dk>
2022-11-08 15:21:52 +0100

    log user claims

--- domains/certificates/Query.API/API/Query.API/Controllers/CertificatesController.cs
+++ domains/certificates/Query.API/API/Query.API/Controllers/CertificatesController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace API.Controllers;
 
@@ -10,12 +11,22 @@ namespace API.Controllers;
 [ApiController]
 public class CertificatesController : ControllerBase
 {
+    private readonly ILogger<CertificatesController> logger;
+
+    public CertificatesController(ILogger<CertificatesController> logger)
+    {
+        this.logger = logger;
+    }
+
     [HttpGet]
     [Route("certificates")]
     public ActionResult<CertificateList> Get()
     {
-        //TODO: Read authenticated user
-
+        var claims = User.Claims
+            .Select(c => c.ToString())
+            .ToArray();
+        logger.LogInformation("User claims {claims}", string.Join(";", claims));
+        
         var gsrns = new[]
         {
             "123456789000000000",
