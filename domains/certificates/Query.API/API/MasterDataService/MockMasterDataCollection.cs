6ef72e4c0c287ee34129e6eeb99800da43340da2
Paul Toft Duizer <paul@duizer.dk>
2022-11-27 10:05:39 +0100

    split into mock master data

--- domains/certificates/Query.API/API/MasterDataService/MockMasterDataCollection.cs
+++ domains/certificates/Query.API/API/MasterDataService/MockMasterDataCollection.cs
@@ -1,3 +1,15 @@
+using System;
+using Baseline;
+using System.Collections.Generic;
+using System.Linq;
+using CertificateEvents.Primitives;
+
 namespace API.MasterDataService;
 
-internal record MockMasterDataCollection(MasterData[] Data);
+internal record MockMasterDataCollection(MockMasterData[] Data)
+{
+    public IEnumerable<string> GetAllGsrns() => Data.Select(d => d.GSRN);
+};
+
+public record MockMasterData(string GSRN, string GridArea, MeteringPointType Type, Technology Technology,
+    string CVR, DateTimeOffset MeteringPointOnboardedStartDate);
