35b11728f9e7eb7cb8d8b9569bcfad71d1176b34
Paul Toft Duizer <paul@duizer.dk>
2022-12-01 15:03:36 +0100

    masstransit

--- domains/certificates/Query.API/API.AppTests/DbTest.cs
+++ domains/certificates/Query.API/API.AppTests/DbTest.cs
@@ -9,15 +9,21 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using API.DataSyncSyncer;
+using API.MasterDataService;
 using API.Query.API.ApiModels;
+using CertificateEvents.Primitives;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
+using IntegrationEvents;
+using MassTransit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace API.AppTests;
 
@@ -58,10 +64,12 @@ public class MartenFixture : IAsyncLifetime
 public class DbTest : IClassFixture<MyApplicationFactory>
 {
     private readonly MyApplicationFactory factory;
+    private readonly ITestOutputHelper testOutputHelper;
 
-    public DbTest(MyApplicationFactory factory)
+    public DbTest(MyApplicationFactory factory, ITestOutputHelper testOutputHelper)
     {
         this.factory = factory;
+        this.testOutputHelper = testOutputHelper;
     }
 
     [Fact]
@@ -77,26 +85,66 @@ public class DbTest : IClassFixture<MyApplicationFactory>
     [Fact]
     public async Task HasData()
     {
-        using var client = factory.CreateAuthenticatedClient(Guid.NewGuid().ToString());
+        var subject = Guid.NewGuid().ToString();
 
-        var createCertRes = await client.GetAsync("createcert");
+        factory.SetMasterData(new MasterData(
+            GSRN: "GSRN",
+            GridArea: "GridArea",
+            Type: MeteringPointType.Production,
+            Technology: new Technology("foo", "bar"),
+            MeteringPointOwner: subject,
+            MeteringPointOnboardedStartDate: DateTimeOffset.Now.AddYears(-1)));
 
-        Assert.Equal(HttpStatusCode.OK, createCertRes.StatusCode);
+        var bus = factory.GetMassTransitBus();
 
-        var response2 = await client.GetAsync("certificates");
-        Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
-        var certificateList = await response2.Content.ReadFromJsonAsync<CertificateList>();
+        await bus.Publish(new EnergyMeasuredIntegrationEvent(
+            GSRN: "GSRN",
+            DateFrom: DateTimeOffset.Now.AddHours(-1).ToUnixTimeSeconds(),
+            DateTo: DateTimeOffset.Now.ToUnixTimeSeconds(),
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured));
+        
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        //await Task.Delay(TimeSpan.FromMilliseconds(10000));
+
+        //var response2 = await client.GetAsync("certificates");
+        //Assert.Equal(HttpStatusCode.OK, response2.StatusCode);
+        //var certificateList = await response2.Content.ReadFromJsonAsync<CertificateList>();
+        //Assert.Equal(1, certificateList?.Result.Count());
+
+        HttpResponseMessage apiResponse;
+        do
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
+            apiResponse = await client.GetAsync("certificates");
+            testOutputHelper.WriteLine("Call GetAsync");
+        } while (apiResponse.StatusCode == HttpStatusCode.NoContent);
+
+        Assert.Equal(HttpStatusCode.OK, apiResponse.StatusCode);
+        var certificateList = await apiResponse.Content.ReadFromJsonAsync<CertificateList>();
         Assert.Equal(1, certificateList?.Result.Count());
     }
+}
+
+public class AnotherMock : IMasterDataService
+{
+    private MasterData? masterData;
+
+    public Task<MasterData?> GetMasterData(string gsrn) => Task.FromResult(masterData);
+
+    public void Set(MasterData data) => masterData = data;
 }
 
 public class MyApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
     private readonly MartenFixture martenFixture;
+    private readonly AnotherMock masterDataServiceMock;
 
     public MyApplicationFactory()
     {
         martenFixture = new MartenFixture();
+        masterDataServiceMock = new AnotherMock();
     }
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -105,8 +153,12 @@ public class MyApplicationFactory : WebApplicationFactory<Program>, IAsyncLifeti
 
         builder.ConfigureTestServices(services =>
         {
-            //Remove DataSyncSyncerWorker
+            // Remove DataSyncSyncerWorker
             services.Remove(services.First(s => s.ImplementationType == typeof(DataSyncSyncerWorker)));
+
+            // Replace IMasterDataService
+            services.Remove(services.First(s => s.ServiceType == typeof(IMasterDataService)));
+            services.AddSingleton<IMasterDataService>(masterDataServiceMock);
         });
     }
 
@@ -119,6 +171,10 @@ public class MyApplicationFactory : WebApplicationFactory<Program>, IAsyncLifeti
 
         return client;
     }
+
+    public IBus GetMassTransitBus() => Services.GetRequiredService<IBus>();
+
+    public void SetMasterData(MasterData data) => masterDataServiceMock.Set(data);
 
     private static string GenerateToken(
         string scope = "",
