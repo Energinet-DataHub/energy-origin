efb012985f3256b123271c8bfcab6ec7252b2043
Paul Toft Duizer <paul@duizer.dk>
2022-12-02 11:49:46 +0100

    cleanup

--- domains/certificates/Query.API/API.AppTests/Extensions/HttpClientExtensions.cs
+++ domains/certificates/Query.API/API.AppTests/Extensions/HttpClientExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
--- domains/certificates/Query.API/API.AppTests/Infrastructure/MartenDbContainer.cs
+++ domains/certificates/Query.API/API.AppTests/Infrastructure/MartenDbContainer.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
