c7cdbd83117a41ba7e8e877388615fbed21da4b4
Paul Toft Duizer <paul@duizer.dk>
2022-12-06 13:14:20 +0100

    to docs

--- domains/certificates/Query.API/API/Program.cs
+++ domains/certificates/Query.API/API/Program.cs
@@ -83,10 +83,10 @@ var app = builder.Build();
 app.MapHealthChecks("/health");
 
 //app.UseSwagger();
-app.UseSwagger(o => o.RouteTemplate = "api-docs/certificates/{documentName}/swagger.json");
+app.UseSwagger(o => o.RouteTemplate = "docs/certificates/{documentName}/swagger.json");
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerUI(o => o.SwaggerEndpoint("/api-docs/certificates/v1/swagger.json", "API v1"));
+    app.UseSwaggerUI(o => o.SwaggerEndpoint("/docs/certificates/v1/swagger.json", "API v1"));
     //app.UseSwaggerUI();
 }
 
