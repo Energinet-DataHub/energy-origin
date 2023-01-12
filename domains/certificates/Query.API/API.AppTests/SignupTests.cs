e32b8cc4ae432b9350c60f24770c9780bf1f00c9
Paul Toft Duizer <paul@duizer.dk>
2023-01-12 12:00:15 +0100

    Add signuptest + proposal for interface

---
+++ domains/certificates/Query.API/API.AppTests/SignupTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using API.AppTests.Infrastructure;
+using FluentAssertions;
+using Xunit;
+
+namespace API.AppTests;
+
+public class SignupTests : IClassFixture<QueryApiWebApplicationFactory>
+{
+    private readonly QueryApiWebApplicationFactory factory;
+
+    public SignupTests(QueryApiWebApplicationFactory factory/*, MartenDbContainer martenDbContainer*/) //Leaving out Marten for now as it takes some time to load and we need to agree on the api interface first
+    {
+        this.factory = factory;
+    }
+
+    [Fact]
+    public async Task GetSomething()
+    {
+        var subject = Guid.NewGuid().ToString();
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        var body = new { gsrn = "12345", startDate = DateTimeOffset.Now.ToUnixTimeSeconds() };
+        
+        var response = await client.PostAsJsonAsync("api/signup", body);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+}
--- domains/certificates/Query.API/API/Query.API/Controllers/SignUpController.cs
+++ domains/certificates/Query.API/API/Query.API/Controllers/SignUpController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Marten;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Query.API.Controllers;
 
@@ -16,18 +15,18 @@ public class SignUpController : ControllerBase
     [ProducesResponseType(201)]
     [ProducesResponseType(302)]
     [Route("api/signup")]
-    public async Task<ActionResult> SignUp([FromServices] IQuerySession querySession)
+    public async Task<ActionResult> SignUp([FromServices] IQuerySession querySession, [FromBody] CreateSignup createSignup)
     {
         var meteringPointOwner = User.FindFirstValue("subject");
         Guid.Parse(meteringPointOwner);
 
-        var exists = querySession.Events.QueryAllRawEvents()
-            .Where(x => x.StreamId == Guid.Parse(meteringPointOwner))
-            .OrderBy(x => x.Sequence)
-            .ToList();
+        //var exists = querySession.Events.QueryAllRawEvents()
+        //    .Where(x => x.StreamId == Guid.Parse(meteringPointOwner))
+        //    .OrderBy(x => x.Sequence)
+        //    .ToList();
 
-
+        return Ok();
     }
-
-
 }
+
+public record CreateSignup(string Gsrn, long StartDate);
