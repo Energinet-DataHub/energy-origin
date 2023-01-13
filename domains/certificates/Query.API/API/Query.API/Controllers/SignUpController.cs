2207ad4e53caf2fc9c2612f8178882b0c79dd64d
Exobitt <dannip1123@outlook.com>
2023-01-13 14:14:43 +0100

    changes

--- domains/certificates/Query.API/API/Query.API/Controllers/SignUpController.cs
+++ domains/certificates/Query.API/API/Query.API/Controllers/SignUpController.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using API.MasterDataService;
+using API.Query.API.Repositories;
 using Marten;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static API.Query.API.Repositories.IMeteringPointSignupRepository;
 
 namespace API.Query.API.Controllers;
 
@@ -15,8 +18,9 @@ public class SignUpController : ControllerBase
     [ProducesResponseType(201)]
     [ProducesResponseType(302)]
     [Route("api/signup")]
-    public async Task<ActionResult> SignUp([FromServices] IQuerySession querySession, [FromBody] CreateSignup createSignup)
+    public async Task<ActionResult> SignUp([FromServices] IDocumentSession session, [FromBody] CreateSignup createSignup)
     {
+        var documentStoreHandler = new MeteringPointSignupRepository(session);
         var meteringPointOwner = User.FindFirstValue("subject");
 
         // Validate CreateSignup
@@ -24,10 +28,31 @@ public class SignUpController : ControllerBase
         // Check ownership and if it is production type of GSRN in datahub
 
         // Check if GSRN is already signed up
-        // Save
+        var document = documentStoreHandler.GetByGsrn(createSignup.Gsrn);
 
+        if (!document.IsFaulted || document.Result != null)
+        {
+            return Conflict();
+        }
+
+        // Save
+        var userObject = new MeteringPointSignup()
+        {
+            Id = new Guid(),
+            GSRN = createSignup.Gsrn,
+            MeteringPointType = MeteringPointType.Production, // This needs to change, when we have data from datasync
+            MeteringPointOwner = meteringPointOwner,
+            SignupStartDate = DateTimeOffset.UtcNow, // Also needs change
+            Created = DateTimeOffset.UtcNow
+        };
+        var documentSaved = documentStoreHandler.Save(userObject);
+        if (documentSaved.IsFaulted)
+        {
+            return Problem();
+        }
+        
         return Ok();
     }
 }
 
-public record CreateSignup(string Gsrn, long StartDate);
+public record CreateSignup(long Gsrn, long StartDate);
--- domains/certificates/Query.API/API/Query.API/Repositories/IMeteringPointSignupRepository.cs
+++ domains/certificates/Query.API/API/Query.API/Repositories/IMeteringPointSignupRepository.cs
@@ -8,7 +8,6 @@ namespace API.Query.API.Repositories;
 public interface IMeteringPointSignupRepository
 {
     Task Save(MeteringPointSignup meteringPointSignup);
-
     Task<MeteringPointSignup?> GetByGsrn(long gsrn);
     Task<IEnumerable<MeteringPointSignup>> GetAll(); //To be used by DataSync
     Task<IEnumerable<MeteringPointSignup>> GetByMeteringPointOwner(string meteringPointOwner);
@@ -16,12 +15,9 @@ public interface IMeteringPointSignupRepository
 
 public class MeteringPointSignup
 {
-    public Guid Id { get; init; } // Maybe GSRN is Id?
-
+    public Guid Id { get; set; } // Maybe GSRN is Id?
     public long GSRN { get; set; }
-
     public MeteringPointType MeteringPointType { get; set; }
-
     public string MeteringPointOwner { get; set; }
     public DateTimeOffset SignupStartDate { get; set; }
     public DateTimeOffset Created { get; set; }
