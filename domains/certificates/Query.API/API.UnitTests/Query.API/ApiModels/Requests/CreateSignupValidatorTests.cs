2e14d50576b56238c6e8b6b03f5caf7038198a89
Paul Toft Duizer <paul@duizer.dk>
2023-01-16 14:56:59 +0100

    Do not trim

--- domains/certificates/Query.API/API.UnitTests/Query.API/ApiModels/Requests/CreateSignupValidatorTests.cs
+++ domains/certificates/Query.API/API.UnitTests/Query.API/ApiModels/Requests/CreateSignupValidatorTests.cs
@@ -56,6 +56,8 @@ public class CreateSignupValidatorTests
     [InlineData("12345678901234567")]
     [InlineData("1234567890123456789")]
     [InlineData("1234567890 12345678")]
+    [InlineData("123456789012345678 ")]
+    [InlineData(" 123456789012345678")]
     public async Task Validate_InvalidGsrn_HaveValidationError(string invalidGsrn)
     {
         var validator = new CreateSignupValidator();
@@ -68,8 +70,6 @@ public class CreateSignupValidatorTests
 
     [Theory]
     [InlineData("123456789012345678")]
-    [InlineData("123456789012345678 ")]
-    [InlineData(" 123456789012345678")]
     public async Task Validate_ValidGsrn_NoValidationError(string validGsrn)
     {
         var validator = new CreateSignupValidator();
