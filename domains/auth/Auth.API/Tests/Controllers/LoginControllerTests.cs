137bcd6b81a5c570b405b2fcc857ad85ab76200e
AFY <AFY@energinet.dk>
2023-01-27 11:21:14 +0100

    test for login

--- domains/auth/Auth.API/Tests/Controllers/LoginControllerTests.cs
+++ domains/auth/Auth.API/Tests/Controllers/LoginControllerTests.cs
@@ -1,11 +1,51 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using API.Controllers;
+using API.Options;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
 
 namespace Tests.Controllers;
 
 public class LoginControllerTests
 {
+    private readonly LoginController sut;
+    public LoginControllerTests()
+    {
+        sut = new LoginController();
+    }
+
+    [Fact]
+    public async Task Index_ReturnsAViewResult_WithAListOfBrainstormSessions()
+    {
+        // Arrange
+        var discoveryCacheMock = new Mock<IDiscoveryCache>();
+        var discoveryDocumentResponseMock = new Mock<DiscoveryDocumentResponse>();
+        var I = discoveryDocumentResponseMock.Object.GetType().GetProperty(nameof(discoveryDocumentResponseMock.Object.IsError), BindingFlags.Public | BindingFlags.Instance);
+        I.SetValue(discoveryDocumentResponseMock.Object, false);
+       // discoveryDocumentResponseMock.SetupGet(z=>z.IsError).Returns(false);
+       // discoveryDocumentResponseMock.SetupGet(z => z.AuthorizeEndpoint).Returns("example.com");
+        discoveryCacheMock.Setup(d => d.GetAsync()).ReturnsAsync(discoveryDocumentResponseMock.Object);
+
+       
+
+
+
+        var oidcOptions = Options.Create(new OidcOptions());
+        oidcOptions.Value.AuthorityUrl = new Uri("example.com");
+        oidcOptions.Value.CacheDuration = new TimeSpan(6, 0, 0);
+        oidcOptions.Value.ClientId = "testClientId";
+        oidcOptions.Value.RedirectUri = "example.com";
+        var loggerMoq = Mock.Of<ILogger<LoginController>>();
+
+        // Act
+        var result = await sut.GetAsync(discoveryCacheMock.Object, oidcOptions, loggerMoq);
+        var okResult = result as ObjectResult;
+
+        // Assert
+        Assert.NotNull(okResult);
+        Assert.Equal(307, okResult.StatusCode);
+    }
 }
