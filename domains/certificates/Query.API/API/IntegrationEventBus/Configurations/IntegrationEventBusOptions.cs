f80af59043c6626e80a1f5790c2a1cd4eff6694d
Christoffer Frydkjær <christofferfk@gmail.com>
2023-02-08 17:56:37 +0100

    add masstransit rabbitmq

---
+++ domains/certificates/Query.API/API/IntegrationEventBus/Configurations/IntegrationEventBusOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace API.IntegrationEventBus.Configurations;
+
+public class IntegrationEventBusOptions
+{
+    public const string IntegrationEventBus = "IntegrationEventBus";
+
+    public string Url { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+}
--- domains/certificates/Query.API/API/IntegrationEventBus/Startup.cs
+++ domains/certificates/Query.API/API/IntegrationEventBus/Startup.cs
@@ -1,18 +1,36 @@
+using API.Configurations;
 using API.GranularCertificateIssuer;
+using API.IntegrationEventBus.Configurations;
 using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace API.IntegrationEventBus;
 
 public static class Startup
 {
-    public static void AddIntegrationEventBus(this IServiceCollection services) =>
+    public static void AddIntegrationEventBus(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<IntegrationEventBusOptions>(
+            configuration.GetSection(IntegrationEventBusOptions.IntegrationEventBus));
+
         services.AddMassTransit(o =>
         {
             o.SetKebabCaseEndpointNameFormatter();
 
             o.AddConsumer<EnergyMeasuredConsumer>(cc => cc.UseConcurrentMessageLimit(1));
 
-            o.UsingInMemory((context, cfg) => cfg.ConfigureEndpoints(context));
+            o.UsingRabbitMq((context, cfg) =>
+            {
+                var options = context.GetRequiredService<IOptions<IntegrationEventBusOptions>>().Value;
+                cfg.Host(options.Url, "/", h =>
+                {
+                    h.Username(options.Username);
+                    h.Password(options.Password);
+                });
+                cfg.ConfigureEndpoints(context);
+            });
         });
+    }
 }
