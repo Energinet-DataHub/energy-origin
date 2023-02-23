a4bdb905d924b11c40da5b0503b64be4aa8a7f10
Jakob Jensen <jakobj@jakobj.dk>
2023-02-23 15:25:05 +0100

    Add timezone to measurement API (#245)

--- domains/measurements/Measurements.API/API/Services/IDataSyncService.cs
+++ domains/measurements/Measurements.API/API/Services/IDataSyncService.cs
@@ -5,7 +5,7 @@ namespace API.Services;
 
 public interface IDataSyncService
 {
-    Task<IEnumerable<Measurement>> GetMeasurements(AuthorizationContext context, string gsrn, DateTime dateFrom, DateTime dateTo);
+    Task<IEnumerable<Measurement>> GetMeasurements(AuthorizationContext context, string gsrn, DateTimeOffset dateFrom, DateTimeOffset dateTo);
 
     Task<IEnumerable<MeteringPoint>> GetListOfMeteringPoints(AuthorizationContext context);
 }
