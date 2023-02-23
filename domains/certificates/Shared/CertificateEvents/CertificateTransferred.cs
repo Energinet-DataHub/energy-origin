ebc36ee0f505f315a7812fecf886d7678efef61e
Christoffer Frydkjær <christofferfk@gmail.com>
2023-02-23 11:19:41 +0100

    add event

---
+++ domains/certificates/Shared/CertificateEvents/CertificateTransferred.cs
@@ -0,0 +1,18 @@
+using System;
+using CertificateEvents.Primitives;
+
+namespace CertificateEvents;
+
+public record CertificateTransferred(
+    Guid CertificateId,
+    string From,
+    string To,
+
+    // Duplicated values from below
+    string GridArea,
+    Period Period,
+    Technology Technology,
+    string MeteringPointOwner,
+    ShieldedValue<string> ShieldedGSRN,
+    ShieldedValue<long> ShieldedQuantity
+);
