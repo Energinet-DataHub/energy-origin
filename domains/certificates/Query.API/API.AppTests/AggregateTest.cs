f00fadf233caf07acdb7c49d8ce830b14bc20a9c
Paul Toft Duizer <paul@duizer.dk>
2023-02-27 14:07:21 +0100

    concurrency test

--- domains/certificates/Query.API/API.AppTests/AggregateTest.cs
+++ domains/certificates/Query.API/API.AppTests/AggregateTest.cs
@@ -3,6 +3,7 @@ using API.GranularCertificateIssuer;
 using CertificateEvents.Primitives;
 using FluentAssertions;
 using Marten;
+using Marten.Exceptions;
 using Xunit;
 
 namespace API.AppTests;
@@ -29,5 +30,30 @@ public class AggregateTest
         aggregateFromRepo.Transfer("owner2", "owner3");
 
         aggregateFromRepo.CertificateOwner.Should().Be("owner3");
+    }
+
+    [Fact]
+    public async Task ConcurrencyTest()
+    {
+        var documentStore = DocumentStore.For("host=localhost;Port=5432;Database=marten;username=postgres;password=postgres;");
+
+        var repo = new AggregateRepository(documentStore);
+
+        var aggregate = new ProductionCertificateAggregate("gridArea", new Period(1, 42), new Technology("f00", "t00"),
+            "owner1", "gsrn", 42);
+
+        await repo.StoreAsync(aggregate);
+
+        var aggregateFromRepo1 = await repo.LoadAsync<ProductionCertificateAggregate>(aggregate.Id);
+        var aggregateFromRepo2 = await repo.LoadAsync<ProductionCertificateAggregate>(aggregate.Id);
+
+        aggregateFromRepo1.Issue();
+        aggregateFromRepo2.Reject("foo");
+
+        var storeAction1 = () => repo.StoreAsync(aggregateFromRepo1);
+        var storeAction2 = () => repo.StoreAsync(aggregateFromRepo2);
+
+        await storeAction1.Should().NotThrowAsync();
+        await storeAction2.Should().ThrowAsync<EventStreamUnexpectedMaxEventIdException>();
     }
 }
