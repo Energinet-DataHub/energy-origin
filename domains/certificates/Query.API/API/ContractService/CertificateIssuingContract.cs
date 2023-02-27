4439fffb58e8136db4f7df83e139ad725e17448c
anna-knudsen <anna-knudsen@outlook.dk>
2023-02-27 12:08:58 +0100

    #1291 - marten unique index added to CertificateIssuingContract.cs to avoid concurrency problems

--- domains/certificates/Query.API/API/ContractService/CertificateIssuingContract.cs
+++ domains/certificates/Query.API/API/ContractService/CertificateIssuingContract.cs
@@ -1,12 +1,18 @@
 using System;
 using CertificateEvents.Primitives;
+using Marten.Schema;
 
 namespace API.ContractService;
 
 public class CertificateIssuingContract
 {
+    private const string uniqueIndexName = "unique_index_contract";
+
     public Guid Id { get; set; }
+    [UniqueIndex(IndexType = UniqueIndexType.Computed, IndexName = uniqueIndexName)]
     public string GSRN { get; set; } = "";
+    [UniqueIndex(IndexType = UniqueIndexType.Computed, IndexName = uniqueIndexName)]
+    public int ContractNumber { get; set; }
     public string GridArea { get; set; } = "";
     public MeteringPointType MeteringPointType { get; set; }
     public string MeteringPointOwner { get; set; } = "";
