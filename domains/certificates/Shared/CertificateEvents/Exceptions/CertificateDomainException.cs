e12ee2cb07bff7fea3d6ca708d84db67b06438aa
Paul Toft Duizer <paul@duizer.dk>
2023-02-27 21:09:06 +0100

    fix smell

--- domains/certificates/Shared/CertificateEvents/Exceptions/CertificateDomainException.cs
+++ domains/certificates/Shared/CertificateEvents/Exceptions/CertificateDomainException.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace CertificateEvents.Exceptions;
 
+[Serializable]
 public class CertificateDomainException : Exception
 {
     public CertificateDomainException(Guid certificateId, string message) : base(message)
+    {
+    }
+
+    protected CertificateDomainException(SerializationInfo info, StreamingContext context) : base(info, context)
     {
     }
 }
