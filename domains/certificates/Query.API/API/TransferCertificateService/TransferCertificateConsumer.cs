b2e817a95ecd9cb5070cc27cb6bd9fc385d29e09
Paul Toft Duizer <paul@duizer.dk>
2023-03-01 20:55:46 +0100

    fix log statement

--- domains/certificates/Query.API/API/TransferCertificateService/TransferCertificateConsumer.cs
+++ domains/certificates/Query.API/API/TransferCertificateService/TransferCertificateConsumer.cs
@@ -1,31 +1,20 @@
-using System;
 using System.Threading.Tasks;
 using API.GranularCertificateIssuer.Repositories;
 using CertificateEvents.Exceptions;
 using Contracts.Transfer;
 using MassTransit;
-using Microsoft.Extensions.Logging;
 using static Contracts.Transfer.TransferProductionCertificateResponse;
 
 namespace API.TransferCertificateService;
 
 public class TransferCertificateConsumer : IConsumer<TransferProductionCertificateRequest>
 {
-    private readonly ILogger<TransferCertificateConsumer> logger;
     private readonly IProductionCertificateRepository repository;
 
-    public TransferCertificateConsumer(ILogger<TransferCertificateConsumer> logger, IProductionCertificateRepository repository)
-    {
-        this.logger = logger;
-        this.repository = repository;
-    }
+    public TransferCertificateConsumer(IProductionCertificateRepository repository) => this.repository = repository;
 
     public async Task Consume(ConsumeContext<TransferProductionCertificateRequest> context)
     {
-        logger.LogInformation($"Current owner: {context.Message.CurrentOwner} \nl" +
-                              $"New owner: {context.Message.NewOwner} \nl" +
-                              $"CertificateID: {context.Message.CertificateId}");
-
         var productionCertificate = await repository.Get(context.Message.CertificateId, cancellationToken: context.CancellationToken);
 
         if (productionCertificate == null)
