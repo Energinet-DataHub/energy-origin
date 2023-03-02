1d9d1fd6b6501c5d3dc61777f36fae46baf77570
Jakob Jensen <jakobj@jakobj.dk>
2023-03-02 16:08:20 +0100

    Upgrade emissions to dotnet 7 and add timezone to API (#254)

--- domains/emissions/Emissions.API/API/Models/EnergySourceRequest.cs
+++ domains/emissions/Emissions.API/API/Models/EnergySourceRequest.cs
@@ -5,12 +5,16 @@ namespace API.Models;
 
 public class EnergySourceRequest
 {
-    public long DateFrom { get; set; }
+    public long DateFrom { get; init; }
 
-    public long DateTo { get; set; }
+    public long DateTo { get; init; }
 
     [DefaultValue(Aggregation.Total)]
-    public Aggregation Aggregation { get; set; } = Aggregation.Total;
+    public Aggregation Aggregation { get; init; } = Aggregation.Total;
+
+    public string TimeZone { get; init; } = TimeZoneInfo.Utc.Id;
+
+    internal TimeZoneInfo TimeZoneInfo => TimeZoneInfo.FindSystemTimeZoneById(TimeZone);
 
     public class Validator : AbstractValidator<EnergySourceRequest>
     {
@@ -18,6 +22,7 @@ public class EnergySourceRequest
         {
             RuleFor(a => a.DateFrom).NotEmpty().LessThan(a => a.DateTo);
             RuleFor(a => a.DateTo).NotEmpty().GreaterThan(a => a.DateFrom);
+            RuleFor(x => x.TimeZone).Must(id => { try { _ = TimeZoneInfo.FindSystemTimeZoneById(id); return true; } catch { return false; } }).WithMessage("Must be a valid time zone identifier");
         }
     }
 }
