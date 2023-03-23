d4cd2b64bd712d2e7f5e7cda31922504468945ba
CodeBeaver <afy@energinet.dk>
2023-03-23 10:48:41 +0100

    changes

--- domains/auth/Auth.API/API/Utilities/UserDescriptMapper.cs
+++ domains/auth/Auth.API/API/Utilities/UserDescriptMapper.cs
@@ -6,12 +6,10 @@ namespace API.Utilities;
 public class UserDescriptMapper : UserDescriptMapperBase, IUserDescriptMapper
 {
     private readonly ICryptography cryptography;
-    private readonly ILogger<UserDescriptMapper> logger;
 
     public UserDescriptMapper(ICryptography cryptography, ILogger<UserDescriptMapper> logger) : base(cryptography, logger)
     {
         this.cryptography = cryptography;
-        this.logger = logger;
     }
 
     public UserDescriptor Map(User user, string accessToken, string identityToken) => new(cryptography)
