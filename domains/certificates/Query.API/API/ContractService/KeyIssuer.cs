fa1019e76cc62d7fd0ef1b7b2b013e5f5147330c
Exobitt <dannip1123@outlook.com>
2023-03-27 20:01:21 +0200

    Key Issuer

--- domains/certificates/Query.API/API/ContractService/KeyIssuer.cs
+++ domains/certificates/Query.API/API/ContractService/KeyIssuer.cs
@@ -1,21 +1,38 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using NSec.Cryptography;
 
 namespace API.ContractService;
 
-public class KeyIssuer
+public class KeyIssuer : IKeyIssuer
 {
-    public void Create(string meteringPointOwner)
+    private readonly SignatureAlgorithm algorithm;
+
+    public KeyIssuer()
     {
-        // QUESTION: Do we need to store the private key, when we can verify through a message?
-        var algorithm = SignatureAlgorithm.Ed25519;
+        algorithm = SignatureAlgorithm.Ed25519;
+    }
+
+    public (string PublicKey, string signature) Create(string meteringPointOwner)
+    {
         using var key = Key.Create(algorithm);
 
         var data = Encoding.UTF8.GetBytes(meteringPointOwner);
 
         var signature = algorithm.Sign(key, data);
 
-        
+
+        if (Verify(key.PublicKey, data, signature))
+        {
+            return (Encode(key.PublicKey.Export(KeyBlobFormat.NSecPublicKey)), Encode(signature));
+        }
+
+        throw new Exception("Key could not be generated");
     }
 
+    public bool Verify(PublicKey publicKey, byte[] data, byte[] signature) => algorithm.Verify(publicKey, data, signature);
+
+    public string Encode(byte[] target) => Convert.ToBase64String(target);
+
+    public byte[] Decode(string target) => Convert.FromBase64String(target);
 }
