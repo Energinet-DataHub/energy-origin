63ccbf2d3c533219b58cc599cf4ac6d22f18e1f4
Exobitt <dannip1123@outlook.com>
2023-03-27 14:34:08 +0200

    Preparation for keys

---
+++ domains/certificates/Query.API/API/ContractService/KeyIssuer.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using NSec.Cryptography;
+
+namespace API.ContractService;
+
+public class KeyIssuer
+{
+    public void Create(string meteringPointOwner)
+    {
+        // QUESTION: Do we need to store the private key, when we can verify through a message?
+        var algorithm = SignatureAlgorithm.Ed25519;
+        using var key = Key.Create(algorithm);
+
+        var data = Encoding.UTF8.GetBytes(meteringPointOwner);
+
+        var signature = algorithm.Sign(key, data);
+
+        
+    }
+
+}
