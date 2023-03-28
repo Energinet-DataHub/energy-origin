fd5e6574b8452da6ab672560ce74b04c4c8ec974
Paul Toft Duizer <paul@duizer.dk>
2023-03-28 14:44:08 +0200

    Setup MassTransit

--- domains/certificates/RegistryConnector/Worker/Program.cs
+++ domains/certificates/RegistryConnector/Worker/Program.cs
@@ -1,7 +1,10 @@
+using System;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using RegistryConnector.Worker;
 using Serilog;
 using Serilog.Formatting.Json;
@@ -19,10 +22,30 @@ loggerConfiguration = builder.Environment.IsDevelopment()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(loggerConfiguration.CreateLogger());
 
+builder.Services.Configure<RabbitMqOptions>(builder.Configuration.GetSection(RabbitMqOptions.RabbitMq));
 builder.Services.Configure<RegistryOptions>(builder.Configuration.GetSection(RegistryOptions.Registry));
+
 builder.Services.AddHostedService<Worker>();
 
 builder.Services.AddHealthChecks();
+
+builder.Services.AddMassTransit(o =>
+{
+    o.SetKebabCaseEndpointNameFormatter();
+
+    o.UsingRabbitMq((context, cfg) =>
+    {
+        var options = context.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+        var url = $"rabbitmq://{options.Host}:{options.Port}";
+
+        cfg.Host(new Uri(url), h =>
+        {
+            h.Username(options.Username);
+            h.Password(options.Password);
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
