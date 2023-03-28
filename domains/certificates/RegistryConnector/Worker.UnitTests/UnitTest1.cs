1d820af411e5862d21cbf04b7bd1cf045c5e37a6
Paul Toft Duizer <paul@duizer.dk>
2023-03-28 14:30:07 +0200

    format

--- domains/certificates/RegistryConnector/Worker.UnitTests/UnitTest1.cs
+++ domains/certificates/RegistryConnector/Worker.UnitTests/UnitTest1.cs
@@ -5,5 +5,5 @@ namespace RegistryConnector.Worker.UnitTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1() => Assert.Equal(2+2, 4);
+    public void Test1() => Assert.Equal(2 + 2, 4);
 }
