982135b49dd64d57b822b3efc7058aa484c537f6
Paul Toft Duizer <paul@duizer.dk>
2023-03-28 14:00:54 +0200

    add health endpoint

--- domains/certificates/RegistryConnector/RegistryConnector/Program.cs
+++ domains/certificates/RegistryConnector/RegistryConnector/Program.cs
@@ -22,8 +22,10 @@ builder.Logging.AddSerilog(loggerConfiguration.CreateLogger());
 builder.Services.Configure<RegistryOptions>(builder.Configuration.GetSection(RegistryOptions.Registry));
 builder.Services.AddHostedService<Worker>();
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapHealthChecks("/health");
 
 app.Run();
