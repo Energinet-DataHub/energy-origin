878a035ea7cbb2cd76bc4b10a5437aeb79ae8fcf
Paul Toft Duizer <paul@duizer.dk>
2023-03-28 11:35:32 +0200

    logging + fix chart

--- domains/certificates/RegistryConnector/RegistryConnector/Program.cs
+++ domains/certificates/RegistryConnector/RegistryConnector/Program.cs
@@ -1,6 +1,22 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var loggerConfiguration = new LoggerConfiguration()
+    .Filter
+    .ByExcluding("RequestPath like '/health%'");
+
+loggerConfiguration = builder.Environment.IsDevelopment()
+    ? loggerConfiguration.WriteTo.Console()
+    : loggerConfiguration.WriteTo.Console(new JsonFormatter());
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(loggerConfiguration.CreateLogger());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
