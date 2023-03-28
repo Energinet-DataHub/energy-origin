fae2a807467edd42c84fbde36b0811841a5924e7
Morten (MOQ) <40111000+MortMH@users.noreply.github.com>
2023-03-28 14:17:14 +0200

    Library updates for #1350 (#281)

---
+++ libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation.Unit.Tests/Utilities/UserDescriptorTests.cs
@@ -0,0 +1,66 @@
+using EnergyOrigin.TokenValidation.Options;
+using EnergyOrigin.TokenValidation.Utilities;
+using EnergyOrigin.TokenValidation.Utilities.Interfaces;
+using EnergyOrigin.TokenValidation.Values;
+
+namespace EnergyOrigin.TokenValidation.Unit.Tests.Utilities;
+
+public class UserDescriptorTests
+{
+    private readonly ICryptography cryptography;
+
+    public UserDescriptorTests()
+    {
+        var options = new CryptographyOptions()
+        {
+            Key = "secretsecretsecretsecret"
+        };
+
+        cryptography = new Cryptography(Microsoft.Extensions.Options.Options.Create(options));
+    }
+
+    [Theory]
+    [InlineData("dd76accc-e477-4895-9e04-763309acee4e", "8d025d7a-eae2-4242-a861-43bc4e6a2c16", "8d025d7a-eae2-4242-a861-43bc4e6a2c16")]
+    [InlineData("dd76accc-e477-4895-9e04-763309acee4e", null, "dd76accc-e477-4895-9e04-763309acee4e")]
+    [InlineData(null, "8d025d7a-eae2-4242-a861-43bc4e6a2c16", "8d025d7a-eae2-4242-a861-43bc4e6a2c16")]
+    [InlineData(null, null, null)]
+    public void UserDescriptor_ShouldCalculateSubject_WhenSuppliedWithUserAndCompanyIds(string? userId, string? companyId, string? expectedSubject)
+    {
+        var descriptor = new UserDescriptor(null!)
+        {
+            Id = Guid.TryParse(userId, out var userGuid) ? userGuid : null,
+            CompanyId = Guid.TryParse(companyId, out var companyGuid) ? companyGuid : null,
+        };
+
+        Assert.Equal(expectedSubject, descriptor.Subject?.ToString());
+    }
+
+    [Fact]
+    public void UserDescriptor_ShouldUnencryptAndFormatKeys_WhenProviderKeysIsReferenced()
+    {
+        var dict = new Dictionary<ProviderKeyType, string>
+        {
+            { ProviderKeyType.PID, Guid.NewGuid().ToString() },
+            { ProviderKeyType.RID, Guid.NewGuid().ToString() },
+            { ProviderKeyType.MitID_UUID, Guid.NewGuid().ToString() },
+        };
+
+        var providerKeys = string.Join(" ", dict.Select(x => $"{x.Key}={x.Value}"));
+        var providerKeysEncrypted = cryptography.Encrypt(providerKeys);
+
+        var descriptor = new UserDescriptor(cryptography)
+        {
+            EncryptedProviderKeys = providerKeysEncrypted
+        };
+
+        var keys = descriptor.ProviderKeys;
+
+        Assert.Equal(3, dict.Count);
+        Assert.Equal(keys.ElementAt(0).Key, dict.ElementAt(0).Key);
+        Assert.Equal(keys.ElementAt(0).Value, dict.ElementAt(0).Value);
+        Assert.Equal(keys.ElementAt(1).Key, dict.ElementAt(1).Key);
+        Assert.Equal(keys.ElementAt(1).Value, dict.ElementAt(1).Value);
+        Assert.Equal(keys.ElementAt(2).Key, dict.ElementAt(2).Key);
+        Assert.Equal(keys.ElementAt(2).Value, dict.ElementAt(2).Value);
+    }
+}
