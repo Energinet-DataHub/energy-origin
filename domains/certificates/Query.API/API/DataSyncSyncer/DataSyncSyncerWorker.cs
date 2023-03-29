afa0653ef3df83875f570b06dd1f34f78be87091
Exobitt <dannip1123@outlook.com>
2023-03-29 14:42:31 +0200

    added automatic updates of contract if publickey is not present

--- domains/certificates/Query.API/API/DataSyncSyncer/DataSyncSyncerWorker.cs
+++ domains/certificates/Query.API/API/DataSyncSyncer/DataSyncSyncerWorker.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using AggregateRepositories;
 using API.ContractService;
+using API.ContractService.Repositories;
 using API.DataSyncSyncer.Client.Dto;
+using API.KeyIssuer;
 using Marten;
 using MassTransit;
 using MeasurementEvents;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -20,17 +22,20 @@ internal class DataSyncSyncerWorker : BackgroundService
     private readonly ILogger<DataSyncSyncerWorker> logger;
     private readonly IDocumentStore documentStore;
     private readonly DataSyncService dataSyncService;
+    private readonly IServiceScopeFactory scopeFactory;
 
     public DataSyncSyncerWorker(
         ILogger<DataSyncSyncerWorker> logger,
         IDocumentStore documentStore,
         IBus bus,
-        DataSyncService dataSyncService)
+        DataSyncService dataSyncService,
+        IServiceScopeFactory scopeFactory)
     {
         this.bus = bus;
         this.logger = logger;
         this.documentStore = documentStore;
         this.dataSyncService = dataSyncService;
+        this.scopeFactory = scopeFactory;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -40,13 +45,19 @@ internal class DataSyncSyncerWorker : BackgroundService
             var allContracts = await GetAllContracts(stoppingToken);
             foreach (var contract in allContracts)
             {
-                /*
                 if (string.IsNullOrEmpty(contract.PublicKey))
                 {
-                    documentStore.Save()
+                    using var scope = scopeFactory.CreateScope();
+                    var keyIssuer = scope.ServiceProvider.GetService<IKeyIssuer>();
+                    var certificateIssuingContractRepository = scope.ServiceProvider.GetService<ICertificateIssuingContractRepository>();
 
+                    if (keyIssuer != null && certificateIssuingContractRepository != null)
+                    {
+                        var publicKey = await keyIssuer.Create(contract.MeteringPointOwner, stoppingToken);
+                        contract.PublicKey = publicKey;
+                        await certificateIssuingContractRepository.Update(contract);
+                    }
                 }
-                */
 
                 var measurements = await dataSyncService.FetchMeasurements(contract,
                     stoppingToken);
--- domains/certificates/Query.API/API/KeyIssuer/KeyIssuingDocument.cs
+++ domains/certificates/Query.API/API/KeyIssuer/KeyIssuingDocument.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Marten.Schema;
-
-namespace API.KeyIssuer;
+﻿ namespace API.KeyIssuer;
 
 public class KeyIssuingDocument
 {
