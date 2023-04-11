365fb684aa97797e1365345cc2e8d83bbbac2296
Paul Toft Duizer <paul@duizer.dk>
2023-04-11 19:20:40 +0200

    write class

--- domains/certificates/Query.API/API.AppTests/Infrastructure/WriteToConsole/WriteToConsoleAttribute.cs
+++ domains/certificates/Query.API/API.AppTests/Infrastructure/WriteToConsole/WriteToConsoleAttribute.cs
@@ -9,12 +9,12 @@ internal class WriteToConsoleAttribute : BeforeAfterTestAttribute
     public override void Before(MethodInfo methodUnderTest)
     {
         Console.WriteLine();
-        Console.WriteLine($"[Starting {methodUnderTest.Name}]");
+        Console.WriteLine($"[Starting {methodUnderTest.DeclaringType}.{methodUnderTest.Name}]");
     }
 
     public override void After(MethodInfo methodUnderTest)
     {
-        Console.WriteLine($"[Finished {methodUnderTest.Name}]");
+        Console.WriteLine($"[Finished {methodUnderTest.DeclaringType}.{methodUnderTest.Name}]");
         Console.WriteLine();
     }
 }
