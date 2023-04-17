bbdb6ce19d4a87071f0e37163ead535ccf008ffa
Jakob Jensen <jakobj@jakobj.dk>
2023-04-17 13:45:14 +0200

    Fix tests

--- domains/auth/Auth.API/Unit.Tests/Options/IdentityProviderOptionsTests.cs
+++ domains/auth/Auth.API/Unit.Tests/Options/IdentityProviderOptionsTests.cs
@@ -6,14 +6,14 @@ namespace Unit.Tests.Options;
 public class IdentityProviderOptionsTests
 {
     [Theory]
-    [InlineData("openid ssn userinfo_token nemid", "nemid", """{"nemid": {"amr_values": "nemid.otp"}}""", ProviderType.NemID_Private)]
-    [InlineData("openid ssn userinfo_token nemid", "nemid", """{"nemid": {"amr_values": "nemid.keyfile"}}""", ProviderType.NemID_Professional)]
+    [InlineData("openid ssn userinfo_token nemid", "nemid", """{"nemid": {"amr_values": "nemid.otp nemid.keyfile"}}""", ProviderType.NemID_Private)]
+    [InlineData("openid ssn userinfo_token nemid", "nemid", """{"nemid": {"amr_values": "nemid.otp nemid.keyfile"}}""", ProviderType.NemID_Professional)]
     [InlineData("openid ssn userinfo_token nemid private_to_business", "nemid", """{"nemid": {"amr_values": "nemid.otp nemid.keyfile"}}""", ProviderType.NemID_Private, ProviderType.NemID_Professional)]
     [InlineData("openid ssn userinfo_token mitid", "mitid", null, ProviderType.MitID_Private)]
     [InlineData("openid ssn userinfo_token nemlogin", "mitid_erhverv", null, ProviderType.MitID_Professional)]
     [InlineData("openid ssn userinfo_token mitid nemlogin", "mitid mitid_erhverv", null, ProviderType.MitID_Private, ProviderType.MitID_Professional)]
-    [InlineData("openid ssn userinfo_token nemid mitid", "nemid mitid", """{"nemid": {"amr_values": "nemid.otp"}}""", ProviderType.MitID_Private, ProviderType.NemID_Private)]
-    [InlineData("openid ssn userinfo_token nemid nemlogin", "nemid mitid_erhverv", """{"nemid": {"amr_values": "nemid.keyfile"}}""", ProviderType.MitID_Professional, ProviderType.NemID_Professional)]
+    [InlineData("openid ssn userinfo_token nemid mitid", "nemid mitid", """{"nemid": {"amr_values": "nemid.otp nemid.keyfile"}}""", ProviderType.MitID_Private, ProviderType.NemID_Private)]
+    [InlineData("openid ssn userinfo_token nemid nemlogin", "nemid mitid_erhverv", """{"nemid": {"amr_values": "nemid.otp nemid.keyfile"}}""", ProviderType.MitID_Professional, ProviderType.NemID_Professional)]
     [InlineData("openid ssn userinfo_token nemid private_to_business mitid nemlogin", "nemid mitid mitid_erhverv", """{"nemid": {"amr_values": "nemid.otp nemid.keyfile"}}""", ProviderType.MitID_Private, ProviderType.MitID_Professional, ProviderType.NemID_Private, ProviderType.NemID_Professional)]
     public void GetIdentityProviderArguments_ShouldReturnCorrectValues_WhenInvokedWithNemIdProviderOptions(string expectedScope, string expectedValues, string expectedParams, params ProviderType[] providerTypes)
     {
@@ -26,7 +26,17 @@ public class IdentityProviderOptionsTests
 
         Assert.Equal(expectedScope, actualScope);
         Assert.Contains(new KeyValuePair<string, string>("idp_values", expectedValues), actualArguments);
-        Assert.Equal(!string.IsNullOrEmpty(expectedParams), actualArguments.Any(arg => arg.Key == "idp_params" && arg.Value == expectedParams));
+
+        var ipdParams = actualArguments.Where(x => x.Key == "idp_params").Select(x => x.Value).FirstOrDefault();
+        if (ipdParams != null)
+        {
+            Assert.True(!string.IsNullOrEmpty(expectedParams));
+            Assert.Equal(expectedParams, ipdParams);
+        }
+        else
+        {
+            Assert.True(string.IsNullOrEmpty(expectedParams));
+        }
     }
 
     [Fact]
