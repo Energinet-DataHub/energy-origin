d32cd041a9ad2bcb1b8cdfec4b1b92deed574762
Morten Munck Hansen <MOQ@energinet.dk>
2023-04-18 11:37:35 +0200

    temp

--- libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/Options/CryptographyOptions.cs
+++ libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/Options/CryptographyOptions.cs
@@ -5,6 +5,7 @@ namespace EnergyOrigin.TokenValidation.Options;
 public class CryptographyOptions
 {
     public const string Prefix = "Cryptography";
+
     [Required]
     public string Key { get; init; } = null!;
 }
