964fb118f7098a934b87f218977cf2c2811de254
CodeBeaver <afy@energinet.dk>
2023-04-18 11:16:17 +0200

    Validation of CryptographyOptions

--- libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/Options/CryptographyOptions.cs
+++ libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/Options/CryptographyOptions.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EnergyOrigin.TokenValidation.Options;
 
 public class CryptographyOptions
 {
     public const string Prefix = "Cryptography";
-
+    [Required]
     public string Key { get; init; } = null!;
 }
