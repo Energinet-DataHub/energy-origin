b1535e1eb11cfced24f9a9f2570b1a75a3e2dee1
Paul Toft Duizer <paul@duizer.dk>
2023-05-02 09:35:11 +0200

    Use ValidationProblemDetails as response type for 400 BadRequest (#303)

--- domains/certificates/Query.API/API/Query.API/Startup.cs
+++ domains/certificates/Query.API/API/Query.API/Startup.cs
@@ -1,9 +1,12 @@
+using System;
+using System.IO;
 using API.Query.API.ApiModels.Requests;
 using API.Query.API.Projections;
+using API.Query.API.SwaggerGen;
 using FluentValidation;
 using Marten;
-using Marten.Events.Projections;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace API.Query.API;
 
@@ -11,6 +14,19 @@ public static class Startup
 {
     public static void AddQueryApi(this IServiceCollection services)
     {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(o =>
+        {
+            o.OperationFilter<ResponseSchemaForBadRequestFilter>();
+            o.SupportNonNullableReferenceTypes();
+            o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "documentation.xml"));
+            o.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Certificates Query API"
+            });
+        });
+
         services.ConfigureMarten(o =>
         {
             o.Projections.Add(new CertificatesByOwnerProjection());
