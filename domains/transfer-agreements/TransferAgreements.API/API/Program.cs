ecef8fcc0376426ed6a24246b744a90afc0d7329
Paul Toft Duizer <paul@duizer.dk>
2023-05-04 19:53:34 +0200

    add serilog

--- domains/transfer-agreements/TransferAgreements.API/API/Program.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/Program.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog.Formatting.Json;
+using Serilog;
+using Serilog.Enrichers.Span;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var loggerConfiguration = new LoggerConfiguration()
+    .Filter.ByExcluding("RequestPath like '/health%'")
+    .Filter.ByExcluding("RequestPath like '/metrics%'")
+    .Enrich.WithSpan();
+
+loggerConfiguration = builder.Environment.IsDevelopment()
+    ? loggerConfiguration.WriteTo.Console()
+    : loggerConfiguration.WriteTo.Console(new JsonFormatter());
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(loggerConfiguration.CreateLogger());
 
 builder.Services.AddHealthChecks();
 
