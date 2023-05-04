d7999892a702b0f0ef1fc0ba1480763ad2862ecd
Paul Toft Duizer <paul@duizer.dk>
2023-05-04 16:53:44 +0200

    Add Dockerfile

--- domains/transfer-agreements/TransferAgreements.API/API/Controllers/WeatherForecastController.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace API.Controllers;
 
--- domains/transfer-agreements/TransferAgreements.API/API/WeatherForecast.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace API;
 
 public class WeatherForecast
