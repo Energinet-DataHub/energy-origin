d4e8e27db6865090407ece7e29525f7773b19013
Paul Toft Duizer <paul@duizer.dk>
2023-05-04 21:10:29 +0200

    set route

---
+++ domains/transfer-agreements/TransferAgreements.API/API/Controllers/TransferAgreementsController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.ApiModels.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+public class TransferAgreementsController : ControllerBase
+{
+    private static readonly string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    /// <summary>
+    /// This comment is included
+    /// </summary>
+    [HttpGet]
+    [ProducesResponseType(typeof(WeatherForecastList), 200)]
+    [Route("api/transfer-agreements")]
+    public IActionResult Get() =>
+        Ok(new WeatherForecastList
+        {
+            Result =
+                Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray()
+        });
+}
