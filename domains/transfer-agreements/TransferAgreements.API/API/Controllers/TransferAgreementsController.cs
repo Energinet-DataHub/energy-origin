f9d01136c8d172097c871d071932717e3f4e86c3
sahma19 <saiuan@msn.com>
2023-05-22 15:01:22 +0200

    Removed weatherforecast traces from API

--- domains/transfer-agreements/TransferAgreements.API/API/Controllers/TransferAgreementsController.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/Controllers/TransferAgreementsController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Linq;
 using System.Security.Claims;
-using API.ApiModels.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,34 +10,4 @@ namespace API.Controllers;
 [ApiController]
 public class TransferAgreementsController : ControllerBase
 {
-    private static readonly string[] summaries = {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    /// <summary>
-    /// This comment is included
-    /// </summary>
-    [HttpGet]
-    [ProducesResponseType(typeof(WeatherForecastList), 200)]
-    [Route("api/transfer-agreements")]
-    public IActionResult Get() =>
-        Ok(new WeatherForecastList
-        {
-            Result =
-                Enumerable.Range(1, 5)
-                    .Select(index => new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray()
-        });
-
-    /// <summary>
-    /// An example of how to get the UUID for the signed in user
-    /// </summary>
-    [HttpGet]
-    [Route("api/transfer-agreements/subject")]
-    public IActionResult GetSubject() => Ok(User.FindFirstValue(ClaimTypes.NameIdentifier));
 }
