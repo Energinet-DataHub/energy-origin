136fdb814a1d56cd3fe32f54088a156ca8c7df85
tnickelsen <TNE@energinet.dk>
2023-05-23 10:33:02 +0200

    Changed gsrn to use regex

--- domains/certificates/Shared/DomainCertificate/ValueObjects/Gsrn.cs
+++ domains/certificates/Shared/DomainCertificate/ValueObjects/Gsrn.cs
@@ -1,12 +1,15 @@
+using System.Text.RegularExpressions;
+
 namespace DomainCertificate.ValueObjects;
 
 public class Gsrn : ValueObject
 {
+    private readonly Regex regex = new("^57\\d{16}$");
     public string Value { get; }
 
     public Gsrn(string value)
     {
-        if (value.Length != 18 || !value.StartsWith("57"))
+        if (!regex.IsMatch(value) || !value.StartsWith("57"))
             throw new ArgumentException("GSRN must be 18 characters long and start with 57.");
 
         Value = value;
