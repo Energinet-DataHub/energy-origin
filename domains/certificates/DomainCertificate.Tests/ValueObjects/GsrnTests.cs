ae768127bdbf4d1383655e6c220690b02dce9982
Paul Toft Duizer <paul@duizer.dk>
2023-05-25 09:26:41 +0200

    more test cases

--- domains/certificates/DomainCertificate.Tests/ValueObjects/GsrnTests.cs
+++ domains/certificates/DomainCertificate.Tests/ValueObjects/GsrnTests.cs
@@ -19,6 +19,13 @@ namespace DomainCertificate.Tests.ValueObjects
         [InlineData("471234567890123456")]
         [InlineData("57123456789012345")]
         [InlineData("5712345678901234567")]
+        [InlineData("571234567890 123456")]
+        [InlineData(" 571234567890123456")]
+        [InlineData("571234567890123456 ")]
+        [InlineData(null)]
+        [InlineData("not a number")]
+        [InlineData("42")]
+        [InlineData("Some18CharsLongStr")]
         public void Ctor_Fail(string value)
         {
             var createGsrn = () => new Gsrn(value);
