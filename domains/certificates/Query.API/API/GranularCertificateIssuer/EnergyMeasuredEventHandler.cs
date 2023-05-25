405cf169812d37b97830e12583e39f531a833b8e
tnickelsen <TNE@energinet.dk>
2023-05-25 12:57:00 +0200

    Review comments..

--- domains/certificates/Query.API/API/GranularCertificateIssuer/EnergyMeasuredEventHandler.cs
+++ domains/certificates/Query.API/API/GranularCertificateIssuer/EnergyMeasuredEventHandler.cs
@@ -48,6 +48,8 @@ public class EnergyMeasuredEventHandler : IConsumer<EnergyMeasuredIntegrationEve
 
         await repository.Save(productionCertificate, context.CancellationToken);
 
+        //TODO handle R values. See issue https://app.zenhub.com/workspaces/team-atlas-633199659e255a37cd1d144f/issues/gh/energinet-datahub/energy-origin-issues/1517
+        //TODO Save to eventstore and publish event must happen in same transaction. See issue https://app.zenhub.com/workspaces/team-atlas-633199659e255a37cd1d144f/issues/gh/energinet-datahub/energy-origin-issues/1518 
         await context.Publish(new ProductionCertificateCreatedEvent(productionCertificate.Id,
             contract.GridArea,
             new Period(message.DateFrom, message.DateTo),
--- domains/certificates/RegistryConnector/Worker/Cache/CertificateEventsInMemoryCache.cs
+++ domains/certificates/RegistryConnector/Worker/Cache/CertificateEventsInMemoryCache.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Contracts.Certificates;
 using Microsoft.Extensions.Logging;
@@ -7,15 +8,23 @@ namespace RegistryConnector.Worker.Cache;
 public class CertificateEventsInMemoryCache : ICertificateEventsInMemoryCache
 {
     private readonly ILogger<CertificateEventsInMemoryCache> logger;
-    private readonly Dictionary<string, MessageWrapper<ProductionCertificateCreatedEvent>> certificateEvents;
+    private readonly ConcurrentDictionary<string, MessageWrapper<ProductionCertificateCreatedEvent>> certificateEvents;
 
     public CertificateEventsInMemoryCache(ILogger<CertificateEventsInMemoryCache> logger)
     {
         this.logger = logger;
-        certificateEvents = new Dictionary<string, MessageWrapper<ProductionCertificateCreatedEvent>>();
+        certificateEvents = new ConcurrentDictionary<string, MessageWrapper<ProductionCertificateCreatedEvent>>();
     }
 
-    public void AddCertificateWithCommandId(CommandId commandId, MessageWrapper<ProductionCertificateCreatedEvent> msg) => certificateEvents.Add(HexHelper.ToHex(commandId), msg);
+    public void AddCertificateWithCommandId(CommandId commandId, MessageWrapper<ProductionCertificateCreatedEvent> msg)
+    {
+        var hex = HexHelper.ToHex(commandId);
+        if (!certificateEvents.TryAdd(hex, msg))
+        {
+            logger.LogError("Key already exist in cache. CommandId: {hex}", hex);
+            throw new KeyAlreadyInCacheException(commandId);
+        }
+    }
 
     public MessageWrapper<ProductionCertificateCreatedEvent>? PopCertificateWithCommandId(CommandId commandId)
     {
