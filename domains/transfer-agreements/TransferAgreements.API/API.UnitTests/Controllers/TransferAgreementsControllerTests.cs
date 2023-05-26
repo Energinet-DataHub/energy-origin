62461b415ae31680c3532bb0d1aebcbdc794aa05
Saiuan Ahmad <saiuan@msn.comn>
2023-05-26 15:23:12 +0200

    Fixed Unit test for controller

--- domains/transfer-agreements/TransferAgreements.API/API.UnitTests/Controllers/TransferAgreementsControllerTests.cs
+++ domains/transfer-agreements/TransferAgreements.API/API.UnitTests/Controllers/TransferAgreementsControllerTests.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using API.Controllers;
-using API.Data;
-using Moq;
-using Xunit;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.ApiModels.Requests;
-using API.ApiModels;
+using API.Controllers;
+using API.Data;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
 
 namespace API.UnitTests.Controllers
 {
@@ -19,19 +21,33 @@ namespace API.UnitTests.Controllers
         {
             mockTransferAgreementService = new Mock<ITransferAgreementService>();
             controller = new TransferAgreementsController(mockTransferAgreementService.Object);
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim("sub", "03bad0af-caeb-46e8-809c-1d35a5863bc7"),
+                new Claim("atr", "d4f32241-442c-4043-8795-a4e6bf574e7f")
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = user }
+            };
         }
 
         [Fact]
-        public async Task Create_ShouldCallServiceCreateOnce()
+        public async Task Create_ShouldCallServiceOnce()
         {
+
             var request = new CreateTransferAgreement();
 
             mockTransferAgreementService.Setup(service => service.CreateTransferAgreement(It.IsAny<TransferAgreement>()))
                 .ReturnsAsync(new TransferAgreement());
 
+
             await controller.Create(request);
 
-            mockTransferAgreementService.Verify(service => service.CreateTransferAgreement(It.IsAny<TransferAgreement>()), Times.AtMostOnce());
+
+            mockTransferAgreementService.Verify(service => service.CreateTransferAgreement(It.IsAny<TransferAgreement>()), Times.AtMostOnce);
         }
     }
 }
