e40cbb2bbcd2b4dceaa0f2c842b80f987a7a8576
sahma19 <saiuan@msn.com>
2023-05-26 12:40:34 +0200

    Changed Datetime to Datetimeutcnow

--- domains/transfer-agreements/TransferAgreements.API/API/Program.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/Program.cs
@@ -91,7 +91,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-// JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 app.UseAuthentication();
 app.UseAuthorization();
 
