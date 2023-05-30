27c9ab2ab5cec1f3e27de54f298e85e31132432d
Morten Munck Hansen <MOQ@energinet.dk>
2023-05-30 17:29:06 +0200

    temp

--- domains/auth/Auth.API/API/Options/JaegerOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace API.Options;
-
-public class JaegerOptions
-{
-    public const string Prefix = "Jaeger";
-
-    public string AgentHost { get; init; } = null!;
-    public int AgentPort { get; init; }
-}
