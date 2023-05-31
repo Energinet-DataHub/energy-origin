7a735fe8948ccf82b679145621304ebd03ed3417
sahma19 <saiuan@msn.com>
2023-05-31 14:54:19 +0200

    Cleanup before review

--- domains/transfer-agreements/TransferAgreements.API/API.UnitTests/Controllers/TransferAgreementsControllerTests.cs
+++ domains/transfer-agreements/TransferAgreements.API/API.UnitTests/Controllers/TransferAgreementsControllerTests.cs
@@ -1,53 +1,49 @@
-﻿using System;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Threading.Tasks;
 using API.ApiModels.Requests;
 using API.Controllers;
 using API.Data;
-using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
 
-namespace API.UnitTests.Controllers
+namespace API.UnitTests.Controllers;
+
+public class TransferAgreementsControllerTests
 {
-    public class TransferAgreementsControllerTests
+    private readonly TransferAgreementsController controller;
+    private readonly Mock<ITransferAgreementService> mockTransferAgreementService;
+
+    public TransferAgreementsControllerTests()
     {
-        private readonly Mock<ITransferAgreementService> mockTransferAgreementService;
-        private readonly TransferAgreementsController controller;
+        mockTransferAgreementService = new Mock<ITransferAgreementService>();
+        controller = new TransferAgreementsController(mockTransferAgreementService.Object);
 
-        public TransferAgreementsControllerTests()
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
-            mockTransferAgreementService = new Mock<ITransferAgreementService>();
-            controller = new TransferAgreementsController(mockTransferAgreementService.Object);
+            new("sub", "03bad0af-caeb-46e8-809c-1d35a5863bc7"),
+            new("atr", "d4f32241-442c-4043-8795-a4e6bf574e7f")
+        }, "mock"));
 
-            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-            {
-                new Claim("sub", "03bad0af-caeb-46e8-809c-1d35a5863bc7"),
-                new Claim("atr", "d4f32241-442c-4043-8795-a4e6bf574e7f")
-            }, "mock"));
-
-            controller.ControllerContext = new ControllerContext()
-            {
-                HttpContext = new DefaultHttpContext() { User = user }
-            };
-        }
-
-        [Fact]
-        public async Task Create_ShouldCallServiceOnce()
+        controller.ControllerContext = new ControllerContext
         {
+            HttpContext = new DefaultHttpContext { User = user }
+        };
+    }
 
-            var request = new CreateTransferAgreement();
+    [Fact]
+    public async Task Create_ShouldCallServiceOnce()
+    {
+        var request = new CreateTransferAgreement();
 
-            mockTransferAgreementService.Setup(service => service.CreateTransferAgreement(It.IsAny<TransferAgreement>()))
-                .ReturnsAsync(new TransferAgreement());
+        mockTransferAgreementService.Setup(service => service.CreateTransferAgreement(It.IsAny<TransferAgreement>()))
+            .ReturnsAsync(new TransferAgreement());
 
 
-            await controller.Create(request);
+        await controller.Create(request);
 
 
-            mockTransferAgreementService.Verify(service => service.CreateTransferAgreement(It.IsAny<TransferAgreement>()), Times.AtMostOnce);
-        }
+        mockTransferAgreementService.Verify(service => service.CreateTransferAgreement(It.IsAny<TransferAgreement>()), Times.AtMostOnce);
     }
 }
--- domains/transfer-agreements/TransferAgreements.API/API.UnitTests/Data/TransferAgreementServiceTests.cs
+++ domains/transfer-agreements/TransferAgreements.API/API.UnitTests/Data/TransferAgreementServiceTests.cs
@@ -17,30 +17,28 @@ namespace API.UnitTests.Data
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
                 .Options;
 
-            using (var context = new ApplicationDbContext(options))
+            await using var context = new ApplicationDbContext(options);
+            await context.Database.EnsureCreatedAsync();
+
+            var service = new TransferAgreementService(context);
+
+            var transferAgreement = new TransferAgreement
             {
-                await context.Database.EnsureCreatedAsync();
+                Id = Guid.NewGuid(),
+                SenderId = Guid.NewGuid(),
+                ActorId = Guid.NewGuid().ToString(),
+                StartDate = DateTimeOffset.UtcNow,
+                EndDate = DateTimeOffset.UtcNow.AddDays(1),
+                ReceiverTin = "12345678"
+            };
 
-                var service = new TransferAgreementService(context);
+            var result = await service.CreateTransferAgreement(transferAgreement);
 
-                var transferAgreement = new TransferAgreement
-                {
-                    Id = Guid.NewGuid(),
-                    SenderId = Guid.NewGuid(),
-                    ActorId = Guid.NewGuid().ToString(),
-                    StartDate = DateTimeOffset.UtcNow,
-                    EndDate = DateTimeOffset.UtcNow.AddDays(1),
-                    ReceiverTin = "12345678"
-                };
+            result.Should().NotBeNull();
+            result.Id.Should().NotBeEmpty();
 
-                var result = await service.CreateTransferAgreement(transferAgreement);
-
-                result.Should().NotBeNull();
-                result.Id.Should().NotBeEmpty();
-
-                var addedTransferAgreement = await context.TransferAgreements.FindAsync(result.Id);
-                addedTransferAgreement.Should().BeEquivalentTo(result);
-            }
+            var addedTransferAgreement = await context.TransferAgreements.FindAsync(result.Id);
+            addedTransferAgreement.Should().BeEquivalentTo(result);
         }
     }
 }
