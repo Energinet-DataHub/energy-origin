e77253baa1767a950350fa0409d2c01388dd9592
Paul Toft Duizer <paul@duizer.dk>
2023-07-13 21:19:52 +0200

    test final part of overlaps+contains

--- domains/certificates/Query.API/API.UnitTests/ContractService/CertificateIssuingContractTests.cs
+++ domains/certificates/Query.API/API.UnitTests/ContractService/CertificateIssuingContractTests.cs
@@ -29,8 +29,8 @@ public class CertificateIssuingContractTests
     [InlineData("2023-01-01T11:00:00Z", "2023-01-01T12:00:00.0001Z")]
     [InlineData("2023-01-01T12:59:59Z", "2023-01-01T14:00:00Z")]
     [InlineData("2023-01-01T12:59:59.999Z", "2023-01-01T14:00:00Z")]
-    public void has_overlap(string start, string end)
-        => contract.Overlaps(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
+    public void has_overlap(string start, string end) =>
+        contract.Overlaps(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
             .Should().BeTrue();
 
     [Theory]
@@ -38,17 +38,22 @@ public class CertificateIssuingContractTests
     [InlineData("2023-01-01T22:00:00Z", "2023-01-01T23:00:00Z")]
     [InlineData("2023-01-01T11:00:00Z", "2023-01-01T12:00:00Z")]
     [InlineData("2023-01-01T13:00:00Z", "2023-01-01T14:00:00Z")]
-    public void has_no_overlap(string start, string end)
-        => contract.Overlaps(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
+    public void has_no_overlap(string start, string end) =>
+        contract.Overlaps(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
             .Should().BeFalse();
+
+    [Fact]
+    public void overlaps_works_with_unix_time_seconds() =>
+        contract.Overlaps(DateTimeOffset.Parse("2023-01-01T12:00:00Z").ToUnixTimeSeconds(), DateTimeOffset.Parse("2023-01-01T13:00:00Z").ToUnixTimeSeconds())
+            .Should().BeTrue();
 
     [Theory]
     [InlineData("2023-01-01T12:00:00Z", "2023-01-01T13:00:00Z")]
     [InlineData("2023-01-01T12:15:00Z", "2023-01-01T12:45:00Z")]
     [InlineData("2023-01-01T12:00:00Z", "2023-01-01T12:00:01Z")]
     [InlineData("2023-01-01T12:59:59Z", "2023-01-01T13:00:00Z")]
-    public void does_contain(string start, string end)
-        => contract.Contains(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
+    public void does_contain(string start, string end) =>
+        contract.Contains(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
             .Should().BeTrue();
 
     [Theory]
@@ -56,7 +61,12 @@ public class CertificateIssuingContractTests
     [InlineData("2023-01-01T13:00:00Z", "2023-01-01T14:00:00Z")]
     [InlineData("2023-01-01T11:59:59.999Z", "2023-01-01T12:30:00Z")]
     [InlineData("2023-01-01T12:30:00Z", "2023-01-01T13:00:00.0001Z")]
-    public void does_not_contain(string start, string end)
-        => contract.Contains(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
+    public void does_not_contain(string start, string end) =>
+        contract.Contains(DateTimeOffset.Parse(start), DateTimeOffset.Parse(end))
             .Should().BeFalse();
+
+    [Fact]
+    public void contains_works_with_unix_time_seconds() =>
+        contract.Contains(DateTimeOffset.Parse("2023-01-01T12:00:00Z").ToUnixTimeSeconds(), DateTimeOffset.Parse("2023-01-01T13:00:00Z").ToUnixTimeSeconds())
+            .Should().BeTrue();
 }
