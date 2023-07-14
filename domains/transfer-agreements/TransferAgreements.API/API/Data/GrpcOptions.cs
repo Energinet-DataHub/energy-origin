28009386874cf28216f1da58e0bd8e673ead1d25
sahma19 <saiuan@msn.com>
2023-07-14 15:11:45 +0200

    Added WalletEndPointService

---
+++ domains/transfer-agreements/TransferAgreements.API/API/Data/GrpcOptions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Text;
+
+namespace API.Data;
+
+public class GrpcOptions
+{
+    public const string ProjectOrigin = nameof(ProjectOrigin);
+    public string WalletUrl { get; set; } = "";
+}
--- domains/transfer-agreements/TransferAgreements.API/API/Data/WalletEndPointService.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/Data/WalletEndPointService.cs
@@ -1,6 +1,62 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ProjectOrigin.WalletSystem.V1;
+
 namespace API.Data;
 
-public class WalletEndPointService
+public class WalletEndPointService : IWalletEndPointService
 {
-    
+    protected readonly IOptions<GrpcOptions> ProjectOriginOptions;
+    protected readonly ILogger Logger;
+    public WalletEndPointService(IOptions<GrpcOptions> grOptions, ILogger logger)
+    {
+        ProjectOriginOptions = grOptions;
+        Logger = logger;
+    }
+
+    protected static string GenerateToken(string issuer, string audience, string subject, string name, int expirationMinutes = 5)
+    {
+        var ecdsa = ECDsa.Create(ECCurve.NamedCurves.nistP256);
+
+        var claims = new[]
+        {
+            new Claim("sub", subject),
+            new Claim("name", name),
+            new Claim("iat", DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
+        };
+
+        var key = new ECDsaSecurityKey(ecdsa);
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.EcdsaSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: issuer,
+            audience: audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(expirationMinutes),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+    protected async Task<WalletDepositEndpoint> CreateWalletDepositEndpoint(string bearerToken)
+    {
+        using var channel = GrpcChannel.ForAddress(ProjectOriginOptions.Value.WalletUrl);
+
+        var walletServiceClient = new WalletService.WalletServiceClient(channel);
+        var request = new CreateWalletDepositEndpointRequest();
+        var headers = new Metadata
+            { { "Authorization", $"Bearer {bearerToken}" } };
+        var response =
+            await walletServiceClient.CreateWalletDepositEndpointAsync(request, headers);
+
+        return response.WalletDepositEndpoint;
+    }
 }
