89edd334140e8ffbb9ffa0a6f16bffef7d74c787
Morten Munck Hansen <MOQ@energinet.dk>
2023-07-17 13:49:29 +0200

    temp

--- domains/auth/Auth.API/API/Models/Entities/UserTerms.cs
+++ domains/auth/Auth.API/API/Models/Entities/UserTerms.cs
@@ -1,10 +1,16 @@
 namespace API.Models.Entities;
 
+public enum TermsType
+{
+    PrivacyPolicy,
+    TermsOfService
+}
+
 public record UserTerms
 {
     public Guid? Id { get; init; }
     public Guid UserId { get; set; }
-    public string TermsKey { get; set; } = null!;
+    public TermsType Type { get; set; }
     public int AcceptedVersion { get; set; }
 
     public User User { get; set; } = null!;
