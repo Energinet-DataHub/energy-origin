e1cd920abac2e79cc8d5e6dc225e09f9631d6a29
sahma19 <saiuan@msn.com>
2023-07-18 14:28:36 +0200

    Disable swagger again

--- domains/transfer-agreements/TransferAgreements.API/API/Program.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/Program.cs
@@ -97,17 +97,18 @@ builder.Services.AddSwaggerGen(o =>
         Title = "Transfer Agreements API"
     });
 
-
-    o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    if (builder.Environment.IsDevelopment())
     {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
-    });
-    o.AddSecurityRequirement(new OpenApiSecurityRequirement
+        o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+        });
+        o.AddSecurityRequirement(new OpenApiSecurityRequirement
         {
             {
                 new OpenApiSecurityScheme
@@ -121,6 +122,7 @@ builder.Services.AddSwaggerGen(o =>
                 new string[] { }
             }
         });
+    }
 });
 
 builder.Services.AddLogging();
