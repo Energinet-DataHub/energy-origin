8451de749696f54285e8e8d4a02889ad14394d80
sahma19 <saiuan@msn.com>
2023-07-18 14:09:40 +0200

    formatting

--- domains/transfer-agreements/TransferAgreements.API/API/Program.cs
+++ domains/transfer-agreements/TransferAgreements.API/API/Program.cs
@@ -98,16 +98,16 @@ builder.Services.AddSwaggerGen(o =>
     });
 
 
-        o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer",
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
-        });
-        o.AddSecurityRequirement(new OpenApiSecurityRequirement
+    o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+    });
+    o.AddSecurityRequirement(new OpenApiSecurityRequirement
         {
             {
                 new OpenApiSecurityScheme
