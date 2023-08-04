2c1b714350e222ad88cf5da78844fecaadcf5ff0
sahma19 <saiuan@msn.com>
2023-08-04 12:52:03 +0200

    updated migrations script

--- domains/transfer-agreements/chart/API.migrations.sql
+++ domains/transfer-agreements/chart/API.migrations.sql
@@ -167,3 +167,22 @@ BEGIN
 END $EF$;
 COMMIT;
 
+START TRANSACTION;
+
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230719131121_AddReceiverReference') THEN
+    ALTER TABLE "TransferAgreements" ADD "ReceiverReference" uuid NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000';
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20230719131121_AddReceiverReference') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20230719131121_AddReceiverReference', '7.0.5');
+    END IF;
+END $EF$;
+COMMIT;
+
