1c5fc70eeba646b5692a7acf1eb79aa9eedfe2e3
Paul Toft Duizer <paul@duizer.dk>
2023-08-10 13:00:34 +0200

    bump wallet version

--- domains/certificates/Query.API/API.IntegrationTests/Testcontainers/ProjectOriginStack.cs
+++ domains/certificates/Query.API/API.IntegrationTests/Testcontainers/ProjectOriginStack.cs
@@ -35,7 +35,7 @@ public class ProjectOriginStack : RegistryFixture
             var hostPort = ((IPEndPoint)udp.Client.LocalEndPoint!).Port;
 
             return new ContainerBuilder()
-                .WithImage("ghcr.io/project-origin/wallet-server:0.1.1")
+                .WithImage("ghcr.io/project-origin/wallet-server:0.1.3")
                 .WithPortBinding(hostPort, GrpcPort)
                 .WithCommand("--serve", "--migrate")
                 .WithEnvironment("ConnectionStrings__Database", connectionString)
