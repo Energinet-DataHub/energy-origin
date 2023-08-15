5fcaa47256b8b9e4ce0608e806d98c3cf3f76ad2
Paul Toft Duizer <paul@duizer.dk>
2023-08-15 11:47:34 +0200

    Disable concurrency limit

--- domains/certificates/Query.API/API.IntegrationTests/Testcontainers/RegistryFixture.cs
+++ domains/certificates/Query.API/API.IntegrationTests/Testcontainers/RegistryFixture.cs
@@ -12,8 +12,8 @@ namespace API.IntegrationTests.Testcontainers;
 
 public class RegistryFixture : IAsyncLifetime
 {
-    private const string registryImage = "ghcr.io/project-origin/registry-server:0.2.0-rc.17";
-    private const string electricityVerifierImage = "ghcr.io/project-origin/electricity-server:0.2.0-rc.17";
+    private const string registryImage = "ghcr.io/project-origin/registry-server:0.2.0";
+    private const string electricityVerifierImage = "ghcr.io/project-origin/electricity-server:0.2.0";
     protected const int GrpcPort = 80;
     private const string registryName = "TestRegistry";
 
@@ -52,6 +52,7 @@ public class RegistryFixture : IAsyncLifetime
                 .WithEnvironment("RegistryName", registryName)
                 .WithEnvironment("IMMUTABLELOG__TYPE", "log")
                 .WithEnvironment("VERIFIABLEEVENTSTORE__BATCHSIZEEXPONENT", "0")
+                .WithEnvironment("Logging__LogLevel__Default", "Trace")
                 .WithWaitStrategy(
                     Wait.ForUnixContainer()
                         .UntilPortIsAvailable(GrpcPort)
