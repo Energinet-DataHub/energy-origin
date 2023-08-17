eca1964a1d147c71e6d55ada2570917db6051b4e
Paul Toft Duizer <paul@duizer.dk>
2023-08-17 14:40:17 +0200

    remove semaphore

--- domains/certificates/Shared/AggregateRepositories/AggregateRepositoryExtensions.cs
+++ domains/certificates/Shared/AggregateRepositories/AggregateRepositoryExtensions.cs
@@ -9,27 +9,17 @@ namespace AggregateRepositories;
 
 public static class AggregateRepositoryExtensions
 {
-    private static readonly SemaphoreSlim semaphoreSlim = new(1, 1);
-
     public static async Task Save(this IDocumentStore store, AggregateBase aggregate, CancellationToken cancellationToken = default)
     {
-        await semaphoreSlim.WaitAsync(cancellationToken);
-        try
-        {
-            await using var session = store.LightweightSession();
+        await using var session = store.LightweightSession();
 
-            var events = aggregate.GetUncommittedEvents().ToArray();
+        var events = aggregate.GetUncommittedEvents().ToArray();
 
-            session.Events.Append(aggregate.Id, aggregate.Version, events);
+        session.Events.Append(aggregate.Id, aggregate.Version, events);
 
-            await session.SaveChangesAsync(cancellationToken);
+        await session.SaveChangesAsync(cancellationToken);
 
-            aggregate.ClearUncommittedEvents();
-        }
-        finally
-        {
-            semaphoreSlim.Release();
-        }
+        aggregate.ClearUncommittedEvents();
     }
 
     public static async Task<T?> Get<T>(this IDocumentStore store, Guid id, int? version = null, CancellationToken cancellationToken = default) where T : AggregateBase
