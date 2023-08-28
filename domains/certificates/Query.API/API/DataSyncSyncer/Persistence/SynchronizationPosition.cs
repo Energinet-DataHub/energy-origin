beb1f4efd464e0dcc4a9dd64e2366e9b34a60ba1
Paul Toft Duizer <paul@duizer.dk>
2023-08-28 10:55:59 +0200

    bump

---
+++ domains/certificates/Query.API/API/DataSyncSyncer/Persistence/SynchronizationPosition.cs
@@ -0,0 +1,10 @@
+using Marten.Schema;
+
+namespace API.DataSyncSyncer.Persistence;
+
+public class SynchronizationPosition
+{
+    [Identity]
+    public string GSRN { get; set; }
+    public long SyncedTo { get; set; }
+}
