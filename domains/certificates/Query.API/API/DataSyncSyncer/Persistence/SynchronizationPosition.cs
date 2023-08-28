7ad9069a9ca49a305c4ae0c19bedd6089f92fb4c
Paul Toft Duizer <paul@duizer.dk>
2023-08-28 11:06:13 +0200

    remove code smell

--- domains/certificates/Query.API/API/DataSyncSyncer/Persistence/SynchronizationPosition.cs
+++ domains/certificates/Query.API/API/DataSyncSyncer/Persistence/SynchronizationPosition.cs
@@ -5,6 +5,6 @@ namespace API.DataSyncSyncer.Persistence;
 public class SynchronizationPosition
 {
     [Identity]
-    public string GSRN { get; set; }
+    public string GSRN { get; set; } = "";
     public long SyncedTo { get; set; }
 }
