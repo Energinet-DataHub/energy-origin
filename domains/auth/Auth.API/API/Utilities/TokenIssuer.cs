4b20d77e02a95081a7e9e938c5e032812bf370dd
CodingDegen <afy@energinet.dk>
2023-09-01 15:04:01 +0200

    temp

--- domains/auth/Auth.API/API/Utilities/TokenIssuer.cs
+++ domains/auth/Auth.API/API/Utilities/TokenIssuer.cs
@@ -15,7 +15,8 @@ namespace API.Utilities;
 
 public class TokenIssuer : ITokenIssuer
 {
-    public const string AllAcceptedScopes = $"{UserScopeName.Dashboard} {UserScopeName.Production} {UserScopeName.Meters} {UserScopeName.Certificates}";
+    public const string AllAcceptedScopes =
+        $"{UserScopeName.Dashboard} {UserScopeName.Production} {UserScopeName.Meters} {UserScopeName.Certificates}";
 
     private readonly TermsOptions termsOptions;
     private readonly TokenOptions tokenOptions;
@@ -28,13 +29,15 @@ public class TokenIssuer : ITokenIssuer
         this.roleOptions = roleOptions;
     }
 
-    public string Issue(UserDescriptor descriptor, UserData data, bool versionBypass = false, DateTime? issueAt = default)
+    public string Issue(UserDescriptor descriptor, UserData data, bool versionBypass = false,
+        DateTime? issueAt = default)
     {
         var credentials = CreateSigningCredentials(tokenOptions);
 
         var state = ResolveState(termsOptions, data, versionBypass);
 
-        return CreateToken(CreateTokenDescriptor(tokenOptions, roleOptions, credentials, descriptor, data, state, issueAt ?? DateTime.UtcNow));
+        return CreateToken(CreateTokenDescriptor(tokenOptions, roleOptions, credentials, descriptor, data, state,
+            issueAt ?? DateTime.UtcNow));
     }
 
     private static SigningCredentials CreateSigningCredentials(TokenOptions options)
@@ -49,20 +52,31 @@ public class TokenIssuer : ITokenIssuer
 
     private static UserState ResolveState(TermsOptions options, UserData data, bool versionBypass)
     {
-        string? scope = null;
-        if (options.PrivacyPolicyVersion != data.PrivacyPolicyVersion)
-        {
-            scope = string.Join(" ", scope, UserScopeName.NotAcceptedPrivacyPolicy);
-        }
-
+        var scope = HandleNotAcceptedScope(options, data);
         scope = versionBypass ? AllAcceptedScopes : scope ?? AllAcceptedScopes;
-
         scope = scope.Trim();
 
         return new(scope);
     }
 
-    private static SecurityTokenDescriptor CreateTokenDescriptor(TokenOptions tokenOptions, RoleOptions roleOptions, SigningCredentials credentials, UserDescriptor descriptor, UserData data, UserState state, DateTime issueAt)
+    private static string? HandleNotAcceptedScope(TermsOptions options, UserData data)
+    {
+        string? scope = null;
+        if (options.PrivacyPolicyVersion != data.PrivacyPolicyVersion)
+        {
+            return string.Join(" ", scope, UserScopeName.NotAcceptedPrivacyPolicy);
+        }
+        if (options.TermsOfServiceVersion == data.TermsOfServiceVersion) return null;
+
+        if (data.AssignedRoles != null && data.AssignedRoles.Contains(RoleKey.OrganizationAdmin))
+        {
+            return string.Join(" ", scope, UserScopeName.NotAcceptedTermsOfServiceOrganizationAdmin);
+        }
+        return string.Join(" ", scope, UserScopeName.NotAcceptedTermsOfService);
+    }
+
+    private static SecurityTokenDescriptor CreateTokenDescriptor(TokenOptions tokenOptions, RoleOptions roleOptions,
+        SigningCredentials credentials, UserDescriptor descriptor, UserData data, UserState state, DateTime issueAt)
     {
         var claims = new Dictionary<string, object>
         {
@@ -82,7 +96,9 @@ public class TokenIssuer : ITokenIssuer
         var assignedRoles = data.AssignedRoles ?? Array.Empty<string>();
         var matchedRoles = descriptor.MatchedRoles.Split(" ") ?? Array.Empty<string>();
         var allottedRoles = assignedRoles.Concat(matchedRoles).Distinct().Where(x => !x.IsNullOrEmpty());
-        var roles = AddInheritedRoles(roleOptions.RoleConfigurations.ToDictionary(x => x.Key), allottedRoles).Distinct().Where(x => validRoles.Contains(x));
+        var roles = AddInheritedRoles(roleOptions.RoleConfigurations.ToDictionary(x => x.Key), allottedRoles)
+            .Distinct()
+            .Where(x => validRoles.Contains(x));
 
         claims.Add(UserClaimName.Roles, roles);
 
@@ -117,13 +133,15 @@ public class TokenIssuer : ITokenIssuer
         };
     }
 
-    private static IEnumerable<string> AddInheritedRoles(Dictionary<string, RoleConfiguration> configurations, IEnumerable<string> roles)
+    private static IEnumerable<string> AddInheritedRoles(Dictionary<string, RoleConfiguration> configurations,
+        IEnumerable<string> roles)
     {
         if (roles.IsNullOrEmpty())
         {
             return Enumerable.Empty<string>();
         }
-        var inherited = roles.Where(x => configurations[x].Inherits.IsNullOrEmpty() == false).SelectMany(x => configurations[x].Inherits);
+        var inherited = roles.Where(x => configurations[x].Inherits.IsNullOrEmpty() == false)
+            .SelectMany(x => configurations[x].Inherits);
         return roles.Concat(inherited).Concat(AddInheritedRoles(configurations, inherited));
     }
 
@@ -134,11 +152,13 @@ public class TokenIssuer : ITokenIssuer
         return handler.WriteToken(token);
     }
 
-    public record UserData(int PrivacyPolicyVersion, int TermsOfServiceVersion, IEnumerable<string>? AssignedRoles = default)
+    public record UserData(int PrivacyPolicyVersion, int TermsOfServiceVersion,
+        IEnumerable<string>? AssignedRoles = default)
     {
         public static UserData From(User? user) => new(
             user?.UserTerms.SingleOrDefault(x => x.Type == UserTermsType.PrivacyPolicy)?.AcceptedVersion ?? 0,
-            user?.Company?.CompanyTerms.SingleOrDefault(x => x.Type == CompanyTermsType.TermsOfService)?.AcceptedVersion ?? 0,
+            user?.Company?.CompanyTerms.SingleOrDefault(x => x.Type == CompanyTermsType.TermsOfService)
+                ?.AcceptedVersion ?? 0,
             user?.UserRoles.Select(x => x.Role)
         );
     }
