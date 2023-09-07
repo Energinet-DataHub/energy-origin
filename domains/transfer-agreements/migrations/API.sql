7f83d856283e43b93cccfcdd425d6476370dbb35
Paul Toft Duizer <paul@duizer.dk>
2023-09-07 10:18:24 +0200

    Change to utf-8 and lf

--- domains/transfer-agreements/migrations/API.sql
+++ domains/transfer-agreements/migrations/API.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
     "MigrationId" character varying(150) NOT NULL,
     "ProductVersion" character varying(32) NOT NULL,
     CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
