8800f76bea50ee3e0431eb4d5502086fa22beac5
Simon Due <simon.99@live.dk>
2023-09-08 11:42:33 +0200

    typo

--- domains/auth/Auth.API/API/Values/UserScopeName.cs
+++ domains/auth/Auth.API/API/Values/UserScopeName.cs
@@ -2,7 +2,7 @@ namespace API.Values;
 
 public struct UserScopeName
 {
-    public const string NotAcceptedTermsOfServiceOrganizationAdmin = "not-accepted-termsofservice-terms-organisation-admin";
+    public const string NotAcceptedTermsOfServiceOrganizationAdmin = "not-accepted-termsofservice-terms-organization-admin";
     public const string NotAcceptedTermsOfService = "not-accepted-termsofservice-terms";
     public const string NotAcceptedPrivacyPolicy = "not-accepted-privacypolicy-terms";
     public const string Dashboard = "dashboard";
