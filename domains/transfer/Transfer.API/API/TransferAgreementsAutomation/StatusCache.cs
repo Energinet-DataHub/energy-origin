784396877912041fd78f3ec0f1ce7a2ca225c8cf
Christoffer Frydkjær <christofferfk@gmail.com>
2023-09-14 15:40:26 +0200

    remove unused imports

--- domains/transfer/Transfer.API/API/TransferAgreementsAutomation/StatusCache.cs
+++ domains/transfer/Transfer.API/API/TransferAgreementsAutomation/StatusCache.cs
@@ -1,11 +1,10 @@
-using System.Reflection.Metadata;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace API.TransferAgreementsAutomation;
 
 public class StatusCache
 {
-    public MemoryCache Cache { get; } = new(
+    public MemoryCache Cache { get; } = new (
         new MemoryCacheOptions
         {
             SizeLimit = 1
