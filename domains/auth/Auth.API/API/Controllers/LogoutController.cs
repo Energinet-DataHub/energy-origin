3b3c0370a9930dbba18f0391ed4b2550993a0413
CodingDegen <afy@energinet.dk>
2023-09-18 10:12:46 +0200

    .

--- domains/auth/Auth.API/API/Controllers/LogoutController.cs
+++ domains/auth/Auth.API/API/Controllers/LogoutController.cs
@@ -10,57 +10,11 @@ namespace API.Controllers;
 [ApiController]
 public class LogoutController : ControllerBase
 {
-    // [HttpGet()]
-    // [Route("auth/logout")]
-    // public async Task<IActionResult> LogoutAsync(IMetrics metrics, IDiscoveryCache discoveryCache, ICryptography cryptography, OidcOptions oidcOptions, ILogger<LogoutController> logger, [FromQuery] string? overrideRedirectionUri = default)
-    // {
-    //     var redirectionUri = oidcOptions.FrontendRedirectUri.AbsoluteUri;
-    //     if (oidcOptions.AllowRedirection && overrideRedirectionUri != null)
-    //     {
-    //         redirectionUri = overrideRedirectionUri;
-    //     }
-    //
-    //     var discoveryDocument = await discoveryCache.GetAsync();
-    //     if (discoveryDocument == null || discoveryDocument.IsError)
-    //     {
-    //         logger.LogError("Unable to fetch discovery document: {Error}", discoveryDocument?.Error);
-    //         return RedirectPreserveMethod(redirectionUri);
-    //     }
-    //
-    //     var requestUrl = new RequestUrl(discoveryDocument.EndSessionEndpoint);
-    //
-    //     DecodableUserDescriptor descriptor;
-    //     try
-    //     {
-    //         descriptor = new DecodableUserDescriptor(User, cryptography);
-    //     }
-    //     catch
-    //     {
-    //         return RedirectPreserveMethod(redirectionUri);
-    //     }
-    //
-    //     var url = requestUrl.CreateEndSessionUrl(
-    //         idTokenHint: descriptor.IdentityToken,
-    //         postLogoutRedirectUri: redirectionUri
-    //     );
-    //
-    //
-    //     logger.AuditLog(
-    //         "{User} logged off for {Subject} at {TimeStamp}.",
-    //         descriptor.Id,
-    //         descriptor.Subject,
-    //         DateTimeOffset.Now.ToUnixTimeSeconds()
-    //     );
-    //
-    //     metrics.Logout(descriptor.Id, descriptor.Organization?.Id, descriptor.ProviderType);
-    //
-    //     return RedirectPreserveMethod(url);
-    // }
-
-
-    [HttpGet()]
+    [HttpGet]
     [Route("auth/logout")]
-    public async Task<IActionResult> LogoutAsync(IMetrics metrics, IDiscoveryCache discoveryCache, ICryptography cryptography, OidcOptions oidcOptions, ILogger<LogoutController> logger, [FromQuery] string? overrideRedirectionUri = default)
+    public async Task<IActionResult> LogoutAsync(IMetrics metrics, IDiscoveryCache discoveryCache,
+        ICryptography cryptography, OidcOptions oidcOptions, ILogger<LogoutController> logger,
+        [FromQuery] string? overrideRedirectionUri = default)
     {
         var redirectionUri = oidcOptions.FrontendRedirectUri.AbsoluteUri;
         if (oidcOptions.AllowRedirection && overrideRedirectionUri != null)
@@ -87,27 +41,22 @@ public class LogoutController : ControllerBase
 
         var client = new HttpClient();
         client.BaseAddress = new Uri(discoveryDocument.EndSessionEndpoint);
-
         var response = await client.GetAsync($"?id_token_hint={descriptor.IdentityToken}");
 
-        if (response.IsSuccessStatusCode)
-        {
-            logger.AuditLog(
-                "{User} logged off for {Subject} at {TimeStamp}.",
-                descriptor.Id,
-                descriptor.Subject,
-                DateTimeOffset.Now.ToUnixTimeSeconds()
-            );
-
-            metrics.Logout(descriptor.Id, descriptor.Organization?.Id, descriptor.ProviderType);
-
-            return Ok("Logout successful");
-        }
-        else
+        if (!response.IsSuccessStatusCode)
         {
             logger.LogError("Logout failed: {Error}", response.ReasonPhrase);
-            return BadRequest("Logout failed");
+            return BadRequest(redirectionUri);
         }
-    }
 
+        logger.AuditLog(
+            "{User} logged off for {Subject} at {TimeStamp}.",
+            descriptor.Id,
+            descriptor.Subject,
+            DateTimeOffset.Now.ToUnixTimeSeconds()
+        );
+
+        metrics.Logout(descriptor.Id, descriptor.Organization?.Id, descriptor.ProviderType);
+        return Ok(redirectionUri);
+    }
 }
