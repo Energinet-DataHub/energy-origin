72e06d596f930be4608ed2fdd84e872216a1d9b1
sahma19 <saiuan@msn.com>
2023-09-18 12:42:10 +0200

    revert change

--- domains/transfer/Transfer.API/API/Data/ApplicationDbContext.cs
+++ domains/transfer/Transfer.API/API/Data/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 using API.Models;
 using Audit.EntityFramework;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace API.Data;
 
@@ -16,13 +15,6 @@ public class ApplicationDbContext : AuditDbContext
     public DbSet<TransferAgreementHistoryEntry> TransferAgreementHistoryEntries { get; set; }
     public DbSet<ConnectionInvitation> ConnectionInvitations { get; set; }
     public DbSet<Connection> Connections { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseLoggerFactory(loggerFactory);
-        base.OnConfiguring(optionsBuilder);
-    }
-    ILoggerFactory loggerFactory = new LoggerFactory();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
