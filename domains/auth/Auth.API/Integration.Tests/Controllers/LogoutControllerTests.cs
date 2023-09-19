ceda060d3035d48b4030b574b0a9117f0838a51d
CodingDegen <afy@energinet.dk>
2023-09-19 09:10:41 +0200

    .

--- domains/auth/Auth.API/Integration.Tests/Controllers/LogoutControllerTests.cs
+++ domains/auth/Auth.API/Integration.Tests/Controllers/LogoutControllerTests.cs
@@ -13,32 +13,32 @@ public class LogoutControllerTests : IClassFixture<AuthWebApplicationFactory>
     private readonly AuthWebApplicationFactory factory;
     public LogoutControllerTests(AuthWebApplicationFactory factory) => this.factory = factory;
 
-    // [Fact]
-    // public async Task LogoutAsync_ShouldReturnRedirectToAuthority_WhenInvoked()
-    // {
-    //     var server = WireMockServer.Start().MockConfigEndpoint().MockJwksEndpoint();
-    //
-    //     var identityToken = Guid.NewGuid().ToString();
-    //     var user = await factory.AddUserToDatabaseAsync();
-    //     var oidcOptions = new OidcOptions()
-    //     {
-    //         AuthorityUri = new Uri($"http://localhost:{server.Port}/op"),
-    //         FrontendRedirectUri = new Uri("https://example.com")
-    //     };
-    //
-    //     var client = factory.CreateAuthenticatedClient(user, identityToken: identityToken, config: builder => builder.ConfigureTestServices(services => services.AddScoped(_ => oidcOptions)));
-    //
-    //     var result = await client.GetAsync("auth/logout");
-    //     Assert.NotNull(result);
-    //
-    //     var query = HttpUtility.UrlDecode(result.Headers.Location?.AbsoluteUri);
-    //     Assert.Contains($"post_logout_redirect_uri={oidcOptions.FrontendRedirectUri.AbsoluteUri}", query);
-    //     Assert.Contains($"id_token_hint={identityToken}", query);
-    //
-    //     var uri = new Uri(query!);
-    //     Assert.Equal(oidcOptions.AuthorityUri.Host, uri.Host);
-    //     Assert.Equal(HttpStatusCode.TemporaryRedirect, result.StatusCode);
-    // }
+    [Fact]
+    public async Task LogoutAsync_ShouldReturnRedirectToAuthority_WhenInvoked()
+    {
+        var server = WireMockServer.Start().MockConfigEndpoint().MockJwksEndpoint();
+
+        var identityToken = Guid.NewGuid().ToString();
+        var user = await factory.AddUserToDatabaseAsync();
+        var oidcOptions = new OidcOptions()
+        {
+            AuthorityUri = new Uri($"http://localhost:{server.Port}/op"),
+            FrontendRedirectUri = new Uri("https://example.com")
+        };
+
+        var client = factory.CreateAuthenticatedClient(user, identityToken: identityToken, config: builder => builder.ConfigureTestServices(services => services.AddScoped(_ => oidcOptions)));
+
+        var result = await client.GetAsync("auth/logout");
+        Assert.NotNull(result);
+
+        var query = HttpUtility.UrlDecode(result.Headers.Location?.AbsoluteUri);
+        Assert.Contains($"post_logout_redirect_uri={oidcOptions.FrontendRedirectUri.AbsoluteUri}", query);
+        Assert.Contains($"id_token_hint={identityToken}", query);
+
+        var uri = new Uri(query!);
+        Assert.Equal(oidcOptions.AuthorityUri.Host, uri.Host);
+        Assert.Equal(HttpStatusCode.TemporaryRedirect, result.StatusCode);
+    }
 
     [Fact]
     public async Task LogoutAsync_ShouldReturnErrorCodeUrl_WhenDiscoveryCacheFails()
