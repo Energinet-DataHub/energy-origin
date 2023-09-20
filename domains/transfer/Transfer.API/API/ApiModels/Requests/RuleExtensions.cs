4cbfc53ff8dc795c7159d4a9230c8bd95a2c4a48
sahma19 <saiuan@msn.com>
2023-09-20 13:26:13 +0200

    (Refactor) PR Comments

--- domains/transfer/Transfer.API/API/ApiModels/Requests/RuleExtensions.cs
+++ domains/transfer/Transfer.API/API/ApiModels/Requests/RuleExtensions.cs
@@ -15,17 +15,18 @@ public static class RuleExtensions
             .LessThan(253402300800)
             .WithMessage("{PropertyName} too high! Please make sure the format is UTC in seconds.");
 
-    public static IRuleBuilderOptions<T, string> MustBeValidBase64<T>(this IRuleBuilder<T, string> ruleBuilder)
-            => ruleBuilder
-                .Must(IsValidBase64String)
-                .WithMessage("Base64-encoded Wallet Deposit Endpoint is not valid");
+    public static IRuleBuilderOptions<T, string> MustBeValidBase64<T>(this IRuleBuilder<T, string> ruleBuilder,
+        string message = "String is not Base64")
+        => ruleBuilder
+            .Must(IsValidBase64String)
+            .WithMessage(message);
 
     private static bool IsValidBase64String(string base64)
     {
         if (string.IsNullOrEmpty(base64))
             return false;
 
-        Span<byte> buffer = new Span<byte>(new byte[base64.Length]);
+        var buffer = new Span<byte>(new byte[base64.Length]);
         return Convert.TryFromBase64String(base64, buffer, out _);
     }
 }
