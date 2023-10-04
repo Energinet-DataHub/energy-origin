210a5e44783250dc47897e01285307aecfd6c760
Paul Toft Duizer <paul@duizer.dk>
2023-10-04 09:29:13 +0200

    format

--- domains/auth/Auth.API/API/Controllers/TermsController.cs
+++ domains/auth/Auth.API/API/Controllers/TermsController.cs
@@ -67,7 +67,7 @@ public class TermsController : ControllerBase
                     Console.WriteLine(e);
                     throw;
                 }
-                
+
             }
         }
 
