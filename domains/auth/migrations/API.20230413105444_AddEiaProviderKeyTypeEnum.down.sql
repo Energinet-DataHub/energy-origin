a14f986aad71aef7a059dea9a373f29f39942655
Jakob Jensen <jakobj@jakobj.dk>
2023-10-11 14:44:13 +0200

    Try with windows

--- domains/auth/migrations/API.20230413105444_AddEiaProviderKeyTypeEnum.down.sql
+++ domains/auth/migrations/API.20230413105444_AddEiaProviderKeyTypeEnum.down.sql
@@ -1,12 +1,8 @@
-﻿    CREATE TYPE provider_key_type_x AS ENUM ('pid', 'rid', 'mit_id_uuid');
-    ALTER TABLE "UserProviders" ADD COLUMN "ProviderKeyTypeX" provider_key_type_x DEFAULT('pid');
-    DELETE FROM "UserProviders" WHERE "ProviderKeyType" = 'eia';
-    UPDATE "UserProviders" SET "ProviderKeyTypeX" = "ProviderKeyType"::text::provider_key_type_x;
-    ALTER TABLE "UserProviders" ALTER COLUMN "ProviderKeyTypeX" DROP DEFAULT;
-    ALTER TABLE "UserProviders" ALTER COLUMN "ProviderKeyTypeX" SET NOT NULL;
-    ALTER TABLE "UserProviders" DROP COLUMN "ProviderKeyType";
-    ALTER TABLE "UserProviders" RENAME COLUMN "ProviderKeyTypeX" TO "ProviderKeyType";
+﻿    DELETE FROM "UserProviders" WHERE "ProviderKeyType" = 'eia';
+    ALTER TABLE "UserProviders" ALTER COLUMN "ProviderKeyType" TYPE varchar(255);
     DROP TYPE provider_key_type;
+    CREATE TYPE provider_key_type AS ENUM ('pid', 'rid', 'mit_id_uuid');
+    ALTER TABLE "UserProviders" ALTER COLUMN "ProviderKeyType" TYPE provider_key_type USING "ProviderKeyType"::provider_key_type;
 
 DELETE FROM "__EFMigrationsHistory"
 WHERE "MigrationId" = '20230413105444_AddEiaProviderKeyTypeEnum';
