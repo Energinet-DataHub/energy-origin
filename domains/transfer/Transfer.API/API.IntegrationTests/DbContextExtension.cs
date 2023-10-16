013f5cc0199ab8a79b77faa79f58dff3826d2a67
Theis Nickelsen <130448180+tnickelsen@users.noreply.github.com>
2023-10-16 13:22:48 +0200

    1886 Added unit tests for latest migration  (#456)
    Co-authored-by: Paul Toft Duizer <paul@duizer.dk>

--- domains/transfer/Transfer.API/API.IntegrationTests/DbContextExtension.cs
+++ domains/transfer/Transfer.API/API.IntegrationTests/DbContextExtension.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
+using API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.IntegrationTests;
@@ -27,5 +29,14 @@ public static class DbContextExtension
         } while (stopwatch.Elapsed < limit);
 
         throw new Exception($"Entity not found within the time limit ({limit.TotalSeconds} seconds)");
+    }
+
+    public static async Task TruncateTransferAgreementsTables(this ApplicationDbContext dbContext)
+    {
+        var agreementsTable = dbContext.Model.FindEntityType(typeof(TransferAgreement))!.GetTableName();
+        var historyTable = dbContext.Model.FindEntityType(typeof(TransferAgreementHistoryEntry))!.GetTableName();
+
+        await dbContext.Database.ExecuteSqlRawAsync($"TRUNCATE TABLE \"{agreementsTable}\" CASCADE");
+        await dbContext.Database.ExecuteSqlRawAsync($"TRUNCATE TABLE \"{historyTable}\"");
     }
 }
