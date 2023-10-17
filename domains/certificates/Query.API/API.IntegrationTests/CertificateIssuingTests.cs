c39a738fac637feb0d1bca0e396aacde48b0417e
tnickelsen <TNE@energinet.dk>
2023-10-17 13:21:10 +0200

    Added migrations for consumption certificates.

--- domains/certificates/Query.API/API.IntegrationTests/CertificateIssuingTests.cs
+++ domains/certificates/Query.API/API.IntegrationTests/CertificateIssuingTests.cs
@@ -80,6 +80,51 @@ public sealed class CertificateIssuingTests :
 
         await factory.AddContract(subject, gsrn, utcMidnight, MeteringPointType.Production, dataSyncWireMock);
 
+        var measurement = new EnergyMeasuredIntegrationEvent(
+            GSRN: gsrn,
+            DateFrom: utcMidnight.ToUnixTimeSeconds(),
+            DateTo: utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured);
+
+        await factory.GetMassTransitBus().Publish(measurement);
+
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        var certificateList =
+            await client.RepeatedlyGetUntil<CertificateList>("api/certificates", res => res.Result.Any());
+
+        var expected = new CertificateList
+        {
+            Result = new[]
+            {
+                new Certificate
+                {
+                    DateFrom = utcMidnight.ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    Quantity = 42,
+                    FuelCode = "F00000000",
+                    TechCode = "T070000",
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                }
+            }
+        };
+
+        certificateList.Should().BeEquivalentTo(expected, CertificateListAssertionOptions);
+    }
+
+    [Fact]
+    public async Task GetList_ProductionMeasurementAddedToBus_ReturnsList()
+    {
+        var subject = Guid.NewGuid().ToString();
+        var gsrn = GsrnHelper.GenerateRandom();
+
+        var now = DateTimeOffset.UtcNow;
+        var utcMidnight = now.Subtract(now.TimeOfDay);
+
+        await factory.AddContract(subject, gsrn, utcMidnight, MeteringPointType.Production, dataSyncWireMock);
+
         var measurement = new ProductionEnergyMeasuredIntegrationEvent(
             GSRN: gsrn,
             DateFrom: utcMidnight.ToUnixTimeSeconds(),
@@ -115,7 +160,105 @@ public sealed class CertificateIssuingTests :
     }
 
     [Fact]
+    public async Task GetList_ConsumptionMeasurementAddedToBus_ReturnsList()
+    {
+        var subject = Guid.NewGuid().ToString();
+        var gsrn = GsrnHelper.GenerateRandom();
+
+        var now = DateTimeOffset.UtcNow;
+        var utcMidnight = now.Subtract(now.TimeOfDay);
+
+        await factory.AddContract(subject, gsrn, utcMidnight, MeteringPointType.Consumption, dataSyncWireMock);
+
+        var measurement = new ConsumptionEnergyMeasuredIntegrationEvent(
+            GSRN: gsrn,
+            DateFrom: utcMidnight.ToUnixTimeSeconds(),
+            DateTo: utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured);
+
+        await factory.GetMassTransitBus().Publish(measurement);
+
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        var certificateList =
+            await client.RepeatedlyGetUntil<CertificateList>("api/certificates", res => res.Result.Any());
+
+        var expected = new CertificateList
+        {
+            Result = new[]
+            {
+                new Certificate
+                {
+                    DateFrom = utcMidnight.ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    Quantity = 42,
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                }
+            }
+        };
+
+        certificateList.Should().BeEquivalentTo(expected, CertificateListAssertionOptions);
+    }
+
+    [Fact]
     public async Task GetList_SameMeasurementAddedTwice_ReturnsList()
+    {
+        var subject = Guid.NewGuid().ToString();
+        var gsrn = GsrnHelper.GenerateRandom();
+
+        var now = DateTimeOffset.UtcNow;
+        var utcMidnight = now.Subtract(now.TimeOfDay);
+
+        await factory.AddContract(subject, gsrn, utcMidnight, MeteringPointType.Production, dataSyncWireMock);
+
+        var dateFrom = utcMidnight.ToUnixTimeSeconds();
+        var dateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds();
+
+        var measurement1 = new EnergyMeasuredIntegrationEvent(
+            GSRN: gsrn,
+            DateFrom: dateFrom,
+            DateTo: dateTo,
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured);
+
+        var measurement2 = new EnergyMeasuredIntegrationEvent(
+            GSRN: gsrn,
+            DateFrom: dateFrom,
+            DateTo: dateTo,
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured);
+
+        await factory.GetMassTransitBus().PublishBatch(new[] { measurement1, measurement2 });
+
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        var certificateList =
+            await client.RepeatedlyGetUntil<CertificateList>("api/certificates", res => res.Result.Any());
+
+        var expected = new CertificateList
+        {
+            Result = new[]
+            {
+                new Certificate
+                {
+                    DateFrom = utcMidnight.ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    Quantity = 42,
+                    FuelCode = "F00000000",
+                    TechCode = "T070000",
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                }
+            }
+        };
+
+        certificateList.Should().BeEquivalentTo(expected, CertificateListAssertionOptions);
+    }
+
+    [Fact]
+    public async Task GetList_SameProductionMeasurementAddedTwice_ReturnsList()
     {
         var subject = Guid.NewGuid().ToString();
         var gsrn = GsrnHelper.GenerateRandom();
@@ -170,7 +313,150 @@ public sealed class CertificateIssuingTests :
     }
 
     [Fact]
-    public async Task GetList_FiveMeasurementAddedToBus_ReturnsList()
+    public async Task GetList_SameConsumptionMeasurementAddedTwice_ReturnsList()
+    {
+        var subject = Guid.NewGuid().ToString();
+        var gsrn = GsrnHelper.GenerateRandom();
+
+        var now = DateTimeOffset.UtcNow;
+        var utcMidnight = now.Subtract(now.TimeOfDay);
+
+        await factory.AddContract(subject, gsrn, utcMidnight, MeteringPointType.Consumption, dataSyncWireMock);
+
+        var dateFrom = utcMidnight.ToUnixTimeSeconds();
+        var dateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds();
+
+        var measurement1 = new ConsumptionEnergyMeasuredIntegrationEvent(
+            GSRN: gsrn,
+            DateFrom: dateFrom,
+            DateTo: dateTo,
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured);
+
+        var measurement2 = new ConsumptionEnergyMeasuredIntegrationEvent(
+            GSRN: gsrn,
+            DateFrom: dateFrom,
+            DateTo: dateTo,
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured);
+
+        await factory.GetMassTransitBus().PublishBatch(new[] { measurement1, measurement2 });
+
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        var certificateList =
+            await client.RepeatedlyGetUntil<CertificateList>("api/certificates", res => res.Result.Any());
+
+        var expected = new CertificateList
+        {
+            Result = new[]
+            {
+                new Certificate
+                {
+                    DateFrom = utcMidnight.ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    Quantity = 42,
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                }
+            }
+        };
+
+        certificateList.Should().BeEquivalentTo(expected, CertificateListAssertionOptions);
+    }
+
+    [Fact]
+    public async Task GetList_FiveMeasurementsAddedToBus_ReturnsList()
+    {
+        var subject = Guid.NewGuid().ToString();
+        var gsrn = GsrnHelper.GenerateRandom();
+
+        var now = DateTimeOffset.UtcNow;
+        var utcMidnight = now.Subtract(now.TimeOfDay);
+
+        await factory.AddContract(subject, gsrn, utcMidnight, MeteringPointType.Production, dataSyncWireMock);
+
+        const int measurementCount = 5;
+
+        var measurements = Enumerable.Range(0, measurementCount)
+            .Select(i => new EnergyMeasuredIntegrationEvent(
+                GSRN: gsrn,
+                DateFrom: utcMidnight.AddHours(i).ToUnixTimeSeconds(),
+                DateTo: utcMidnight.AddHours(i + 1).ToUnixTimeSeconds(),
+                Quantity: 42 + i,
+                Quality: MeasurementQuality.Measured))
+            .ToArray();
+
+        await factory.GetMassTransitBus().PublishBatch(measurements);
+
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        var certificateList =
+            await client.RepeatedlyGetUntil<CertificateList>("api/certificates",
+                res => res.Result.Count() == measurementCount);
+
+        var expected = new CertificateList
+        {
+            Result = new[]
+            {
+                new Certificate
+                {
+                    Quantity = 46,
+                    DateFrom = utcMidnight.AddHours(4).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(5).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn,
+                    FuelCode = "F00000000",
+                    TechCode = "T070000"
+                },
+                new Certificate
+                {
+                    Quantity = 45,
+                    DateFrom = utcMidnight.AddHours(3).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(4).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn,
+                    FuelCode = "F00000000",
+                    TechCode = "T070000"
+                },
+                new Certificate
+                {
+                    Quantity = 44,
+                    DateFrom = utcMidnight.AddHours(2).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(3).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn,
+                    FuelCode = "F00000000",
+                    TechCode = "T070000"
+                },
+                new Certificate
+                {
+                    Quantity = 43,
+                    DateFrom = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(2).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn,
+                    FuelCode = "F00000000",
+                    TechCode = "T070000"
+                },
+                new Certificate
+                {
+                    Quantity = 42,
+                    DateFrom = utcMidnight.ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn,
+                    FuelCode = "F00000000",
+                    TechCode = "T070000"
+                }
+            }
+        };
+
+        certificateList.Should().BeEquivalentTo(expected, CertificateListAssertionOptions);
+    }
+
+    [Fact]
+    public async Task GetList_FiveProductionMeasurementsAddedToBus_ReturnsList()
     {
         var subject = Guid.NewGuid().ToString();
         var gsrn = GsrnHelper.GenerateRandom();
@@ -252,6 +538,86 @@ public sealed class CertificateIssuingTests :
                     GSRN = gsrn,
                     FuelCode = "F00000000",
                     TechCode = "T070000"
+                }
+            }
+        };
+
+        certificateList.Should().BeEquivalentTo(expected, CertificateListAssertionOptions);
+    }
+
+    [Fact]
+    public async Task GetList_FiveConsumptionMeasurementsAddedToBus_ReturnsList()
+    {
+        var subject = Guid.NewGuid().ToString();
+        var gsrn = GsrnHelper.GenerateRandom();
+
+        var now = DateTimeOffset.UtcNow;
+        var utcMidnight = now.Subtract(now.TimeOfDay);
+
+        await factory.AddContract(subject, gsrn, utcMidnight, MeteringPointType.Consumption, dataSyncWireMock);
+
+        const int measurementCount = 5;
+
+        var measurements = Enumerable.Range(0, measurementCount)
+            .Select(i => new ConsumptionEnergyMeasuredIntegrationEvent(
+                GSRN: gsrn,
+                DateFrom: utcMidnight.AddHours(i).ToUnixTimeSeconds(),
+                DateTo: utcMidnight.AddHours(i + 1).ToUnixTimeSeconds(),
+                Quantity: 42 + i,
+                Quality: MeasurementQuality.Measured))
+            .ToArray();
+
+        await factory.GetMassTransitBus().PublishBatch(measurements);
+
+        using var client = factory.CreateAuthenticatedClient(subject);
+
+        var certificateList =
+            await client.RepeatedlyGetUntil<CertificateList>("api/certificates",
+                res => res.Result.Count() == measurementCount);
+
+        var expected = new CertificateList
+        {
+            Result = new[]
+            {
+                new Certificate
+                {
+                    Quantity = 46,
+                    DateFrom = utcMidnight.AddHours(4).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(5).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                },
+                new Certificate
+                {
+                    Quantity = 45,
+                    DateFrom = utcMidnight.AddHours(3).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(4).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                },
+                new Certificate
+                {
+                    Quantity = 44,
+                    DateFrom = utcMidnight.AddHours(2).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(3).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                },
+                new Certificate
+                {
+                    Quantity = 43,
+                    DateFrom = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(2).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn
+                },
+                new Certificate
+                {
+                    Quantity = 42,
+                    DateFrom = utcMidnight.ToUnixTimeSeconds(),
+                    DateTo = utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+                    GridArea = "DK1",
+                    GSRN = gsrn
                 }
             }
         };
