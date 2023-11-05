04f7d7d0fcbc577bd60196ed3ba73831af785604
Paul Toft Duizer <paul@duizer.dk>
2023-11-05 09:31:34 +0100

    review

--- domains/certificates/Query.API/API/ContractService/Repositories/CertificateIssuingContractRepository.cs
+++ domains/certificates/Query.API/API/ContractService/Repositories/CertificateIssuingContractRepository.cs
@@ -1,11 +1,11 @@
 using DataContext;
+using DataContext.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using DataContext.Models;
 
 namespace API.ContractService.Repositories;
 
