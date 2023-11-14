ca164586249e4cc2d1b31b1b6a4e0ce0134ae10d
sahma19 <saiuan@msn.com>
2023-11-14 13:55:56 +0100

    Add migrationscript for updating empty techcode fuelcode rows in contracts table

---
+++ domains/certificates/Query.API/API.IntegrationTests/Shared/Migrations/UpdateTechnologyCodesMigrationTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+using API.ContractService;
+using API.Data;
+using API.IntegrationTests.Testcontainers;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
+using Xunit;
+
+namespace API.IntegrationTests.Shared.Migrations;
+
+public class UpdateTechnologyCodesMigrationTests : IAsyncDisposable
+{
+    private readonly PostgresContainer container = new();
+
+    [Fact]
+    public async Task ApplyMigration_WhenContractsHaveEmptyTechnologyCodes_UpdatesFieldsCorrectly()
+    {
+        await using var dbContext = await CreateNewCleanDatabase();
+
+        var migrator = dbContext.GetService<IMigrator>();
+        await migrator.MigrateAsync("20231107095405_AddTechnologyToCertificateIssuingContract");
+
+        var contractId = Guid.NewGuid();
+        await InsertContractWithEmptyTechnologyFields(dbContext, contractId, "123456789", "DK1");
+
+        var contractBeforeMigration = await dbContext.Contracts.FindAsync(contractId);
+        contractBeforeMigration!.Technology.FuelCode.Should().Be("");
+        contractBeforeMigration.Technology.TechCode.Should().Be("");
+
+        var applyMigration = () => migrator.MigrateAsync("20231114121811_UpdateEmptyTechnologyCodes");
+        await applyMigration.Should().NotThrowAsync();
+
+        await using var newDbContext = await CreateNewCleanDatabase();
+        var updatedContract = await newDbContext.Contracts.FindAsync(contractId);
+        updatedContract!.Technology.FuelCode.Should().Be("F00000000");
+        updatedContract.Technology.TechCode.Should().Be("T070000");
+    }
+
+    private static async Task InsertContractWithEmptyTechnologyFields(ApplicationDbContext dbContext, Guid id, string gsrn, string gridArea)
+    {
+        var contractsTable = dbContext.Model.FindEntityType(typeof(CertificateIssuingContract))!.GetTableName();
+
+        var contractQuery = $@"INSERT INTO ""{contractsTable}"" (
+                        ""Id"",
+                        ""GSRN"",
+                        ""GridArea"",
+                        ""MeteringPointType"",
+                        ""MeteringPointOwner"",
+                        ""StartDate"",
+                        ""Created"",
+                        ""WalletUrl"",
+                        ""WalletPublicKey"",
+                        ""ContractNumber"",
+                        ""Technology_FuelCode"",
+                        ""Technology_TechCode"")
+                    VALUES (
+                        @Id,
+                        @GSRN,
+                        @GridArea,
+                        @MeteringPointType,
+                        @MeteringPointOwner,
+                        @StartDate,
+                        @Created,
+                        @WalletUrl,
+                        @WalletPublicKey,
+                        @ContractNumber,
+                        @Technology_FuelCode,
+                        @Technology_TechCode
+                    )";
+
+        object[] contractFields = {
+            new NpgsqlParameter("Id", id),
+            new NpgsqlParameter("GSRN", gsrn),
+            new NpgsqlParameter("GridArea", gridArea),
+            new NpgsqlParameter("MeteringPointType", 1),
+            new NpgsqlParameter("MeteringPointOwner", "DummyOwner"),
+            new NpgsqlParameter("StartDate", DateTimeOffset.UtcNow),
+            new NpgsqlParameter("Created", DateTimeOffset.UtcNow),
+            new NpgsqlParameter("WalletUrl", "DummyUrl"),
+            new NpgsqlParameter("WalletPublicKey", new byte[]{}),
+            new NpgsqlParameter("ContractNumber", 1),
+            new NpgsqlParameter("Technology_FuelCode", ""),
+            new NpgsqlParameter("Technology_TechCode", "")
+        };
+
+        await dbContext.Database.ExecuteSqlRawAsync(contractQuery, contractFields);
+    }
+
+    private async Task<ApplicationDbContext> CreateNewCleanDatabase()
+    {
+        await container.InitializeAsync();
+        var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                                .UseNpgsql(container.ConnectionString)
+                                .Options;
+
+        var dbContext = new ApplicationDbContext(contextOptions);
+        return dbContext;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await container.DisposeAsync();
+    }
+}
---
+++ domains/certificates/Query.API/API/Migrations/20231114121811_UpdateEmptyTechnologyCodes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class UpdateEmptyTechnologyCodes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"
+    UPDATE ""Contracts""
+    SET
+        ""Technology_FuelCode"" = CASE
+            WHEN ""Technology_FuelCode"" = '' THEN 'F00000000'
+            ELSE ""Technology_FuelCode""
+        END,
+        ""Technology_TechCode"" = CASE
+            WHEN ""Technology_TechCode"" = '' THEN 'T070000'
+            ELSE ""Technology_TechCode""
+        END
+    WHERE ""Technology_FuelCode"" = '' OR ""Technology_TechCode"" = '';";
+
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Code to reverse the migration, if necessary
+        }
+    }
+}
