41543cefa26f534e426416fbaf8b337d461aa44d
Morten Munck Hansen <MOQ@energinet.dk>
2023-11-17 12:36:24 +0100

    temp

--- domains/auth/Auth.API/API/Controllers/LoginController.cs
+++ domains/auth/Auth.API/API/Controllers/LoginController.cs
@@ -13,6 +13,14 @@ public class LoginController : ControllerBase
 {
     [AllowAnonymous]
     [HttpGet]
+    [Route("auth/testmethod")]
+    public IActionResult TestMethod()
+    {
+        return Ok("Hello");
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
     [Route("auth/login")]
     public async Task<IActionResult> LoginAsync(
         IDiscoveryCache discoveryCache,
