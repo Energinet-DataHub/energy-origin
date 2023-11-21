e8ca2409f90d3087654013d4fdf4c7858f3abaaa
tnickelsen <TNE@energinet.dk>
2023-11-21 13:56:01 +0100

    Service is now scoped

--- domains/transfer/Transfer.API/API/Transfer/TransferAgreementsAutomation/TransferAgreementsAutomationWorker.cs
+++ domains/transfer/Transfer.API/API/Transfer/TransferAgreementsAutomation/TransferAgreementsAutomationWorker.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using API.Transfer.TransferAgreementsAutomation.Metrics;
 using API.Transfer.TransferAgreementsAutomation.Service;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -12,21 +14,58 @@ public class TransferAgreementsAutomationWorker : BackgroundService
 {
     private readonly ILogger<TransferAgreementsAutomationWorker> logger;
     private readonly IServiceProvider serviceProvider;
+    private readonly ITransferAgreementAutomationMetrics metrics;
+    private readonly AutomationCache memoryCache;
+
+    private readonly MemoryCacheEntryOptions cacheOptions = new()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromDays(1),
+    };
 
     public TransferAgreementsAutomationWorker(
         ILogger<TransferAgreementsAutomationWorker> logger,
-        IServiceProvider serviceProvider
+        IServiceProvider serviceProvider,
+        ITransferAgreementAutomationMetrics metrics,
+        AutomationCache memoryCache
     )
     {
         this.logger = logger;
         this.serviceProvider = serviceProvider;
+        this.metrics = metrics;
+        this.memoryCache = memoryCache;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        using var scope = serviceProvider.CreateScope();
-        var transferAgreementsAutomationService = scope.ServiceProvider.GetRequiredService<ITransferAgreementsAutomationService>();
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            logger.LogInformation("TransferAgreementsAutomationWorker running at: {time}", DateTimeOffset.Now);
+            metrics.ResetCertificatesTransferred();
+            metrics.ResetTransferErrors();
+            memoryCache.Cache.Set(HealthEntries.Key, HealthEntries.Healthy, cacheOptions);
 
-        await transferAgreementsAutomationService.Run(stoppingToken);
+            using var scope = serviceProvider.CreateScope();
+            var transferAgreementsAutomationService = scope.ServiceProvider.GetRequiredService<ITransferAgreementsAutomationService>();
+
+            try
+            {
+                await transferAgreementsAutomationService.Run(stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                memoryCache.Cache.Set(HealthEntries.Key, HealthEntries.Unhealthy, cacheOptions);
+                logger.LogWarning("Something went wrong with the TransferAgreementsAutomationWorker: {exception}", ex);
+            }
+
+            scope.Dispose();
+            await SleepToNearestHour(stoppingToken);
+        }
+    }
+
+    private async Task SleepToNearestHour(CancellationToken cancellationToken)
+    {
+        var minutesToNextHour = 60 - DateTimeOffset.Now.Minute;
+        logger.LogInformation("Sleeping until next full hour {minutesToNextHour}", minutesToNextHour);
+        await Task.Delay(TimeSpan.FromMinutes(minutesToNextHour), cancellationToken);
     }
 }
