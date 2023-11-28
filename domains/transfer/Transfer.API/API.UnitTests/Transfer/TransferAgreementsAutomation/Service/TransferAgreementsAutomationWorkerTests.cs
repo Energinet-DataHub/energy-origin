6dce780aed3c97762c53fe1ee3df00b8b29e2670
tnickelsen <TNE@energinet.dk>
2023-11-28 09:56:06 +0100

    Adjusted unit test

--- domains/transfer/Transfer.API/API.UnitTests/Transfer/TransferAgreementsAutomation/Service/TransferAgreementsAutomationWorkerTests.cs
+++ domains/transfer/Transfer.API/API.UnitTests/Transfer/TransferAgreementsAutomation/Service/TransferAgreementsAutomationWorkerTests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using API.Shared.Data;
@@ -7,11 +6,9 @@ using API.Transfer.Api.Models;
 using API.Transfer.Api.Services;
 using API.Transfer.TransferAgreementsAutomation;
 using API.Transfer.TransferAgreementsAutomation.Metrics;
-using API.Transfer.TransferAgreementsAutomation.Service;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Xunit;
@@ -28,9 +25,30 @@ public class TransferAgreementsAutomationWorkerTests
         var memoryCache = new AutomationCache();
         var poWalletServiceMock = Substitute.For<IProjectOriginWalletService>();
         var dbContextFactoryMock = Substitute.For<IDbContextFactory<ApplicationDbContext>>();
-        var dbContextMock = Substitute.For<ApplicationDbContext>();
+        var inMemoryOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("inMemoryTest1")
+            .Options;
 
-        dbContextFactoryMock.CreateDbContextAsync(Arg.Any<CancellationToken>()).Returns(dbContextMock);
+        await using (var context = new ApplicationDbContext(inMemoryOptions))
+        {
+            context.TransferAgreements.Add(new TransferAgreement
+            {
+                EndDate = null,
+                Id = Guid.NewGuid(),
+                ReceiverReference = Guid.NewGuid(),
+                ReceiverTin = "12345678",
+                SenderId = Guid.NewGuid(),
+                SenderName = "SomeCompany",
+                SenderTin = "11223344",
+                StartDate = DateTimeOffset.UtcNow,
+                TransferAgreementNumber = 1
+            });
+
+            await context.SaveChangesAsync();
+        }
+
+        dbContextFactoryMock.CreateDbContextAsync(Arg.Any<CancellationToken>())
+            .Returns(new ApplicationDbContext(inMemoryOptions));
 
         using var cts = new CancellationTokenSource();
         poWalletServiceMock
@@ -41,7 +59,6 @@ public class TransferAgreementsAutomationWorkerTests
                 throw new Exception();
             });
 
-
         var worker = new TransferAgreementsAutomationWorker(loggerMock, metricsMock, memoryCache, poWalletServiceMock, dbContextFactoryMock);
 
         var act = async () => await worker.StartAsync(cts.Token);
@@ -50,42 +67,52 @@ public class TransferAgreementsAutomationWorkerTests
         memoryCache.Cache.Get(HealthEntries.Key).Should().Be(HealthEntries.Unhealthy);
     }
 
-    //[Fact]
-    //public async Task WhenCalled_CacheIsSetToHealthy()
-    //{
-    //    var loggerMock = Substitute.For<ILogger<TransferAgreementsAutomationWorker>>();
-    //    var metricsMock = Substitute.For<ITransferAgreementAutomationMetrics>();
-    //    var memoryCache = new AutomationCache();
-    //    var taAutomationMock = Substitute.For<ITransferAgreementsAutomationService>();
-
-    //    using var cts = new CancellationTokenSource();
-    //    taAutomationMock
-    //        .When(x => x.Run(Arg.Any<CancellationToken>()))
-    //        .Do(_ =>
-    //        {
-    //            cts.Cancel();
-    //        });
-
-    //    var serviceProviderMock = SetupIServiceProviderMock(taAutomationMock);
-
-    //    var worker = new TransferAgreementsAutomationWorker(loggerMock, serviceProviderMock, metricsMock, memoryCache);
-
-    //    var act = async () => await worker.StartAsync(cts.Token);
-    //    await act.Should().ThrowAsync<TaskCanceledException>();
-
-    //    memoryCache.Cache.Get(HealthEntries.Key).Should().Be(HealthEntries.Healthy);
-    //}
-
-    private static IServiceProvider SetupIServiceProviderMock(ITransferAgreementsAutomationService taAutomationMock)
+    [Fact]
+    public async Task WhenCalled_CacheIsSetToHealthy()
     {
-        var serviceProviderMock = Substitute.For<IServiceProvider>();
-        serviceProviderMock.GetService(typeof(ITransferAgreementsAutomationService)).Returns(taAutomationMock);
-        var serviceScope = Substitute.For<IServiceScope>();
-        serviceScope.ServiceProvider.Returns(serviceProviderMock);
-        var serviceScopeFactoryMock = Substitute.For<IServiceScopeFactory>();
-        serviceScopeFactoryMock.CreateScope().Returns(serviceScope);
-        serviceProviderMock.GetService(typeof(IServiceScopeFactory)).Returns(serviceScopeFactoryMock);
+        var loggerMock = Substitute.For<ILogger<TransferAgreementsAutomationWorker>>();
+        var metricsMock = Substitute.For<ITransferAgreementAutomationMetrics>();
+        var memoryCache = new AutomationCache();
+        var poWalletServiceMock = Substitute.For<IProjectOriginWalletService>();
+        var dbContextFactoryMock = Substitute.For<IDbContextFactory<ApplicationDbContext>>();
+        var inMemoryOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("inMemoryTest2")
+            .Options;
 
-        return serviceProviderMock;
+        await using (var context = new ApplicationDbContext(inMemoryOptions))
+        {
+            context.TransferAgreements.Add(new TransferAgreement
+            {
+                EndDate = null,
+                Id = Guid.NewGuid(),
+                ReceiverReference = Guid.NewGuid(),
+                ReceiverTin = "12345678",
+                SenderId = Guid.NewGuid(),
+                SenderName = "SomeCompany",
+                SenderTin = "11223344",
+                StartDate = DateTimeOffset.UtcNow,
+                TransferAgreementNumber = 1
+            });
+
+            await context.SaveChangesAsync();
+        }
+
+        dbContextFactoryMock.CreateDbContextAsync(Arg.Any<CancellationToken>())
+            .Returns(new ApplicationDbContext(inMemoryOptions));
+
+        using var cts = new CancellationTokenSource();
+        poWalletServiceMock
+            .When(x => x.TransferCertificates(Arg.Any<TransferAgreement>()))
+            .Do(_ =>
+            {
+                cts.Cancel();
+            });
+
+        var worker = new TransferAgreementsAutomationWorker(loggerMock, metricsMock, memoryCache, poWalletServiceMock, dbContextFactoryMock);
+
+        var act = async () => await worker.StartAsync(cts.Token);
+        await act.Should().ThrowAsync<TaskCanceledException>();
+
+        memoryCache.Cache.Get(HealthEntries.Key).Should().Be(HealthEntries.Healthy);
     }
 }
