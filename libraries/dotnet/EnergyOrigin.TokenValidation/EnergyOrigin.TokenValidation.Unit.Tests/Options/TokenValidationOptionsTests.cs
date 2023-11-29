bb1616323fd37e5559327c6a24803f257bfa57fa
sahma19 <saiuan@msn.com>
2023-11-29 15:40:38 +0100

    Add TokenValidationOptionsTests.cs

---
+++ libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation.Unit.Tests/Options/TokenValidationOptionsTests.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EnergyOrigin.TokenValidation.Options;
+
+namespace EnergyOrigin.TokenValidation.Unit.Tests.Options;
+
+public class TokenValidationOptionsTests
+{
+    [Fact]
+    public void Properties_ShouldBeInitializedAsNull()
+    {
+        var options = new TokenValidationOptions();
+
+        Assert.Null(options.PublicKey);
+        Assert.Null(options.Issuer);
+        Assert.Null(options.Audience);
+    }
+
+    [Fact]
+    public void Properties_ShouldBeCorrectlyInitialized()
+    {
+        var publicKey = new byte[] { 1, 2, 3 };
+        var issuer = "TestIssuer";
+        var audience = "TestAudience";
+
+        var options = new TokenValidationOptions
+        {
+            PublicKey = publicKey,
+            Issuer = issuer,
+            Audience = audience
+        };
+
+        Assert.Equal(publicKey, options.PublicKey);
+        Assert.Equal(issuer, options.Issuer);
+        Assert.Equal(audience, options.Audience);
+    }
+
+    [Theory]
+    [InlineData(null, "TestIssuer", "TestAudience")]
+    [InlineData(new byte[0], null, "TestAudience")]
+    [InlineData(new byte[0], "TestIssuer", null)]
+    public void Properties_ShouldFailValidation_WhenRequiredPropertyIsNull(byte[] publicKey, string issuer, string audience)
+    {
+        var options = new TokenValidationOptions
+        {
+            PublicKey = publicKey,
+            Issuer = issuer,
+            Audience = audience
+        };
+
+        var validationResults = new List<ValidationResult>();
+        var validationContext = new ValidationContext(options, null, null);
+        var isValid = Validator.TryValidateObject(options, validationContext, validationResults, true);
+
+        Assert.False(isValid);
+        Assert.NotEmpty(validationResults);
+    }
+}
