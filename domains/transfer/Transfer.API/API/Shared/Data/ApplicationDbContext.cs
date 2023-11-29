209f1591b1e6b41faa54511509eb46b2fa6ddf9b
sahma19 <saiuan@msn.com>
2023-11-29 15:35:52 +0100

    remove pragma

--- domains/transfer/Transfer.API/API/Shared/Data/ApplicationDbContext.cs
+++ domains/transfer/Transfer.API/API/Shared/Data/ApplicationDbContext.cs
@@ -8,9 +8,7 @@ namespace API.Shared.Data;
 
 public class ApplicationDbContext : AuditDbContext
 {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
     }
 
