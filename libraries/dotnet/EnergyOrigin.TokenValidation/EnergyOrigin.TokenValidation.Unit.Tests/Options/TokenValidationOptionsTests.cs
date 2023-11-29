352ce2dd1cece945a5b2700fac1c8bb524118b24
sahma19 <saiuan@msn.com>
2023-11-29 15:02:33 +0100

    add tokenoptions, and unit tests

---
+++ libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation.Unit.Tests/Options/TokenValidationOptionsTests.cs
@@ -0,0 +1,35 @@
+﻿using EnergyOrigin.TokenValidation.Options;
+
+namespace EnergyOrigin.TokenValidation.Unit.Tests.Options;
+
+public class TokenValidationOptionsTests
+{
+    [Fact]
+    public void Properties_ShouldBeInitializedAsNull()
+    {
+        var options = new TokenValidationOptions();
+
+        Assert.Null(options.PublicKey);
+        Assert.Null(options.Issuer);
+        Assert.Null(options.Audience);
+    }
+
+    [Fact]
+    public void Properties_ShouldBeCorrectlyInitialized()
+    {
+        var publicKey = new byte[] { 1, 2, 3 };
+        var issuer = "TestIssuer";
+        var audience = "TestAudience";
+
+        var options = new TokenValidationOptions
+        {
+            PublicKey = publicKey,
+            Issuer = issuer,
+            Audience = audience
+        };
+
+        Assert.Equal(publicKey, options.PublicKey);
+        Assert.Equal(issuer, options.Issuer);
+        Assert.Equal(audience, options.Audience);
+    }
+}
