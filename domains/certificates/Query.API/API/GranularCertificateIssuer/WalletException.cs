45f771511e3d0701875a30563ea6cde4c7f5fc45
sahma19 <saiuan@msn.com>
2023-12-07 14:59:10 +0100

    fix null warnings in certificates, and change to dotnet 8 compliant syntax in WalletException.cs

--- domains/certificates/Query.API/API/GranularCertificateIssuer/WalletException.cs
+++ domains/certificates/Query.API/API/GranularCertificateIssuer/WalletException.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Runtime.Serialization;
 
 namespace API.GranularCertificateIssuer;
 
-[Serializable]
 public class WalletException : Exception
 {
     public WalletException(string message) : base(message)
-    {
-    }
-
-    protected WalletException(SerializationInfo info, StreamingContext context) : base(info, context)
     {
     }
 }
