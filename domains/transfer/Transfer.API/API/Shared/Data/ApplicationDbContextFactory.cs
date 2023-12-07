bfc4ee864f3de8e119d4d253fcc0f6cb1f1de7d9
sahma19 <saiuan@msn.com>
2023-12-07 16:17:57 +0100

    Change to scoped lifetime

---
+++ domains/transfer/Transfer.API/API/Shared/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Shared.Data;
+
+public class ApplicationDbContextFactory : IDbContextFactory<ApplicationDbContext>
+{
+    private readonly string _connectionString;
+
+    public ApplicationDbContextFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public ApplicationDbContext CreateDbContext()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql(_connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
