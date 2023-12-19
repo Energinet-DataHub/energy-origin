4cf712d82fd8ece881d3d72787f5df1c1ddf8938
Christoffer Frydkjær <christofferfk@gmail.com>
2023-12-19 13:45:33 +0100

    to string enum

--- domains/transfer/Transfer.API/API.IntegrationTests/Factories/TransferAgreementsApiWebApplicationFactory.cs
+++ domains/transfer/Transfer.API/API.IntegrationTests/Factories/TransferAgreementsApiWebApplicationFactory.cs
@@ -206,7 +206,7 @@ public class TransferAgreementsApiWebApplicationFactory : WebApplicationFactory<
             { UserClaimName.Tin, tin },
             { UserClaimName.OrganizationName, cpn },
             { JwtRegisteredClaimNames.Name, name },
-            { UserClaimName.ProviderType, ProviderType.MitIdProfessional},
+            { UserClaimName.ProviderType, ProviderType.MitIdProfessional.ToString()},
             { UserClaimName.AllowCprLookup, "false"},
             { UserClaimName.AccessToken, ""},
             { UserClaimName.IdentityToken, ""},
