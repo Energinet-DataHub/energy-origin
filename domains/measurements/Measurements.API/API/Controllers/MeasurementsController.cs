85ec2566015605014cd0f7ed10b36dba8ef27596
sahma19 <saiuan@msn.com>
2023-12-20 11:38:07 +0100

    opt out of inheriting from authorizationcontroller

--- domains/measurements/Measurements.API/API/Controllers/MeasurementsController.cs
+++ domains/measurements/Measurements.API/API/Controllers/MeasurementsController.cs
@@ -10,7 +10,7 @@ namespace API.Controllers;
 
 [ApiController]
 [Authorize]
-public class MeasurementsController : AuthorizationController
+public class MeasurementsController : ControllerBase
 {
     [HttpGet]
     [Route("api/measurements/consumption")]
