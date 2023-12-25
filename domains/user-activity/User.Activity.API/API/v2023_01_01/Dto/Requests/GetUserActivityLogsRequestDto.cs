3cf91b1b6cdb5320466f1f3f0f5e5e79ed42fb54
sahma19 <saiuan@msn.com>
2023-12-25 11:31:01 +0100

    add request dto

---
+++ domains/user-activity/User.Activity.API/API/v2023_01_01/Dto/Requests/GetUserActivityLogsRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.v2023_01_01.Dto.Requests;
+
+public record GetUserActivityLogsRequestDto
+{
+    // This will be populated with the string representations of UserActivityType
+    public List<string> Filter { get; init; } = [];
+    public DateTime? StartDate { get; init; }
+    public DateTime? EndDate { get; init; }
+    public int Offset { get; init; } = 0;
+    public int Limit { get; init; } = 10;
+}
