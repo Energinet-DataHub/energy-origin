b0a3e6dc19a3d7f48a2a11c66d69b9cc7ea14d01
sahma19 <saiuan@msn.com>
2023-12-25 11:12:18 +0100

    Added Repository

---
+++ domains/user-activity/User.Activity.API/API/Repositories/UserActivityLogsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using API.Shared.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories;
+
+public class UserActivityLogsRepository(ApplicationDbContext context) : IUserActivityLogsRepository
+{
+    public async Task<List<UserActivityLog>> GetUserActivityLogsAsync(Guid actorId)
+    {
+        return await context.Set<UserActivityLog>()
+            .Where(log => log.ActorId == actorId)
+            .ToListAsync();
+    }
+}
--- domains/user-activity/User.Activity.API/API/v2023_01_01/Controllers/UserActivityLogsController.cs
+++ domains/user-activity/User.Activity.API/API/v2023_01_01/Controllers/UserActivityLogsController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,36 +7,15 @@ using API.Shared.Data;
 
 namespace API.v2023_01_01.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/user-activity-logs")]
     [ApiController]
-    public class UserActivityLogsController : ControllerBase
+    public class UserActivityLogsController(ApplicationDbContext context) : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-
-        public UserActivityLogsController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
         // GET: api/UserActivityLogs
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserActivityLog>>> GetUserActivityLogs()
         {
-            return await _context.UserActivityLogs.ToListAsync();
-        }
-
-        // GET: api/UserActivityLogs/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<UserActivityLog>> GetUserActivityLog(Guid id)
-        {
-            var userActivityLog = await _context.UserActivityLogs.FindAsync(id);
-
-            if (userActivityLog == null)
-            {
-                return NotFound();
-            }
-
-            return userActivityLog;
+            return await context.UserActivityLogs.ToListAsync();
         }
     }
 }
