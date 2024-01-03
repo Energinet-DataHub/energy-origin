384aae039ec9ec1219e5b212115c0f8d70e75c08
Christoffer Frydkjær <christofferfk@gmail.com>
2024-01-03 10:16:50 +0100

    remove not need code

--- domains/transfer/Shared/DataContext/ApplicationDbContext.cs
+++ domains/transfer/Shared/DataContext/ApplicationDbContext.cs
@@ -32,19 +32,3 @@ public class ApplicationDbContext : AuditDbContext
             .HasDefaultValueSql("current_timestamp at time zone 'UTC'");
     }
 }
-
-// Some of the EF Core Tools commands (for example, the Migrations commands) require a derived DbContext instance to be created at design time
-// By implementing the class below, the EF Core Tools will automatically use that for creating the DbContext
-public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-{
-    public ApplicationDbContext CreateDbContext(string[] args)
-    {
-        const string connectionString = "host=localhost;Port=5432;Database=Database;username=postgres;password=postgres;";
-
-        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        optionsBuilder.UseNpgsql(connectionString);
-
-        return new ApplicationDbContext(optionsBuilder.Options);
-    }
-}
-
