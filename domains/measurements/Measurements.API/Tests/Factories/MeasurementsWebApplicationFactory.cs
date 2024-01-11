889105d63adbdc4d61caecdb6995597bd86070c4
tnickelsen <TNE@energinet.dk>
2024-01-11 13:00:25 +0100

    Added Meteringpoint integrationtests

--- domains/measurements/Measurements.API/Tests/Factories/MeasurementsWebApplicationFactory.cs
+++ domains/measurements/Measurements.API/Tests/Factories/MeasurementsWebApplicationFactory.cs
@@ -2,13 +2,17 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Xunit;
 using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using System.Security.Cryptography;
 using EnergyOrigin.TokenValidation.Utilities;
+using System.Net.Http.Headers;
+using EnergyOrigin.TokenValidation.Values;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.AspNetCore.TestHost;
 
 namespace Tests.Factories;
 
@@ -45,10 +49,84 @@ public class MeasurementsWebApplicationFactory : WebApplicationFactory<Program>
         return client;
     }
 
+    public HttpClient CreateAuthenticatedClient(
+        Meteringpoint.V1.Meteringpoint.MeteringpointClient clientMock, string sub, string tin = "11223344", string name = "Peter Producent",
+        string actor = "d4f32241-442c-4043-8795-a4e6bf574e7f", string apiVersion = "20240110")
+    {
+        var client = WithWebHostBuilder(b =>
+        {
+            b.ConfigureTestServices(services =>
+            {
+                var mpClient = services.Single(d => d.ServiceType == typeof(Meteringpoint.V1.Meteringpoint.MeteringpointClient));
+                services.Remove(mpClient);
+                services.AddSingleton(clientMock);
+            });
+        }).CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", GenerateToken(sub: sub, tin: tin, name: name, actor: actor));
+        client.DefaultRequestHeaders.Add("EO_API_VERSION", apiVersion);
+
+        return client;
+    }
+
+    public HttpClient CreateAuthenticatedClient(string sub, string tin = "11223344", string name = "Peter Producent",
+        string actor = "d4f32241-442c-4043-8795-a4e6bf574e7f", string apiVersion = "20240110")
+    {
+        var client = CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", GenerateToken(sub: sub, tin: tin, name: name, actor: actor));
+        client.DefaultRequestHeaders.Add("EO_API_VERSION", apiVersion);
+        return client;
+    }
+
     public IApiVersionDescriptionProvider GetApiVersionDescriptionProvider()
     {
         using var scope = Services.CreateScope();
         var provider = scope.ServiceProvider.GetRequiredService<IApiVersionDescriptionProvider>();
         return provider;
+    }
+
+
+    private string GenerateToken(
+        string scope = "",
+        string actor = "d4f32241-442c-4043-8795-a4e6bf574e7f",
+        string sub = "03bad0af-caeb-46e8-809c-1d35a5863bc7",
+        string tin = "11223344",
+        string cpn = "Producent A/S",
+        string name = "Peter Producent",
+        string issuer = "Issuer",
+        string audience = "Audience")
+    {
+
+        var claims = new Dictionary<string, object>()
+        {
+            { UserClaimName.Scope, scope },
+            { UserClaimName.ActorLegacy, actor },
+            { UserClaimName.Actor, actor },
+            { UserClaimName.Tin, tin },
+            { UserClaimName.OrganizationName, cpn },
+            { JwtRegisteredClaimNames.Name, name },
+            { UserClaimName.ProviderType, ProviderType.MitIdProfessional.ToString()},
+            { UserClaimName.AllowCprLookup, "false"},
+            { UserClaimName.AccessToken, ""},
+            { UserClaimName.IdentityToken, ""},
+            { UserClaimName.ProviderKeys, ""},
+            { UserClaimName.OrganizationId, sub},
+            { UserClaimName.MatchedRoles, ""},
+            { UserClaimName.Roles, ""},
+            { UserClaimName.AssignedRoles, ""}
+        };
+
+        var signedJwtToken = new TokenSigner(PrivateKey).Sign(
+            sub,
+            name,
+            issuer,
+            audience,
+            null,
+            60,
+            claims
+        );
+
+        return signedJwtToken;
     }
 }
---
+++ domains/measurements/Measurements.API/Tests/MeteringPoints/Api/v2024_01_10/MeteringPointControllerTests.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using API.MeteringPoints.Api.v2024_01_10.Dto.Responses;
+using FluentAssertions;
+using Grpc.Core;
+using Grpc.Core.Testing;
+using NSubstitute;
+using NSubstitute.Core;
+using Tests.Factories;
+using VerifyTests;
+using VerifyXunit;
+using Xunit;
+
+namespace Tests.MeteringPoints.Api.v2024_01_10;
+
+[UsesVerify]
+public class MeteringPointControllerTests : IClassFixture<MeasurementsWebApplicationFactory>
+{
+    private readonly MeasurementsWebApplicationFactory factory;
+
+    public MeteringPointControllerTests(MeasurementsWebApplicationFactory factory)
+    {
+        this.factory = factory;
+    }
+
+    [Fact]
+    public async Task GetMeteringPoints()
+    {
+        var clientMock = Substitute.For<Meteringpoint.V1.Meteringpoint.MeteringpointClient>();
+
+        clientMock.GetOwnedMeteringPointsAsync(Arg.Any<Meteringpoint.V1.OwnedMeteringPointsRequest>())
+            .Returns(new Meteringpoint.V1.MeteringPointsResponse
+            {
+                MeteringPoints =
+                {
+                    new Meteringpoint.V1.MeteringPoint
+                    {
+                        MeteringPointId = "1234567890123456",
+                        TypeOfMp = "E17",
+                        SubtypeOfMp = "D01",
+                        StreetName = "Street",
+                        BuildingNumber = "1",
+                        FloorId = "1",
+                        RoomId = "1",
+                        CityName = "City",
+                        Postcode = "1234",
+                        AssetType = "E17"
+                    }
+                }
+            });
+
+        var client = factory
+            .CreateAuthenticatedClient(clientMock,Guid.NewGuid().ToString());
+
+        var response = await client.GetFromJsonAsync<GetMeteringPointsResponse>("api/meteringpoints");
+
+        response.Should().NotBeNull();
+        var settings = new VerifySettings();
+        settings.DontScrubGuids();
+        await Verifier.Verify(response, settings);
+    }
+}
+
+
+public static class MockExtensions
+{
+    public static ConfiguredCall Returns<TResponse>(this AsyncUnaryCall<TResponse> value, TResponse response)
+        where TResponse : class
+    {
+        var call = TestCalls.AsyncUnaryCall(Task.FromResult(response), Task.FromResult(new Metadata()), () => Status.DefaultSuccess, () => new Metadata(), () => { });
+
+        return value.Returns(call);
+    }
+}
