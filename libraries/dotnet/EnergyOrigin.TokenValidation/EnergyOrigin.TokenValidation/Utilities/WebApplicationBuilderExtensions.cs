87a90d5a3500fb5ccd79e646044060cc46b2c6d6
tnickelsen <TNE@energinet.dk>
2024-01-15 14:54:25 +0100

    Simplified builder extension

--- libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/Utilities/WebApplicationBuilderExtensions.cs
+++ libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/Utilities/WebApplicationBuilderExtensions.cs
@@ -1,7 +1,5 @@
 using EnergyOrigin.TokenValidation.Options;
-using EnergyOrigin.TokenValidation.Values;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace EnergyOrigin.TokenValidation.Utilities;
 
@@ -9,14 +7,6 @@ public static class WebApplicationBuilderExtensions
 {
     public static void AddTokenValidation(this WebApplicationBuilder builder, TokenValidationOptions validationOptions)
     {
-        var validationParameters = new ValidationParameters(validationOptions.PublicKey);
-        validationParameters.ValidIssuer = validationOptions.Issuer;
-        validationParameters.ValidAudience = validationOptions.Audience;
-        builder.Services.AddAuthentication().AddJwtBearer(options =>
-        {
-            options.MapInboundClaims = false;
-            options.TokenValidationParameters = validationParameters;
-        });
-        builder.Services.AddAuthorization(options => options.AddPolicy(PolicyName.RequiresCompany, policy => policy.RequireClaim(UserClaimName.Tin)));
+        builder.Services.AddTokenValidation(validationOptions);
     }
 }
