9058364d231f33d09e684ae03bf3c044aa1a7afb
sahma19 <saiuan@msn.com>
2024-01-15 22:43:50 +0100

    idk anymore...

--- domains/certificates/Query.API/API.IntegrationTests/UnhealthTests.cs
+++ domains/certificates/Query.API/API.IntegrationTests/UnhealthTests.cs
@@ -6,21 +6,23 @@ using Xunit;
 
 namespace API.IntegrationTests;
 
-public class UnhealthTests : TestBase, IClassFixture<QueryApiWebApplicationFactory>
+public class UnhealthTests : TestBase, IClassFixture<QueryApiWebApplicationFactory>, IClassFixture<RegistryConnectorApplicationFactory>
 {
     private readonly QueryApiWebApplicationFactory factory;
+    private readonly RegistryConnectorApplicationFactory connectorFactory;
 
-    public UnhealthTests(QueryApiWebApplicationFactory factory)
+    public UnhealthTests(QueryApiWebApplicationFactory factory, RegistryConnectorApplicationFactory connectorFactory)
     {
         this.factory = factory;
-        this.factory.RabbitMqOptions = null;
-        this.factory.ConnectionString = "host=localhost;port=5432;database=postgres;username=postgres;password=postgres";
+        this.connectorFactory = connectorFactory;
+        this.connectorFactory.RabbitMqOptions = null;
+        this.connectorFactory.ConnectionString = "Server=foo;Port=5432;Database=bar;User Id=baz;Password=qux;";
     }
 
     [Fact]
     public async Task Health_IsCalledWhenRabbitMqIsDown_ReturnsServiceUnavailable()
     {
-        using var client = factory.CreateClient();
+        using var client = connectorFactory.CreateClient();
         using var healthResponse = await client.GetAsync("health");
         healthResponse.StatusCode.Should().Be(HttpStatusCode.ServiceUnavailable);
     }
