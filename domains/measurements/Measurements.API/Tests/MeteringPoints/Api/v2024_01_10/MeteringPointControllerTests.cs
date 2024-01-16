8ac944485d77d5af9ee451be7a82fb2a98040028
tnickelsen <TNE@energinet.dk>
2024-01-16 09:24:52 +0100

    Added MeteringPointController tests

--- domains/measurements/Measurements.API/Tests/MeteringPoints/Api/v2024_01_10/MeteringPointControllerTests.cs
+++ domains/measurements/Measurements.API/Tests/MeteringPoints/Api/v2024_01_10/MeteringPointControllerTests.cs
@@ -20,7 +20,7 @@ namespace Tests.MeteringPoints.Api.v2024_01_10;
 public class MeteringPointControllerTests : MeasurementsTestBase
 {
     public MeteringPointControllerTests(TestServerFixture<Startup> serverFixture)
-    : base(serverFixture)
+        : base(serverFixture)
     {
     }
 
@@ -61,7 +61,8 @@ public class MeteringPointControllerTests : MeasurementsTestBase
             });
         _serverFixture.ConfigureTestServices += services =>
         {
-            var mpClient = services.Single(d => d.ServiceType == typeof(Meteringpoint.V1.Meteringpoint.MeteringpointClient));
+            var mpClient = services.Single(d =>
+                d.ServiceType == typeof(Meteringpoint.V1.Meteringpoint.MeteringpointClient));
             services.Remove(mpClient);
             services.AddSingleton(clientMock);
         };
@@ -74,5 +75,127 @@ public class MeteringPointControllerTests : MeasurementsTestBase
         var settings = new VerifySettings();
         settings.DontScrubGuids();
         await Verifier.Verify(response, settings);
+    }
+
+    [Fact]
+    public async Task GetMeteringPoints_GivenChildMp_ExpectChildMpOmitted()
+    {
+        var childTypeOfMp = "D01";
+        var clientMock = Substitute.For<Meteringpoint.V1.Meteringpoint.MeteringpointClient>();
+
+        clientMock.GetOwnedMeteringPointsAsync(Arg.Any<Meteringpoint.V1.OwnedMeteringPointsRequest>())
+            .Returns(new Meteringpoint.V1.MeteringPointsResponse
+            {
+                MeteringPoints =
+                {
+                    new Meteringpoint.V1.MeteringPoint
+                    {
+                        MeteringPointId = "1234567890123456",
+                        TypeOfMp = "E17",
+                        SubtypeOfMp = "D01",
+                        StreetName = "Street",
+                        BuildingNumber = "1",
+                        FloorId = "1",
+                        RoomId = "1",
+                        CityName = "City",
+                        Postcode = "1234",
+                        AssetType = "E17"
+                    },
+                    new Meteringpoint.V1.MeteringPoint
+                    {
+                        MeteringPointId = "1234567890123457",
+                        TypeOfMp = childTypeOfMp,
+                        SubtypeOfMp = "D01",
+                        StreetName = "Street",
+                        BuildingNumber = "1",
+                        FloorId = "1",
+                        RoomId = "1",
+                        CityName = "City",
+                        Postcode = "1234",
+                        AssetType = "E17"
+                    }
+                }
+            });
+        _serverFixture.ConfigureTestServices += services =>
+        {
+            var mpClient = services.Single(d =>
+                d.ServiceType == typeof(Meteringpoint.V1.Meteringpoint.MeteringpointClient));
+            services.Remove(mpClient);
+            services.AddSingleton(clientMock);
+        };
+
+        var client = _serverFixture.CreateAuthenticatedClient(Guid.NewGuid().ToString());
+
+        var response = await client.GetFromJsonAsync<GetMeteringPointsResponse>("api/meteringpoints");
+
+        response.Should().NotBeNull();
+        var settings = new VerifySettings();
+        settings.DontScrubGuids();
+        await Verifier.Verify(response, settings);
+    }
+
+    [Theory]
+    [InlineData("streetName", "", "", "")]
+    [InlineData("streetName", "buildingNo", "", "")]
+    [InlineData("streetName", "buildingNo", "floor", "")]
+    [InlineData("streetName", "buildingNo", "", "room")]
+    [InlineData("streetName", "", "floor", "room")]
+    [InlineData("streetName", "", "floor", "")]
+    [InlineData("streetName", "", "", "room")]
+    [InlineData("", "buildingNo", "floor", "room")]
+    [InlineData("", "buildingNo", "", "room")]
+    [InlineData("", "buildingNo", "floor", "")]
+    [InlineData("", "", "floor", "room")]
+    [InlineData("", "", "floor", "")]
+    [InlineData("", "", "", "room")]
+    [InlineData("", "", "", "")]
+    [InlineData("streetName", "buildingNo", "floor", "room")]
+    [InlineData("streetName", "    buildingNo", "floor", "room")]
+    [InlineData("streetName", "buildingNo", "floor", "    room")]
+    [InlineData("    streetName", "buildingNo", "floor", "room")]
+    [InlineData("    streetName", "    buildingNo", "    floor", "    room")]
+    [InlineData("streetName", "buildingNo", "    floor", "room")]
+
+    public async void EmptyAddressInformation_GetMeteringPoint_AddressLinesWithoutWhiteSpace(string streetName, string buildingNumber, string floor, string room)
+    {
+        var clientMock = Substitute.For<Meteringpoint.V1.Meteringpoint.MeteringpointClient>();
+
+        clientMock.GetOwnedMeteringPointsAsync(Arg.Any<Meteringpoint.V1.OwnedMeteringPointsRequest>())
+            .Returns(new Meteringpoint.V1.MeteringPointsResponse
+            {
+                MeteringPoints =
+                {
+                    new Meteringpoint.V1.MeteringPoint
+                    {
+                        MeteringPointId = "1234567890123456",
+                        TypeOfMp = "E17",
+                        SubtypeOfMp = "D01",
+                        StreetName = streetName,
+                        BuildingNumber = buildingNumber,
+                        FloorId = floor,
+                        RoomId = room,
+                        CityName = "City",
+                        Postcode = "1234",
+                        AssetType = "E17"
+                    }
+                }
+            });
+        _serverFixture.ConfigureTestServices += services =>
+        {
+            var mpClient = services.Single(d => d.ServiceType == typeof(Meteringpoint.V1.Meteringpoint.MeteringpointClient));
+            services.Remove(mpClient);
+            services.AddSingleton(clientMock);
+        };
+
+        var client = _serverFixture.CreateAuthenticatedClient(Guid.NewGuid().ToString());
+
+        var response = await client.GetFromJsonAsync<GetMeteringPointsResponse>("api/meteringpoints");
+
+        response.Should().NotBeNull();
+
+        response!.Result.First().Address.Address1.Trim().Should().Be(response.Result.First().Address.Address1);
+        response.Result.First().Address.Address2!.Trim().Should().Be(response.Result.First().Address.Address2);
+        response.Result.First().Address.Address1.Should().NotContain("  ");
+        response.Result.First().Address.Address2.Should().NotContain("  ");
     }
 }
