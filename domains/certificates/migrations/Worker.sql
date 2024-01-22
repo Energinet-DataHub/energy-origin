fd8c8dd3de0960d08f9859e46a7649a4cc75b13e
tnickelsen <TNE@energinet.dk>
2024-01-22 13:35:12 +0100

    made files utf-8 BOM

--- domains/certificates/migrations/Worker.sql
+++ domains/certificates/migrations/Worker.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
     "MigrationId" character varying(150) NOT NULL,
     "ProductVersion" character varying(32) NOT NULL,
     CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
