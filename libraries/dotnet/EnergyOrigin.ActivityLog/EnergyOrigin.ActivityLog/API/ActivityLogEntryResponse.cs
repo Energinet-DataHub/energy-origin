fd4c534aa54defd4cd95c4c7da00961c4b4efe5c
Joshua Jesper Krægpøth Ryder <josh@topswagcode.com>
2024-01-24 15:00:20 +0100

    Create ActivityLog nuget package (#588)

---
+++ libraries/dotnet/EnergyOrigin.ActivityLog/EnergyOrigin.ActivityLog/API/ActivityLogEntryResponse.cs
@@ -0,0 +1,26 @@
+namespace EnergyOrigin.ActivityLog.API;
+
+public class ActivityLogEntryResponse
+{
+    public enum ActorTypeEnum { User, System }
+    public enum EntityTypeEnum { TransferAgreement, MeteringPoint }
+    public enum ActionTypeEnum { Created, Accepted, Declined, Activated, Deactivated, ChangeEndDate }
+
+    // General
+    public Guid Id { get; init; }
+    public DateTimeOffset Timestamp { get; init; }
+
+    // User/Machine initiating the request
+    public Guid ActorId { get; init; } // Eks. Granular's id or Charlotte's id
+    public ActorTypeEnum ActorType { get; init; }
+    public string ActorName { get; init; } = ""; // Company name / person name
+
+    // Owner
+    public string OrganizationTin { get; init; } = ""; // CVR
+    public string OrganizationName { get; init; } = ""; // Eks. "Mogens Mølleejer A/S"
+
+    // Action
+    public EntityTypeEnum EntityType { get; init; }
+    public ActionTypeEnum ActionType { get; init; }
+    public Guid EntityId { get; init; }
+}
