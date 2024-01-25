4573cb0e87b38f15e07b5f15d25cf3f01afcb224
sahma19 <saiuan@msn.com>
2024-01-26 00:09:08 +0100

    Revert "Create ActivityLog nuget package (#588)"
    This reverts commit fd4c534aa54defd4cd95c4c7da00961c4b4efe5c.

--- libraries/dotnet/EnergyOrigin.ActivityLog/EnergyOrigin.ActivityLog/API/ActivityLogEntryFilterRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace EnergyOrigin.ActivityLog.API;
-
-public record ActivityLogEntryFilterRequest(DateTimeOffset? Start, DateTimeOffset? End, ActivityLogEntryResponse.EntityTypeEnum? EntityType);
