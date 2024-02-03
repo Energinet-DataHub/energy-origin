3c5e0fa9a2b4889f9d286801701c777db4b188b6
sahma19 <saiuan@msn.com>
2024-02-03 16:38:44 +0100

    remove customization

--- domains/transfer/ClaimAutomation/Worker/Program.cs
+++ domains/transfer/ClaimAutomation/Worker/Program.cs
@@ -113,14 +113,7 @@ builder.Services.AddOpenTelemetry()
             .AddOtlpExporter(o => o.Endpoint = otlpOptions.ReceiverEndpoint))
     .WithTracing(provider =>
         provider
-            .AddGrpcClientInstrumentation(grpcOptions =>
-            {
-                grpcOptions.EnrichWithHttpRequestMessage = (activity, httpRequestMessage) =>
-                    activity.SetTag("requestVersion", httpRequestMessage.Version);
-                grpcOptions.EnrichWithHttpResponseMessage = (activity, httpResponseMessage) =>
-                    activity.SetTag("responseVersion", httpResponseMessage.Version);
-                grpcOptions.SuppressDownstreamInstrumentation = true;
-            })
+            .AddGrpcClientInstrumentation()
             .AddHttpClientInstrumentation()
             .AddAspNetCoreInstrumentation()
             .AddNpgsql()
