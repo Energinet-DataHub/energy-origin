54f6a38ef6b843ff3b68d99bd1dd6dc1d92909ee
sahma19 <saiuan@msn.com>
2024-02-04 11:35:27 +0100

    Add Grpc instrumentation for RegistryConnector

--- domains/certificates/RegistryConnector/Worker/Program.cs
+++ domains/certificates/RegistryConnector/Worker/Program.cs
@@ -126,6 +126,14 @@ builder.Services.AddOpenTelemetry()
             .AddOtlpExporter(o => o.Endpoint = otlpOptions.ReceiverEndpoint))
     .WithTracing(tracerProviderBuilder =>
         tracerProviderBuilder
+            .AddGrpcClientInstrumentation(grpcOptions =>
+            {
+                grpcOptions.EnrichWithHttpRequestMessage = (activity, httpRequestMessage) =>
+                    activity.SetTag("requestVersion", httpRequestMessage.Version);
+                grpcOptions.EnrichWithHttpResponseMessage = (activity, httpResponseMessage) =>
+                    activity.SetTag("responseVersion", httpResponseMessage.Version);
+                grpcOptions.SuppressDownstreamInstrumentation = true;
+            })
             .AddHttpClientInstrumentation()
             .AddAspNetCoreInstrumentation()
             .AddNpgsql()
