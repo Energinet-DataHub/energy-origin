5039d25158d25daf4e8afb4637dfe4f83f347956
sahma19 <saiuan@msn.com>
2024-02-20 11:20:16 +0100

    dotnet format

--- domains/certificates/Query.API/API/Query.API/Swagger/ConfigureSwaggerOptions.cs
+++ domains/certificates/Query.API/API/Query.API/Swagger/ConfigureSwaggerOptions.cs
@@ -28,17 +28,17 @@ public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
         options.DocumentFilter<AddContractsTagDocumentFilter>();
 
 
-            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Name = "Authorization",
-                Type = SecuritySchemeType.ApiKey,
-                Scheme = "Bearer",
-                BearerFormat = "JWT",
-                In = ParameterLocation.Header,
-                Description =
-                    "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
-            });
-            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description =
+                "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\""
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
             {
                 {
                     new OpenApiSecurityScheme
