39939d540230cd37fc7ac0d570d0d13978e92028
Christoffer Frydkjær <christofferfk@gmail.com>
2024-02-28 15:49:12 +0100

    add nuget (#693)
    Co-authored-by: sahma19 <shd@energinet.dk>

---
+++ libraries/dotnet/EnergyOrigin.IntegrationEvents/EnergyOrigin.IntegrationEvents.Tests/Events/OrgAcceptedTerms.cs
@@ -0,0 +1,17 @@
+using EnergyOrigin.IntegrationEvents.Events;
+
+namespace EnergyOrigin.IntegrationEvents.Tests.Events;
+
+public class OrgAcceptedTermsTests
+{
+    [Fact]
+    public void can_set_values()
+    {
+        var subjectId = Guid.NewGuid();
+        var tin = "1234567890";
+        var orgAcceptedTerms = new OrgAcceptedTerms(subjectId, tin);
+
+        Assert.Equal(subjectId, orgAcceptedTerms.SubjectId);
+        Assert.Equal(tin, orgAcceptedTerms.Tin);
+    }
+}
