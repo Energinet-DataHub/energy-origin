ab742ef2ed8b8f9731e4fe429c574fa3cffd508e
Martin Henning Jensen <mhj@mjolner.dk>
2024-02-28 14:31:43 +0000

    add support for missing quantity measurements
    Extend MeasurementSyncerWorker to support measurements marked with
    missing quantity. Info about missing measurements is persisted and data
    fetch is retried later.

--- domains/certificates/Query.API/API/MeasurementsSyncer/Persistence/ISyncState.cs
+++ domains/certificates/Query.API/API/MeasurementsSyncer/Persistence/ISyncState.cs
@@ -1,9 +1,20 @@
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using DataContext.Models;
 
 namespace API.MeasurementsSyncer.Persistence;
 
 public interface ISyncState
 {
     Task<long?> GetPeriodStartTime(MeteringPointSyncInfo syncInfo);
+
     Task SetSyncPosition(string gsrn, long syncedTo);
+
+    Task<MeteringPointTimeSeriesSlidingWindow?> GetMeteringPointSlidingWindow(string gsrn);
+
+    Task UpdateSlidingWindow(MeteringPointTimeSeriesSlidingWindow slidingWindow);
+
+    Task<IReadOnlyList<MeteringPointSyncInfo>> GetSyncInfos(CancellationToken cancellationToken);
+
 }
--- domains/certificates/Query.API/API/MeasurementsSyncer/Startup.cs
+++ domains/certificates/Query.API/API/MeasurementsSyncer/Startup.cs
@@ -14,6 +14,7 @@ public static class Startup
         services.AddMeasurementsOptions();
 
         services.AddSingleton<MeasurementsSyncService>();
+        services.AddSingleton<SlidingWindowService>();
         services.AddSingleton<ISyncState, SyncState>();
 
         services.AddHostedService<MeasurementsSyncerWorker>();
