b486440d74171737a1165b3502a36a43c21f9492
Theis Nickelsen <130448180+tnickelsen@users.noreply.github.com>
2024-03-11 10:07:47 +0100

    Vcluster/2361 shared nuget for setting up EnergyOrigin solutions (#783)

---
+++ libraries/dotnet/EnergyOrigin.Setup/EnergyOrigin.Setup/IServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+using System.Text.Json.Serialization;
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using EnergyOrigin.Setup.Swagger;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace EnergyOrigin.Setup;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddSwagger(this IServiceCollection services, string subsystemName)
+    {
+        services.AddTransient<IConfigureOptions<SwaggerGenOptions>>(s =>
+            new ConfigureSwaggerOptions(s.GetRequiredService<IApiVersionDescriptionProvider>(), subsystemName));
+    }
+
+    public static void AddApiVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = false;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = new HeaderApiVersionReader("EO_API_VERSION");
+            })
+            .AddMvc()
+            .AddApiExplorer();
+    }
+
+    public static void AddControllersWithEnumsAsStrings(this IServiceCollection services)
+    {
+        services.AddControllers()
+            .AddJsonOptions(o =>
+                o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+    }
+}
