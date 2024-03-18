98e1557f5ea539aaeb3ac2e0d33f6d0de22f7b5e
tnickelsen <TNE@energinet.dk>
2024-03-18 10:29:24 +0100

    unit tests in progress

---
+++ domains/transfer/Test/Transfer.Application.Tests/Commands/CreateTransferAgreementProposalCommandTests.cs
@@ -0,0 +1,60 @@
+using System.Threading.Tasks;
+using Xunit;
+using Transfer.Application.Commands;
+using Transfer.Application.Repositories;
+using NSubstitute;
+using EnergyOrigin.ActivityLog.API;
+using System;
+using Transfer.Domain.Entities;
+using FluentAssertions;
+using Transfer.Application.Exceptions;
+using NSubstitute.ExceptionExtensions;
+
+namespace Transfer.Application.Tests.Commands;
+
+public class CreateTransferAgreementProposalCommandTests
+{
+    [Fact]
+    public async Task ShouldCreateTransferAgreementProposal()
+    {
+        var taRepo = Substitute.For<ITransferAgreementRepository>();
+        var taProposalRepo = Substitute.For<ITransferAgreementProposalRepository>();
+        var alRepo = Substitute.For<IActivityLogEntryRepository>();
+        var userContext = Substitute.For<IUserContext>();
+
+        taRepo.HasDateOverlap(Arg.Any<TransferAgreementProposal>()).Returns(false);
+
+        var handler = new CreateTransferAgreementProposalCommandHandler(taRepo, taProposalRepo, alRepo, userContext);
+        var cmd = new CreateTransferAgreementProposalCommand(
+            startDate: DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+            endDate: null,
+            receiverTin: "12345678");
+
+        var response = await handler.Handle(cmd, default);
+
+        response.StartDate.Should().Be(cmd.StartDate);
+        response.EndDate.Should().Be(cmd.EndDate);
+        response.ReceiverCompanyTin.Should().Be(cmd.ReceiverTin);
+    }
+
+    [Fact]
+    public Task ShouldThrowExceptionWhenDateOverlap()
+    {
+        var taRepo = Substitute.For<ITransferAgreementRepository>();
+        var taProposalRepo = Substitute.For<ITransferAgreementProposalRepository>();
+        var alRepo = Substitute.For<IActivityLogEntryRepository>();
+        var userContext = Substitute.For<IUserContext>();
+
+        taRepo.HasDateOverlap(Arg.Any<TransferAgreementProposal>()).Returns(true);
+
+        var handler = new CreateTransferAgreementProposalCommandHandler(taRepo, taProposalRepo, alRepo, userContext);
+        var cmd = new CreateTransferAgreementProposalCommand(
+                       startDate: DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                                  endDate: null,
+                                  receiverTin: "12345678");
+
+        var action = () => handler.Handle(cmd, default);
+
+        //action.Should().Throw<TransferAgreementOverlapsException>();
+    }
+}
