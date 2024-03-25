a8f561f4ea7d0af568a1a582320b68d4188e44b1
cfk <cfk@energinet.dk>
2024-03-25 10:52:56 +0100

    add relation check

--- domains/measurements/Measurements.API/API/MeteringPoints/Api/Dto/Responses/GetMeteringPointsResponse.cs
+++ domains/measurements/Measurements.API/API/MeteringPoints/Api/Dto/Responses/GetMeteringPointsResponse.cs
@@ -3,4 +3,4 @@ using API.MeteringPoints.Api.Models;
 
 namespace API.MeteringPoints.Api.Dto.Responses;
 
-public record GetMeteringPointsResponse(List<MeteringPoint> Result, RelationStatus? Status);
+public record GetMeteringPointsResponse(List<MeteringPoint> Result, RelationStatus Status);
