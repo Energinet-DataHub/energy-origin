7c3264e616947493ac6bd0ffe9aeffd964c6cb2b
tnickelsen <TNE@energinet.dk>
2024-03-27 14:29:55 +0100

    Added unit of work to certificates

--- domains/certificates/Query.API/API/ContractService/IContractService.cs
+++ domains/certificates/Query.API/API/ContractService/IContractService.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using DataContext.Models;
+using EnergyOrigin.TokenValidation.Utilities;
 
 namespace API.ContractService;
 
 public interface IContractService
 {
-    Task<CreateContractResult> Create(string gsrn, string meteringPointOwner, DateTimeOffset startDate, DateTimeOffset? endDate, CancellationToken cancellationToken);
-    Task<SetEndDateResult> SetEndDate(Guid id, string meteringPointOwner, DateTimeOffset? newEndDate, CancellationToken cancellationToken);
+    Task<CreateContractResult> Create(string gsrn, UserDescriptor user, DateTimeOffset startDate, DateTimeOffset? endDate, CancellationToken cancellationToken);
+    Task<SetEndDateResult> SetEndDate(Guid id, UserDescriptor user, DateTimeOffset? newEndDate, CancellationToken cancellationToken);
     Task<IReadOnlyList<CertificateIssuingContract>> GetByOwner(string meteringPointOwner, CancellationToken cancellationToken);
     Task<CertificateIssuingContract?> GetById(Guid id, string meteringPointOwner, CancellationToken cancellationToken);
     Task<IReadOnlyList<CertificateIssuingContract>> GetByGSRN(string gsrn, CancellationToken cancellationToken);
--- domains/certificates/Query.API/API/ContractService/Repositories/ICertificateIssuingContractRepository.cs
+++ domains/certificates/Query.API/API/ContractService/Repositories/ICertificateIssuingContractRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using DataContext.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace API.ContractService.Repositories;
 
 public interface ICertificateIssuingContractRepository
 {
-    Task Save(CertificateIssuingContract certificateIssuingContract);
-    Task Update(CertificateIssuingContract certificateIssuingContract);
+    ValueTask<EntityEntry<CertificateIssuingContract>> Save(CertificateIssuingContract certificateIssuingContract);
+    void Update(CertificateIssuingContract certificateIssuingContract);
     Task<IReadOnlyList<CertificateIssuingContract>> GetByGsrn(string gsrn, CancellationToken cancellationToken);
     Task<IReadOnlyList<CertificateIssuingContract>> GetAllMeteringPointOwnerContracts(string meteringPointOwner, CancellationToken cancellationToken);
     Task<CertificateIssuingContract?> GetById(Guid id, CancellationToken cancellationToken);
