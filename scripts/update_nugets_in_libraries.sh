ab6aa257936beea763712b86642118ac6ae3f1a3
sahma19 <saiuan@msn.com>
2024-04-03 10:08:41 +0200

    finished script

--- scripts/update_nugets_in_libraries.sh
+++ scripts/update_nugets_in_libraries.sh
@@ -1,23 +1,50 @@
-﻿#!/bin/bash
+﻿#!/bin/sh
 
-cd .. || exit
+# Ensure dotnet-outdated is installed or updated
+if dotnet tool list -g | grep -q 'dotnet-outdated'; then
+ echo "dotnet-outdated is already installed. Updating..."
+ dotnet tool update --global dotnet-outdated
+else
+ echo "Installing dotnet-outdated..."
+ dotnet tool install --global dotnet-outdated
+fi
 
+echo "Navigating to the base directory..."
+# Move up one directory level from the current script location
+cd .. || { echo "Failed to navigate to the base directory"; exit 1; }
+
+echo "Starting to find .sln files in libraries/dotnet..."
+# Find all .sln files in libraries/dotnet and its subdirectories
 find libraries/dotnet -type f -name '*.sln' | while read -r slnFile; do
+ echo "Processing solution file: $slnFile"
+ # Navigate to the .sln file directory
+ slnDir=$(dirname "$slnFile")
+ cd "$slnDir" || { echo "Failed to navigate to solution directory: $slnDir"; exit 1; }
 
-  slnDir=$(dirname "$slnFile")
-  cd "$slnDir" || exit
+ echo "Running dotnet outdated..."
+ # Update using dotnet outdated and capture the output
+ outdatedOutput=$(dotnet outdated -u)
 
-  dotnet outdated -u
+ # Check if no dependencies were updated
+ if echo "$outdatedOutput" | grep -q "No outdated dependencies were detected"; then
+    echo "No dependencies were updated for $slnFile. Skipping version increment."
+    # Skip to the next iteration of the loop, effectively skipping the version increment step
+    cd - > /dev/null || { echo "Failed to navigate back"; exit 1; }
+    continue
+ fi
 
-  configFile="${slnDir}/configuration.yaml"
-  if [ -f "$configFile" ]; then
+ echo "Dependencies were updated for $slnFile. Incrementing version in configuration.yaml..."
+ # Find the configuration.yaml file in the same directory as the .sln file
+ configFile=$(find . -maxdepth 1 -type f -name 'configuration.yaml')
+ if [ -n "$configFile" ]; then
+    # Read the current version, increment the patch version, and write to a temporary file
+    tempFile=$(mktemp)
+    awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}' "$configFile" > "$tempFile"
 
-    awk '/version:/ {
-      match($0, /([0-9]+\.){2}[0-9]+/, arr)
-      split(arr[0], ver, ".")
-      $0 = gensub(/([0-9]+\.){2}[0-9]+/, ver[1]"."ver[2]"."ver[3]+1, 1)
-    } {print}' "$configFile" > tempFile && mv tempFile "$configFile"
-  fi
+    # Replace the original file with the updated temporary file
+    mv "$tempFile" "$configFile"
+ fi
 
-  cd - > /dev/null || exit
+ # Return to the original directory before processing the next .sln file
+ cd - > /dev/null || { echo "Failed to navigate back"; exit 1; }
 done
