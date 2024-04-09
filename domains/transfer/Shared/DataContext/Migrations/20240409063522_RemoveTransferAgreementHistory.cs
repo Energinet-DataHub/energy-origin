56c79dd7b9f7a37175769aec0c0569a651deebff
cfk <cfk@energinet.dk>
2024-04-09 08:37:15 +0200

    drop table

---
+++ domains/transfer/Shared/DataContext/Migrations/20240409063522_RemoveTransferAgreementHistory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveTransferAgreementHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransferAgreementHistoryEntries");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransferAgreementHistoryEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TransferAgreementId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ActorId = table.Column<string>(type: "text", nullable: false),
+                    ActorName = table.Column<string>(type: "text", nullable: false),
+                    SenderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SenderName = table.Column<string>(type: "text", nullable: false),
+                    SenderTin = table.Column<string>(type: "text", nullable: false),
+                    ReceiverTin = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    AuditAction = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransferAgreementHistoryEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TransferAgreementHistoryEntries_TransferAgreements_Transfer~",
+                        column: x => x.TransferAgreementId,
+                        principalTable: "TransferAgreements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+    }
+}
