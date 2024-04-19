5c87cbeac6f3628cbf5f4668e4ed5cb67c5c81c0
cfk <cfk@energinet.dk>
2024-04-19 14:15:39 +0200

    remove down migration

--- domains/transfer/Shared/DataContext/Migrations/20240409063522_RemoveTransferAgreementHistory.cs
+++ domains/transfer/Shared/DataContext/Migrations/20240409063522_RemoveTransferAgreementHistory.cs
@@ -18,33 +18,7 @@ namespace DataContext.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "TransferAgreementHistoryEntries",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
-                    TransferAgreementId = table.Column<Guid>(type: "uuid", nullable: false),
-                    StartDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
-                    EndDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
-                    ActorId = table.Column<string>(type: "text", nullable: false),
-                    ActorName = table.Column<string>(type: "text", nullable: false),
-                    SenderId = table.Column<Guid>(type: "uuid", nullable: false),
-                    SenderName = table.Column<string>(type: "text", nullable: false),
-                    SenderTin = table.Column<string>(type: "text", nullable: false),
-                    ReceiverTin = table.Column<string>(type: "text", nullable: false),
-                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
-                    AuditAction = table.Column<string>(type: "text", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_TransferAgreementHistoryEntries", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_TransferAgreementHistoryEntries_TransferAgreements_Transfer~",
-                        column: x => x.TransferAgreementId,
-                        principalTable: "TransferAgreements",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            
         }
     }
 }
