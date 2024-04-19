9684cd56d038f59a7360a6537d4defabac2eb10e
tnickelsen <TNE@energinet.dk>
2024-04-19 15:25:12 +0200

    reverted

--- domains/certificates/Query.API/API.IntegrationTests/CertificateIssuingFailingTests.cs
+++ domains/certificates/Query.API/API.IntegrationTests/CertificateIssuingFailingTests.cs
@@ -55,7 +55,7 @@ public sealed class CertificateIssuingFailingTests :
     }
 
     [Fact]
-    public async Task MeasurementFromProductionMeteringPointAddedToBus_WhenRegistryIsDown_RejectedCertificate()
+    public async Task MeasurementFromProductionMeteringPointAddedToBus_WhenRegistryIsDown_RejectCertificate()
     {
         var subject = Guid.NewGuid().ToString();
         var gsrn = GsrnHelper.GenerateRandom();
