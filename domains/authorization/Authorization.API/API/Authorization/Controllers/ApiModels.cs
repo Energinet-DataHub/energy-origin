ee80bd44cb06c246c8b33999b9629ae8061c21e9
TopSwagCode <josh@topswagcode.com>
2024-04-24 10:19:19 +0200

    MVP endpoints

---
+++ domains/authorization/Authorization.API/API/Authorization/Controllers/ApiModels.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace API.Authorization.Controllers;
+
+public record AuthorizationUserRequest(
+    [property: JsonPropertyName("sub")] string Sub,
+    [property: JsonPropertyName("orgId")] string OrgId,
+    [property: JsonPropertyName("name")] string Name
+);
+
+public record AuthorizationClientRequest(
+    [property: JsonPropertyName("sub")] string Sub
+);
+
+public record AuthorizationResponse(string Sub, string Name, string SubType, IEnumerable<string> OrgIds, string Scope);
