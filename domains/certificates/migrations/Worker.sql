6600a36f3d4d86009f4f1d121ec5ec1e71b8b11f
tnickelsen <TNE@energinet.dk>
2024-04-24 10:49:24 +0200

    sql

--- domains/certificates/migrations/Worker.sql
+++ domains/certificates/migrations/Worker.sql
@@ -469,22 +469,3 @@ BEGIN
 END $EF$;
 COMMIT;
 
-START TRANSACTION;
-
-
-DO $EF$
-BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240423100351_ModifyWalletUrlsOnContracts') THEN
-    UPDATE public."Contracts" SET "WalletUrl" = REPLACE("WalletUrl", '/wallet-api', '/v1/slices');
-    END IF;
-END $EF$;
-
-DO $EF$
-BEGIN
-    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20240423100351_ModifyWalletUrlsOnContracts') THEN
-    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-    VALUES ('20240423100351_ModifyWalletUrlsOnContracts', '8.0.4');
-    END IF;
-END $EF$;
-COMMIT;
-
