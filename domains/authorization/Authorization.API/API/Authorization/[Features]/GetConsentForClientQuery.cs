5f38e0a90f4bee0764c8e59b49d66a6c51d9794e
TopSwagCode <josh@topswagcode.com>
2024-04-25 09:29:19 +0200

    Change OrgId to OrgCvr

--- domains/authorization/Authorization.API/API/Authorization/[Features]/GetConsentForClientQuery.cs
+++ domains/authorization/Authorization.API/API/Authorization/[Features]/GetConsentForClientQuery.cs
@@ -11,12 +11,12 @@ public class GetConsentForClientQueryHandler : IRequestHandler<GetConsentForClie
 {
     public async Task<GetConsentForClientQueryResult> Handle(GetConsentForClientQuery query, CancellationToken cancellationToken)
     {
-        if (query.Sub.Equals("529a55d0-68c7-4129-ba3c-e06d4f1038c4"))
-            return new(query.Sub, "Granular System", "External", "Granular", new[] { "123456789", Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString() }, "dashboard production meters certificates wallet");
+        if (query.ClientId.Equals("529a55d0-68c7-4129-ba3c-e06d4f1038c4"))
+            return new(query.ClientId, "Granular System", "External", "Granular", new[] { "123456789", Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Guid.NewGuid().ToString() }, "dashboard production meters certificates wallet");
 
         return new(default, default, default, default, default, default);
     }
 }
 
-public record GetConsentForClientQuery(string Sub) : IRequest<GetConsentForClientQueryResult>;
+public record GetConsentForClientQuery(string ClientId) : IRequest<GetConsentForClientQueryResult>;
 public record GetConsentForClientQueryResult(string Sub, string Name, string SubType, string OrgName, IEnumerable<string> OrgIds, string Scope);
