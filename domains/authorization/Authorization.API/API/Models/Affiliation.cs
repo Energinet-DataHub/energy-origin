323c02c881b2556be4e3881b9288a2c0ba4b93b3
sahma19 <saiuan@msn.com>
2024-04-27 17:20:06 +0200

    add required

--- domains/authorization/Authorization.API/API/Models/Affiliation.cs
+++ domains/authorization/Authorization.API/API/Models/Affiliation.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Models;
 
@@ -24,7 +25,9 @@ public class Affiliation
     }
 
     public Guid Id { get; set; }
+    [Required]
     public Guid UserId { get; set; }
+    [Required]
     public Guid OrganizationId { get; set; }
     public Organization Organization { get; set; } = null!;
     public User User { get; set; } = null!;
