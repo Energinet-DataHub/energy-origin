a5d47a846b9ac8a29fde43ba0ecbd993953ea687
sahma19 <saiuan@msn.com>
2024-05-18 11:44:15 +0200

    null reference warning

--- domains/authorization/Proxy.IntegrationTests/ProxyIntegrationTests.cs
+++ domains/authorization/Proxy.IntegrationTests/ProxyIntegrationTests.cs
@@ -1,38 +1,27 @@
 ﻿using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 
-namespace Proxy.IntegrationTests
+namespace Proxy.IntegrationTests;
+
+public class ProxyIntegrationTests(IntegrationTestFixture fixture) : IClassFixture<IntegrationTestFixture>
 {
-    public class ProxyIntegrationTests : IClassFixture<IntegrationTestFixture>
+    [Fact]
+    public async Task TokenModificationMiddleware_ModifiesToken_WhenOrganizationIdExists()
     {
-        private readonly IntegrationTestFixture _fixture;
+        var wireMockServer = fixture.WalletWireMockServer;
+        wireMockServer.Given(
+            Request.Create().WithPath("/wallet-api/v1/test").UsingGet()
+        ).RespondWith(
+            Response.Create().WithStatusCode(200)
+        );
 
-        public ProxyIntegrationTests(IntegrationTestFixture fixture)
-        {
-            _fixture = fixture;
-        }
+        var client = fixture.Factory.CreateAuthenticatedClient("123");
 
-        [Fact]
-        public async Task TokenModificationMiddleware_ModifiesToken_WhenOrganizationIdExists()
-        {
-            // Arrange
-            var wireMockServer = _fixture.WalletWireMockServer;
-            wireMockServer.Given(
-                Request.Create().WithPath("/wallet-api/v1/test").UsingGet()
-            ).RespondWith(
-                Response.Create().WithStatusCode(200)
-            );
+        var response = await client.GetAsync("/wallet-api/v1/test?organizationId=123");
 
-            var client = _fixture.Factory.CreateAuthenticatedClient("123");
-
-            // Act
-            var response = await client.GetAsync("/wallet-api/v1/test?organizationId=123");
-
-            // Assert
-            Assert.True(response.IsSuccessStatusCode);
-            var requests = wireMockServer.LogEntries;
-            var requestHeader = requests.First().RequestMessage.Headers["Authorization"];
-            Assert.Equal("Bearer newToken", requestHeader);
-        }
+        Assert.True(response.IsSuccessStatusCode);
+        var requests = wireMockServer.LogEntries;
+        var requestHeader = requests.First().RequestMessage.Headers!["Authorization"];
+        Assert.Equal("Bearer newToken", requestHeader);
     }
 }
