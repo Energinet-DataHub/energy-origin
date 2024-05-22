da6b59721ec76e5295cb6e855062cebf5e9a1e81
sahma19 <saiuan@msn.com>
2024-05-22 14:32:46 +0200

    empty line

--- domains/authorization/Proxy.IntegrationTests/ProjectOriginStackTests.cs
+++ domains/authorization/Proxy.IntegrationTests/ProjectOriginStackTests.cs
@@ -3,49 +3,47 @@ using System.Text;
 using FluentAssertions;
 using Proxy.IntegrationTests.Testcontainers;
 
-namespace Proxy.IntegrationTests
+namespace Proxy.IntegrationTests;
+
+public class ProjectOriginStackTests : IAsyncLifetime
 {
-    public class ProjectOriginStackTests : IAsyncLifetime
+    private readonly ProjectOriginStack _projectOriginStack = new();
+
+    [Fact]
+    public async Task Confirm_WalletSetup_returns401_ifNoHeader()
     {
-        private readonly ProjectOriginStack _projectOriginStack = new();
+        var walletUrl = _projectOriginStack.WalletUrl;
+        var certificatesEndpoint = $"{walletUrl}v1/certificates";
 
-        [Fact]
-        public async Task Confirm_WalletSetup_returns401_ifNoHeader()
-        {
-            var walletUrl = _projectOriginStack.WalletUrl;
-            var certificatesEndpoint = $"{walletUrl}v1/certificates";
+        using var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(certificatesEndpoint);
 
-            using var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(certificatesEndpoint);
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
 
-            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
-        }
+    [Fact]
+    public async Task Confirm_WalletSetup_returns201_IfReceivingHeader()
+    {
+        var walletUrl = _projectOriginStack.WalletUrl;
+        var walletEndpoint = $"{walletUrl}v1/wallets";
+        var organizationId = Guid.NewGuid().ToString();
 
-        [Fact]
-        public async Task Confirm_WalletSetup_returns201_IfReceivingHeader()
-        {
-            var walletUrl = _projectOriginStack.WalletUrl;
-            var walletEndpoint = $"{walletUrl}v1/wallets";
-            var organizationId = Guid.NewGuid().ToString();
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("wallet-owner", organizationId);
+        using var content = new StringContent("{}", Encoding.UTF8, "application/json");
 
-            using var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("wallet-owner", organizationId);
-            using var content = new StringContent("{}", Encoding.UTF8, "application/json");
+        var response = await httpClient.PostAsync(walletEndpoint, content);
 
-            var response = await httpClient.PostAsync(walletEndpoint, content);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
 
-            response.StatusCode.Should().Be(HttpStatusCode.Created);
-        }
+    public async Task InitializeAsync()
+    {
+        await _projectOriginStack.InitializeAsync();
+    }
 
-
-        public async Task InitializeAsync()
-        {
-            await _projectOriginStack.InitializeAsync();
-        }
-
-        public async Task DisposeAsync()
-        {
-            await _projectOriginStack.DisposeAsync();
-        }
+    public async Task DisposeAsync()
+    {
+        await _projectOriginStack.DisposeAsync();
     }
 }
