164c85be649139393a717ac9f2267a993c6f77b8
Christoffer Frydkjær <christofferfk@gmail.com>
2024-06-10 15:21:15 +0200

    wip

---
+++ domains/certificates/Query.API/API.IntegrationTests/MeasurementSyncer/MeasurementSyncerServiceTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using DataContext;
+using DataContext.Models;
+using DataContext.ValueObjects;
+using MassTransit;
+using MassTransit.RabbitMqTransport;
+using MeasurementEvents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectOrigin.HierarchicalDeterministicKeys.Implementations;
+using Testing.Helpers;
+using Xunit;
+
+namespace API.IntegrationTests.MeasurementSyncer;
+
+[Collection(IntegrationTestCollection.CollectionName)]
+public class MeasurementSyncerServiceTest
+{
+    private readonly IntegrationTestFixture integrationTestFixture;
+    private readonly DbContextOptions<ApplicationDbContext> options;
+
+    public MeasurementSyncerServiceTest(IntegrationTestFixture integrationTestFixture)
+    {
+        this.integrationTestFixture = integrationTestFixture;
+        var emptyDb = integrationTestFixture.PostgresContainer.CreateNewDatabase().Result;
+        options = new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql(emptyDb.ConnectionString).Options;
+        using var dbContext = new ApplicationDbContext(options);
+        dbContext.Database.EnsureCreated();
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        var gsrn = GsrnHelper.GenerateRandom();
+        var now = DateTimeOffset.UtcNow;
+        var utcMidnight = now.Subtract(now.TimeOfDay);
+
+        var measurement = new EnergyMeasuredIntegrationEvent(
+            GSRN: gsrn,
+            DateFrom: utcMidnight.ToUnixTimeSeconds(),
+            DateTo: utcMidnight.AddHours(1).ToUnixTimeSeconds(),
+            Quantity: 42,
+            Quality: MeasurementQuality.Measured);
+
+        await using (var dbContext = new ApplicationDbContext(options))
+        {
+            dbContext.Add(MeteringPointTimeSeriesSlidingWindow.Create(gsrn, UnixTimestamp.Create(now)));
+            using var scope = integrationTestFixture.WebApplicationFactory.Services.CreateScope();
+            scope.ServiceProvider.GetRequiredService<IPublishEndpoint>().Publish(measurement);
+
+            await Task.Delay(TimeSpan.FromSeconds(15));
+
+            await dbContext.SaveChangesAsync();
+        }
+
+    }
+}
--- domains/certificates/Query.API/API/MeasurementsSyncer/Persistence/ISyncState.cs
+++ domains/certificates/Query.API/API/MeasurementsSyncer/Persistence/ISyncState.cs
@@ -9,12 +9,10 @@ public interface ISyncState
 {
     Task<long?> GetPeriodStartTime(MeteringPointSyncInfo syncInfo, CancellationToken cancellationToken);
 
-    Task SetSyncPosition(string gsrn, long syncedTo, CancellationToken cancellationToken);
-
     Task<MeteringPointTimeSeriesSlidingWindow?> GetMeteringPointSlidingWindow(string gsrn, CancellationToken cancellationToken);
 
     Task UpdateSlidingWindow(MeteringPointTimeSeriesSlidingWindow slidingWindow, CancellationToken cancellationToken);
 
-
+    Task SaveChangesAsync(CancellationToken cancellationToken);
 
 }
