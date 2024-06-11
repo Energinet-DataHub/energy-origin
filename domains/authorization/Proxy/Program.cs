a529103b31022413dff979fad2913df1c2300f67
TopSwagCode <josh@topswagcode.com>
2024-06-11 10:52:31 +0200

    Moved classes to own file

--- domains/authorization/Proxy/Program.cs
+++ domains/authorization/Proxy/Program.cs
@@ -5,10 +5,7 @@ using EnergyOrigin.Setup;
 using EnergyOrigin.TokenValidation.b2c;
 using EnergyOrigin.TokenValidation.Options;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 using Proxy.Options;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -107,38 +104,6 @@ app.MapSwagger();
 
 app.Run();
 
-
-public static class ServiceCollectionExtensions
-{
-    public static OptionsBuilder<T> AttachOptions<T>(this IServiceCollection services) where T : class
-    {
-        var builder = services.AddOptions<T>();
-        services.AddTransient(x => x.GetRequiredService<IOptions<T>>().Value);
-        return builder;
-    }
-}
-
-public class WalletTagDocumentFilter : IDocumentFilter
-{
-    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
-    {
-        // Check if the "Contracts" tag already exists to avoid duplicates
-        if (!swaggerDoc.Tags.Any(tag => tag.Name == "Wallet"))
-        {
-            swaggerDoc.Tags.Add(new OpenApiTag
-            {
-                Name = "Wallet",
-                Description = "The Wallet is essential for Energy Origin," +
-                              " since it keeps track of all the user’s Granular Certificates" +
-                              " – both the ones generated from the user’s own metering points," +
-                              " but also the ones transferred from other users." +
-                              " In other words, the Wallet will hold all available certificates for the user." +
-                              " Moreover, it will show all transfers, that may have been made," +
-                              " to other users’ wallets as well.\n"
-            });
-        }
-    }
-}
 
 public partial class Program
 {
---
+++ domains/authorization/Proxy/WalletTagDocumentFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+public class WalletTagDocumentFilter : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        // Check if the "Contracts" tag already exists to avoid duplicates
+        if (!swaggerDoc.Tags.Any(tag => tag.Name == "Wallet"))
+        {
+            swaggerDoc.Tags.Add(new OpenApiTag
+            {
+                Name = "Wallet",
+                Description = "The Wallet is essential for Energy Origin," +
+                              " since it keeps track of all the user’s Granular Certificates" +
+                              " – both the ones generated from the user’s own metering points," +
+                              " but also the ones transferred from other users." +
+                              " In other words, the Wallet will hold all available certificates for the user." +
+                              " Moreover, it will show all transfers, that may have been made," +
+                              " to other users’ wallets as well.\n"
+            });
+        }
+    }
+}
