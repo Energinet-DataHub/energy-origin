f56d6311de4e9e2b135e36a359d954f65d36dc26
Martin Henning Jensen <mhj@mjolner.dk>
2024-06-14 08:06:08 +0100

    add injectable identity- and access-descriptors

--- libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/B2C/ForbiddenException.cs
+++ libraries/dotnet/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/B2C/ForbiddenException.cs
@@ -2,6 +2,11 @@ namespace EnergyOrigin.TokenValidation.b2c;
 
 public class ForbiddenException : Exception
 {
+    public ForbiddenException(string msg) : base(msg)
+    {
+
+    }
+
     public ForbiddenException(Guid orgId) : base($"Not authorized to access organization with id {orgId}")
     {
 
