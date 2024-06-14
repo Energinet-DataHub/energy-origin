6cd87243ba8618cda86b228c3d8b7da0ea2b733b
sahma19 <saiuan@msn.com>
2024-06-14 11:06:39 +0200

    rename DTO for clarity

--- domains/authorization/Authorization.API/API/Authorization/[Features]/GetConsentQuery.cs
+++ domains/authorization/Authorization.API/API/Authorization/[Features]/GetConsentQuery.cs
@@ -20,7 +20,7 @@ public class GetConsentQueryHandler(IClientRepository clientRepository)
         var consent = await clientRepository.Query()
             .Where(client => client.IdpClientId == idpClientId)
             .SelectMany(client => client.Consents.Select(consent =>
-                new ConsentDto(client.IdpClientId, consent.Organization.Name, client.RedirectUrl)))
+                new GetConsentQueryResultItem(client.IdpClientId, consent.Organization.Name, client.RedirectUrl)))
             .ToListAsync(cancellationToken);
 
         return new GetConsentsQueryResult(consent);
@@ -29,6 +29,6 @@ public class GetConsentQueryHandler(IClientRepository clientRepository)
 
 public record GetConsentQuery(Guid IdpClientId) : IRequest<GetConsentsQueryResult>;
 
-public record GetConsentsQueryResult(List<ConsentDto> Result);
+public record GetConsentsQueryResult(List<GetConsentQueryResultItem> Result);
 
-public record ConsentDto(IdpClientId IdpClientId, OrganizationName OrganizationName, string RedirectUrl);
+public record GetConsentQueryResultItem(IdpClientId IdpClientId, OrganizationName OrganizationName, string RedirectUrl);
