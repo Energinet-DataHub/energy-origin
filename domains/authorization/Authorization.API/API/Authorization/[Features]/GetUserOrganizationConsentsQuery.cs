e7d3caf3815c6cad7eda3b61a0895408b5d0733a
sahma19 <saiuan@msn.com>
2024-06-14 11:08:35 +0200

    Go directly through clients table by doing:
    Clients --> Consents Where Organization --> User.IdpUserId

--- domains/authorization/Authorization.API/API/Authorization/[Features]/GetUserOrganizationConsentsQuery.cs
+++ domains/authorization/Authorization.API/API/Authorization/[Features]/GetUserOrganizationConsentsQuery.cs
@@ -10,32 +10,27 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Authorization._Features_;
 
-public class GetUserOrganizationConsentsQueryHandler(IUserRepository userRepository)
+public class GetUserOrganizationConsentsQueryHandler(IClientRepository clientRepository)
     : IRequestHandler<GetUserOrganizationConsentsQuery, GetUserOrganizationConsentsQueryResult>
 {
     public async Task<GetUserOrganizationConsentsQueryResult> Handle(GetUserOrganizationConsentsQuery request, CancellationToken cancellationToken)
     {
         var idpUserId = IdpUserId.Create(Guid.Parse(request.IdpUserId));
 
-        var user = await userRepository.Query()
-            .Include(u => u.Affiliations)
-            .ThenInclude(a => a.Organization)
-            .ThenInclude(o => o.Consents)
-            .FirstOrDefaultAsync(u => u.IdpUserId == idpUserId, cancellationToken);
+        var clients = await clientRepository
+            .Query()
+            .Where(client => client.Consents
+                .Any(consent => consent.Organization.Affiliations
+                    .Any(o => o.User.IdpUserId == idpUserId)))
+            .Select(client => new GetUserOrganizationConsentsQueryResultItem(client.Name.Value))
+            .ToListAsync(cancellationToken: cancellationToken);
 
-        if (user == null)
-            throw new Exception("User not found");
-
-        var organization = user.Affiliations.Single().Organization;
-
-        var consents = organization.Consents.Select(c =>
-                new ConsentDto(c.Client.IdpClientId, organization.Name, c.Client.RedirectUrl))
-            .ToList();
-
-        return new GetUserOrganizationConsentsQueryResult(consents);
+        return new GetUserOrganizationConsentsQueryResult(clients);
     }
 }
 
 public record GetUserOrganizationConsentsQuery(string IdpUserId) : IRequest<GetUserOrganizationConsentsQueryResult>;
 
-public record GetUserOrganizationConsentsQueryResult(List<ConsentDto> Result);
+public record GetUserOrganizationConsentsQueryResult(List<GetUserOrganizationConsentsQueryResultItem> Result);
+
+public record GetUserOrganizationConsentsQueryResultItem(string Name);
