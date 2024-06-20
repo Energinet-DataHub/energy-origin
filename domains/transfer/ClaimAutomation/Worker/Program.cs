68e896c7cc6866caaef76fd84ce644a4a8c91a79
TopSwagCode <josh@topswagcode.com>
2024-06-20 13:52:10 +0200

    refactor program to toplevel statement again

--- domains/transfer/ClaimAutomation/Worker/Program.cs
+++ domains/transfer/ClaimAutomation/Worker/Program.cs
@@ -1,13 +1,12 @@
 using System;
-using System.Text.Json.Serialization;
 using API.ClaimAutomation.Api.Repositories;
+using ClaimAutomation.Worker;
 using ClaimAutomation.Worker.Automation;
 using ClaimAutomation.Worker.Metrics;
 using ClaimAutomation.Worker.Options;
 using DataContext;
 using EnergyOrigin.Setup;
 using EnergyOrigin.TokenValidation.Options;
-using EnergyOrigin.TokenValidation.Utilities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -19,92 +18,85 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using ProjectOriginClients;
 
-namespace ClaimAutomation.Worker
+var builder = WebApplication.CreateBuilder(args);
+
+var otlpConfiguration = builder.Configuration.GetSection(OtlpOptions.Prefix);
+var otlpOptions = otlpConfiguration.Get<OtlpOptions>()!;
+
+builder.AddSerilog();
+
+builder.Services.AddOptions<DatabaseOptions>().BindConfiguration(DatabaseOptions.Prefix).ValidateDataAnnotations()
+    .ValidateOnStart();
+builder.Services.AddOptions<ProjectOriginOptions>().BindConfiguration(ProjectOriginOptions.ProjectOrigin)
+    .ValidateDataAnnotations().ValidateOnStart();
+builder.Services.AddOptions<OtlpOptions>().BindConfiguration(OtlpOptions.Prefix).ValidateDataAnnotations()
+    .ValidateOnStart();
+builder.Services.AddOptions<TokenValidationOptions>().BindConfiguration(TokenValidationOptions.Prefix)
+    .ValidateDataAnnotations().ValidateOnStart();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    (sp, options) => options.UseNpgsql(
+        sp.GetRequiredService<IOptions<DatabaseOptions>>().Value.ToConnectionString(),
+        providerOptions => providerOptions.EnableRetryOnFailure()
+    ),
+    optionsLifetime: ServiceLifetime.Singleton);
+builder.Services.AddDbContextFactory<ApplicationDbContext>(); // TODO: Look into why we add DbContext twice.
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(sp => sp.GetRequiredService<IOptions<DatabaseOptions>>().Value.ToConnectionString());
+
+builder.Services.AddLogging();
+
+builder.Services.AddScoped<IClaimAutomationRepository, ClaimAutomationRepository>();
+builder.Services.AddScoped<IClaimService, ClaimService>();
+builder.Services.AddScoped<IShuffler, Shuffler>();
+builder.Services.AddHostedService<ClaimWorker>();
+builder.Services.AddSingleton<AutomationCache>();
+builder.Services.AddSingleton<IClaimAutomationMetrics, ClaimAutomationMetrics>();
+builder.Services.AddHttpClient<IProjectOriginWalletClient, ProjectOriginWalletClient>((sp, c) =>
 {
-    class Program
-    {
-        public static int Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    var options = sp.GetRequiredService<IOptions<ProjectOriginOptions>>().Value;
+    c.BaseAddress = new Uri(options.WalletUrl);
+});
 
-            var otlpConfiguration = builder.Configuration.GetSection(OtlpOptions.Prefix);
-            var otlpOptions = otlpConfiguration.Get<OtlpOptions>()!;
-
-            builder.AddSerilog();
-
-            builder.Services.AddOptions<DatabaseOptions>().BindConfiguration(DatabaseOptions.Prefix).ValidateDataAnnotations()
-                .ValidateOnStart();
-            builder.Services.AddOptions<ProjectOriginOptions>().BindConfiguration(ProjectOriginOptions.ProjectOrigin)
-                .ValidateDataAnnotations().ValidateOnStart();
-            builder.Services.AddOptions<OtlpOptions>().BindConfiguration(OtlpOptions.Prefix).ValidateDataAnnotations()
-                .ValidateOnStart();
-            builder.Services.AddOptions<TokenValidationOptions>().BindConfiguration(TokenValidationOptions.Prefix)
-                .ValidateDataAnnotations().ValidateOnStart();
-
-            builder.Services.AddDbContext<ApplicationDbContext>(
-                (sp, options) => options.UseNpgsql(
-                    sp.GetRequiredService<IOptions<DatabaseOptions>>().Value.ToConnectionString(),
-                    providerOptions => providerOptions.EnableRetryOnFailure()
-                ),
-                optionsLifetime: ServiceLifetime.Singleton);
-            builder.Services.AddDbContextFactory<ApplicationDbContext>(); // TODO: Look into why we add DbContext twice.
-
-            builder.Services.AddHealthChecks()
-                .AddNpgSql(sp => sp.GetRequiredService<IOptions<DatabaseOptions>>().Value.ToConnectionString());
-
-            builder.Services.AddLogging();
-
-            builder.Services.AddScoped<IClaimAutomationRepository, ClaimAutomationRepository>();
-            builder.Services.AddScoped<IClaimService, ClaimService>();
-            builder.Services.AddScoped<IShuffler, Shuffler>();
-            builder.Services.AddHostedService<ClaimWorker>();
-            builder.Services.AddSingleton<AutomationCache>();
-            builder.Services.AddSingleton<IClaimAutomationMetrics, ClaimAutomationMetrics>();
-            builder.Services.AddHttpClient<IProjectOriginWalletClient, ProjectOriginWalletClient>((sp, c) =>
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource
+        .AddService(serviceName: "ClaimAutomation.Worker"))
+    .WithMetrics(meterProviderBuilder =>
+        meterProviderBuilder
+            .AddMeter(ClaimAutomationMetrics.MetricName)
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddProcessInstrumentation()
+            .AddOtlpExporter(o => o.Endpoint = otlpOptions.ReceiverEndpoint))
+    .WithTracing(tracerProviderBuilder =>
+        tracerProviderBuilder
+            .AddGrpcClientInstrumentation(grpcOptions =>
             {
-                var options = sp.GetRequiredService<IOptions<ProjectOriginOptions>>().Value;
-                c.BaseAddress = new Uri(options.WalletUrl);
-            });
+                grpcOptions.SuppressDownstreamInstrumentation = true;
+                grpcOptions.EnrichWithHttpRequestMessage = (activity, httpRequestMessage) =>
+                    activity.SetTag("requestVersion", httpRequestMessage.Version);
+                grpcOptions.EnrichWithHttpResponseMessage = (activity, httpResponseMessage) =>
+                    activity.SetTag("responseVersion", httpResponseMessage.Version);
+            })
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation()
+            .AddNpgsql()
+            .AddOtlpExporter(o => o.Endpoint = otlpOptions.ReceiverEndpoint));
 
-            builder.Services.AddOpenTelemetry()
-                .ConfigureResource(resource => resource
-                    .AddService(serviceName: "ClaimAutomation.Worker"))
-                .WithMetrics(meterProviderBuilder =>
-                    meterProviderBuilder
-                        .AddMeter(ClaimAutomationMetrics.MetricName)
-                        .AddHttpClientInstrumentation()
-                        .AddAspNetCoreInstrumentation()
-                        .AddRuntimeInstrumentation()
-                        .AddProcessInstrumentation()
-                        .AddOtlpExporter(o => o.Endpoint = otlpOptions.ReceiverEndpoint))
-                .WithTracing(tracerProviderBuilder =>
-                    tracerProviderBuilder
-                        .AddGrpcClientInstrumentation(grpcOptions =>
-                        {
-                            grpcOptions.SuppressDownstreamInstrumentation = true;
-                            grpcOptions.EnrichWithHttpRequestMessage = (activity, httpRequestMessage) =>
-                                activity.SetTag("requestVersion", httpRequestMessage.Version);
-                            grpcOptions.EnrichWithHttpResponseMessage = (activity, httpResponseMessage) =>
-                                activity.SetTag("responseVersion", httpResponseMessage.Version);
-                        })
-                        .AddHttpClientInstrumentation()
-                        .AddAspNetCoreInstrumentation()
-                        .AddNpgsql()
-                        .AddOtlpExporter(o => o.Endpoint = otlpOptions.ReceiverEndpoint));
+var app = builder.Build();
+app.MapHealthChecks("/health");
 
-            var app = builder.Build();
-            app.MapHealthChecks("/health");
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-            app.UseHttpsRedirection();
-            app.UseAuthentication();
-            app.UseAuthorization();
+app.Run();
 
-            app.MapControllers();
-
-            app.Run();
-
-            //top level statements here
-            return 0;
-        }
+namespace ClaimAutomation
+{
+    public partial class Program
+    {
     }
 }
