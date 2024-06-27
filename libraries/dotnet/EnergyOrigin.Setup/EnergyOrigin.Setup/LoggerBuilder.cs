a78a28893ea466773bbb28c75a8bf4c41e819362
sahma19 <saiuan@msn.com>
2024-06-27 15:15:02 +0200

    Add back serilogger

---
+++ libraries/dotnet/EnergyOrigin.Setup/EnergyOrigin.Setup/LoggerBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Formatting.Json;
+
+namespace EnergyOrigin.Setup;
+
+public static class LoggerBuilder
+{
+    public static Serilog.ILogger BuildSerilogger()
+    {
+        var loggerConfiguration = new LoggerConfiguration()
+            .Filter.ByExcluding("RequestPath like '/health%'")
+            .Filter.ByExcluding("RequestPath like '/metrics%'");
+
+        loggerConfiguration = loggerConfiguration.WriteTo.Console(new JsonFormatter());
+
+        return loggerConfiguration.CreateLogger();
+    }
+}
