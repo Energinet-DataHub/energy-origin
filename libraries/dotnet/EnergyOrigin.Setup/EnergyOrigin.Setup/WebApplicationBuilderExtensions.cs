6d21b72efd97615c180c6137b3741296882cb8e1
sahma19 <saiuan@msn.com>
2024-06-27 08:57:26 +0200

    Remove redundant enrichers.span package and upgrade other serilog packages

--- libraries/dotnet/EnergyOrigin.Setup/EnergyOrigin.Setup/WebApplicationBuilderExtensions.cs
+++ libraries/dotnet/EnergyOrigin.Setup/EnergyOrigin.Setup/WebApplicationBuilderExtensions.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Enrichers.Span;
 using Serilog.Events;
 using Serilog.Formatting.Json;
 
@@ -14,8 +13,7 @@ public static class WebApplicationBuilderExtensions
     {
         LoggerConfiguration log = new LoggerConfiguration()
             .Filter.ByExcluding("RequestPath like '/health%'")
-            .Filter.ByExcluding("RequestPath like '/metrics%'")
-            .Enrich.WithSpan();
+            .Filter.ByExcluding("RequestPath like '/metrics%'");
 
         var console = builder.Environment.IsDevelopment()
             ? log.WriteTo.Console()
@@ -30,7 +28,6 @@ public static class WebApplicationBuilderExtensions
         var log = new LoggerConfiguration()
             .Filter.ByExcluding("RequestPath like '/health%'")
             .Filter.ByExcluding("RequestPath like '/metrics%'")
-            .Enrich.WithSpan()
             .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning);
 
         var console = builder.Environment.IsDevelopment()
