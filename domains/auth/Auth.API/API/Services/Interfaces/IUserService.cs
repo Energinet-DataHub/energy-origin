38b1ac7bad3d45de6563fa99e2dc32a5a91342e4
Theis Nickelsen <130448180+tnickelsen@users.noreply.github.com>
2024-07-03 13:03:40 +0200

    Uses v2 terms event (#1276)

--- domains/auth/Auth.API/API/Services/Interfaces/IUserService.cs
+++ domains/auth/Auth.API/API/Services/Interfaces/IUserService.cs
@@ -6,7 +6,7 @@ namespace API.Services.Interfaces;
 public interface IUserService
 {
     Task<User> UpsertUserAsync(User user);
-    Task UpdateTermsAccepted(User user, DecodableUserDescriptor descriptor);
+    Task UpdateTermsAccepted(User user, DecodableUserDescriptor descriptor, string traceId);
     Task<User> InsertUserAsync(User user);
     Task<User?> GetUserByIdAsync(Guid? id);
     Task RemoveUserAsync(User user);
