bf331a8ed7ae9b02d900ef40be1ce84b55bb513d
sahma19 <saiuan@msn.com>
2024-07-03 15:12:07 +0200

    Add UnitTests for AcceptTermsCommandHandler

--- domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/[Commands]/AcceptTermsCommandHandlerTests.cs
+++ domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/[Commands]/AcceptTermsCommandHandlerTests.cs
@@ -2,6 +2,7 @@ using API.Authorization._Features_;
 using API.Data;
 using API.Models;
 using API.Repository;
+using API.UnitTests.Repository;
 using API.ValueObjects;
 using FluentAssertions;
 using NSubstitute;
@@ -10,17 +11,17 @@ namespace API.UnitTests._Commands_;
 
 public class AcceptTermsCommandHandlerTests
 {
-    private readonly IOrganizationRepository _organizationRepository;
-    private readonly IUserRepository _userRepository;
-    private readonly ITermsRepository _termsRepository;
+    private readonly FakeOrganizationRepository _organizationRepository;
+    private readonly FakeUserRepository _userRepository;
+    private readonly FakeTermsRepository _termsRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly AcceptTermsCommandHandler _handler;
 
     public AcceptTermsCommandHandlerTests()
     {
-        _organizationRepository = Substitute.For<IOrganizationRepository>();
-        _userRepository = Substitute.For<IUserRepository>();
-        _termsRepository = Substitute.For<ITermsRepository>();
+        _organizationRepository = new FakeOrganizationRepository();
+        _userRepository = new FakeUserRepository();
+        _termsRepository = new FakeTermsRepository();
         _unitOfWork = Substitute.For<IUnitOfWork>();
         _handler = new AcceptTermsCommandHandler(_organizationRepository, _userRepository, _termsRepository, _unitOfWork);
     }
@@ -29,13 +30,12 @@ public class AcceptTermsCommandHandlerTests
     public async Task Handle_WhenOrganizationDoesNotExist_CreatesNewOrganization()
     {
         var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User");
-        _organizationRepository.Query().ReturnsForAnyArgs(new List<Organization>().AsQueryable());
-        _termsRepository.Query().Returns(new List<Terms> { Terms.Create("1.0") }.AsQueryable());
+        await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
 
         var result = await _handler.Handle(command, CancellationToken.None);
 
         result.Should().BeTrue();
-        await _organizationRepository.Received(1).AddAsync(Arg.Is<Organization>(o => o.Tin.Value == command.OrgCvr), Arg.Any<CancellationToken>());
+        _organizationRepository.Query().Count().Should().Be(1);
         await _unitOfWork.Received(1).CommitAsync();
     }
 
@@ -44,15 +44,14 @@ public class AcceptTermsCommandHandlerTests
     {
         var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User");
         var organization = Organization.Create(new Tin(command.OrgCvr), new OrganizationName("Test Org"));
-        _organizationRepository.Query().Returns(new List<Organization> { organization }.AsQueryable());
-        _termsRepository.Query().Returns(new List<Terms> { Terms.Create("1.0") }.AsQueryable());
+        await _organizationRepository.AddAsync(organization, CancellationToken.None);
+        await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
 
         var result = await _handler.Handle(command, CancellationToken.None);
 
         result.Should().BeTrue();
         organization.TermsAccepted.Should().BeTrue();
         organization.TermsVersion.Should().Be("1.0");
-        _organizationRepository.Received(1).Update(Arg.Is<Organization>(o => o.Tin.Value == command.OrgCvr));
         await _unitOfWork.Received(1).CommitAsync();
     }
 
@@ -60,7 +59,6 @@ public class AcceptTermsCommandHandlerTests
     public async Task Handle_WhenNoTermsExist_ThrowsInvalidOperationException()
     {
         var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User");
-        _termsRepository.Query().Returns(new List<Terms>().AsQueryable());
 
         await Assert.ThrowsAsync<InvalidOperationException>(() => _handler.Handle(command, CancellationToken.None));
         await _unitOfWork.Received(1).RollbackAsync();
@@ -71,15 +69,14 @@ public class AcceptTermsCommandHandlerTests
     {
         var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User");
         var organization = Organization.Create(new Tin(command.OrgCvr), new OrganizationName("Test Org"));
-        _organizationRepository.Query().Returns(new List<Organization> { organization }.AsQueryable());
-        _termsRepository.Query().Returns(new List<Terms> { Terms.Create("1.0") }.AsQueryable());
-        _userRepository.Query().ReturnsForAnyArgs(new List<User>().AsQueryable());
+        await _organizationRepository.AddAsync(organization, CancellationToken.None);
+        await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
 
         var result = await _handler.Handle(command, CancellationToken.None);
 
         result.Should().BeTrue();
-        await _userRepository.Received(1).AddAsync(Arg.Is<User>(u => u.IdpUserId.Value == command.UserId), Arg.Any<CancellationToken>());
-        _organizationRepository.Received(1).Update(Arg.Is<Organization>(o => o.Affiliations.Any(a => a.User.IdpUserId.Value == command.UserId)));
+        _userRepository.Query().Count().Should().Be(1);
+        organization.Affiliations.Any(a => a.User.IdpUserId.Value == command.UserId).Should().BeTrue();
         await _unitOfWork.Received(1).CommitAsync();
     }
 
@@ -89,14 +86,14 @@ public class AcceptTermsCommandHandlerTests
         var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User");
         var organization = Organization.Create(new Tin(command.OrgCvr), new OrganizationName("Test Org"));
         var user = User.Create(IdpUserId.Create(command.UserId), UserName.Create(command.UserName));
-        _organizationRepository.Query().Returns(new List<Organization> { organization }.AsQueryable());
-        _termsRepository.Query().Returns(new List<Terms> { Terms.Create("1.0") }.AsQueryable());
-        _userRepository.Query().Returns(new List<User> { user }.AsQueryable());
+        await _organizationRepository.AddAsync(organization, CancellationToken.None);
+        await _userRepository.AddAsync(user, CancellationToken.None);
+        await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
 
         var result = await _handler.Handle(command, CancellationToken.None);
 
         result.Should().BeTrue();
-        _organizationRepository.Received(1).Update(Arg.Is<Organization>(o => o.Affiliations.Any(a => a.User.IdpUserId.Value == command.UserId)));
+        organization.Affiliations.Any(a => a.User.IdpUserId.Value == command.UserId).Should().BeTrue();
         await _unitOfWork.Received(1).CommitAsync();
     }
 
@@ -104,9 +101,12 @@ public class AcceptTermsCommandHandlerTests
     public async Task Handle_WhenExceptionOccurs_RollsBackTransaction()
     {
         var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User");
-        _organizationRepository.Query().Returns(x => throw new Exception("Test exception"));
+        var mockOrganizationRepository = Substitute.For<IOrganizationRepository>();
+        mockOrganizationRepository.Query().Returns(x => throw new Exception("Test exception"));
+        var mockUnitOfWork = Substitute.For<IUnitOfWork>();
+        var handler = new AcceptTermsCommandHandler(mockOrganizationRepository, _userRepository, _termsRepository, mockUnitOfWork);
 
-        await Assert.ThrowsAsync<Exception>(() => _handler.Handle(command, CancellationToken.None));
-        await _unitOfWork.Received(1).RollbackAsync();
+        await Assert.ThrowsAsync<Exception>(() => handler.Handle(command, CancellationToken.None));
+        await mockUnitOfWork.Received(1).RollbackAsync();
     }
 }
