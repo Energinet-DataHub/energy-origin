0a30d1f2317236e51adc80cff45b0f671a9e685d
tnickelsen <TNE@energinet.dk>
2024-07-03 11:48:57 +0200

    removed from database

--- domains/measurements/Measurements.API/API/MeteringPoints/Api/ApplicationDbContext.cs
+++ domains/measurements/Measurements.API/API/MeteringPoints/Api/ApplicationDbContext.cs
@@ -12,7 +12,6 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<RelationDto> Relations { get; set; }
-    public DbSet<OrgAcceptedTermsEvent> OrgAcceptedTermsEvents { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- domains/measurements/Measurements.API/API/MeteringPoints/Api/Models/OrgAcceptedTermsEvent.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace API.MeteringPoints.Api.Models;
-public class OrgAcceptedTermsEvent
-{
-    public Guid EventId { get; set; }
-}
