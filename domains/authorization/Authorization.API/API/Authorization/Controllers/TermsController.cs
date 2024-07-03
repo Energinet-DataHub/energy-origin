ef2815d8d2edbf3a36477bca71990c0ec398c7c7
sahma19 <saiuan@msn.com>
2024-07-03 12:49:40 +0200

    add request response dto

--- domains/authorization/Authorization.API/API/Authorization/Controllers/TermsController.cs
+++ domains/authorization/Authorization.API/API/Authorization/Controllers/TermsController.cs
@@ -14,24 +14,21 @@ public class TermsController(IMediator mediator) : ControllerBase
     [HttpPost]
     [Route("api/authorization/terms/accept")]
     [Authorize(Policy = Policy.B2CPolicy)]
-    public async Task<IActionResult> AcceptTerms([FromBody] AcceptTermsRequest request)
+    public async Task<ActionResult<AcceptTermsResponseDto>> AcceptTerms([FromBody] AcceptTermsRequest request)
     {
         var command = new AcceptTermsCommand(request.OrgCvr, request.UserId, request.UserName);
         var result = await mediator.Send(command);
 
         if (result)
         {
-            return Ok(new { Message = "Terms accepted successfully." });
+            return Ok(new AcceptTermsResponseDto(true, "Terms accepted successfully."));
         }
 
-        return BadRequest(new { Message = "Failed to accept terms." });
+        return BadRequest(new AcceptTermsResponseDto(false, "Failed to accept terms."));
     }
 }
 
-public class AcceptTermsRequest
-{
-    public string OrgCvr { get; set; } = null!;
-    public Guid UserId { get; set; }
-    public string UserName { get; set; } = null!;
-}
+public record AcceptTermsRequest(string OrgCvr, Guid UserId, string UserName);
+public record AcceptTermsResponseDto(bool Status, string Message);
+
 
