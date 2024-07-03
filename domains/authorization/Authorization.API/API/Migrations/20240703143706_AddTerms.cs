ac73ab8e757a19ca037c1db3209cfdb8ee9a45d9
sahma19 <saiuan@msn.com>
2024-07-03 16:37:37 +0200

    Add migration

---
+++ domains/authorization/Authorization.API/API/Migrations/20240703143706_AddTerms.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTerms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "TermsAcceptanceDate",
+                table: "Organizations",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TermsAccepted",
+                table: "Organizations",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TermsVersion",
+                table: "Organizations",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Terms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Version = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Terms", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Terms");
+
+            migrationBuilder.DropColumn(
+                name: "TermsAcceptanceDate",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "TermsAccepted",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "TermsVersion",
+                table: "Organizations");
+        }
+    }
+}
