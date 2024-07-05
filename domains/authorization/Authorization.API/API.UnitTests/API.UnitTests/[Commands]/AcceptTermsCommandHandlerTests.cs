08a505b66fd45dd0bc7c9cadd47a4dbb4c545d39
sahma19 <saiuan@msn.com>
2024-07-05 10:27:23 +0200

    Refactor command, and unittests

--- domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/[Commands]/AcceptTermsCommandHandlerTests.cs
+++ domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/[Commands]/AcceptTermsCommandHandlerTests.cs
@@ -12,7 +12,6 @@ namespace API.UnitTests._Commands_;
 public class AcceptTermsCommandHandlerTests
 {
     private readonly FakeOrganizationRepository _organizationRepository;
-    private readonly FakeUserRepository _userRepository;
     private readonly FakeTermsRepository _termsRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly AcceptTermsCommandHandler _handler;
@@ -20,16 +19,15 @@ public class AcceptTermsCommandHandlerTests
     public AcceptTermsCommandHandlerTests()
     {
         _organizationRepository = new FakeOrganizationRepository();
-        _userRepository = new FakeUserRepository();
         _termsRepository = new FakeTermsRepository();
         _unitOfWork = Substitute.For<IUnitOfWork>();
-        _handler = new AcceptTermsCommandHandler(_organizationRepository, _userRepository, _termsRepository, _unitOfWork);
+        _handler = new AcceptTermsCommandHandler(_organizationRepository, _termsRepository, _unitOfWork);
     }
 
     [Fact]
     public async Task Handle_WhenOrganizationDoesNotExist_CreatesNewOrganization()
     {
-        var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User", "Test Org");
+        var command = new AcceptTermsCommand("12345678", "Test Org");
         await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
 
         var result = await _handler.Handle(command, CancellationToken.None);
@@ -42,7 +40,7 @@ public class AcceptTermsCommandHandlerTests
     [Fact]
     public async Task Handle_WhenOrganizationExistsButTermsNotAccepted_UpdatesTerms()
     {
-        var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User", "Test Org");
+        var command = new AcceptTermsCommand("12345678", "Test Org");
         var organization = Organization.Create(new Tin(command.OrgCvr), new OrganizationName("Test Org"));
         await _organizationRepository.AddAsync(organization, CancellationToken.None);
         await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
@@ -58,51 +56,20 @@ public class AcceptTermsCommandHandlerTests
     [Fact]
     public async Task Handle_WhenNoTermsExist_ThrowsInvalidOperationException()
     {
-        var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User", "Test Org");
+        var command = new AcceptTermsCommand("12345678", "Test Org");
 
         await Assert.ThrowsAsync<InvalidOperationException>(() => _handler.Handle(command, CancellationToken.None));
         await _unitOfWork.Received(1).RollbackAsync();
     }
 
     [Fact]
-    public async Task Handle_WhenUserDoesNotExist_CreatesNewUserAndAffiliation()
-    {
-        var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User", "Test Org");
-        var organization = Organization.Create(new Tin(command.OrgCvr), new OrganizationName("Test Org"));
-        await _organizationRepository.AddAsync(organization, CancellationToken.None);
-        await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
-
-        var result = await _handler.Handle(command, CancellationToken.None);
-
-        result.Should().BeTrue();
-        _userRepository.Query().Count().Should().Be(1);
-        organization.Affiliations.Any(a => a.User.IdpUserId.Value == command.UserId).Should().BeTrue();
-        await _unitOfWork.Received(1).CommitAsync();
-    }
-
-    [Fact]
-    public async Task Handle_WhenUserExists_CreatesNewAffiliation()
-    {
-        var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User", "Test Org");
-        var organization = Organization.Create(new Tin(command.OrgCvr), new OrganizationName("Test Org"));
-        await _organizationRepository.AddAsync(organization, CancellationToken.None);
-        await _termsRepository.AddAsync(Terms.Create("1.0"), CancellationToken.None);
-
-        var result = await _handler.Handle(command, CancellationToken.None);
-
-        result.Should().BeTrue();
-        organization.Affiliations.Any(a => a.User.IdpUserId.Value == command.UserId).Should().BeTrue();
-        await _unitOfWork.Received(1).CommitAsync();
-    }
-
-    [Fact]
     public async Task Handle_WhenExceptionOccurs_RollsBackTransaction()
     {
-        var command = new AcceptTermsCommand("12345678", Guid.NewGuid(), "Test User", "Test Org");
+        var command = new AcceptTermsCommand("12345678", "Test Org");
         var mockOrganizationRepository = Substitute.For<IOrganizationRepository>();
-        mockOrganizationRepository.Query().Returns(x => throw new Exception("Test exception"));
+        mockOrganizationRepository.Query().Returns(_ => throw new Exception("Test exception"));
         var mockUnitOfWork = Substitute.For<IUnitOfWork>();
-        var handler = new AcceptTermsCommandHandler(mockOrganizationRepository, _userRepository, _termsRepository, mockUnitOfWork);
+        var handler = new AcceptTermsCommandHandler(mockOrganizationRepository, _termsRepository, mockUnitOfWork);
 
         await Assert.ThrowsAsync<Exception>(() => handler.Handle(command, CancellationToken.None));
         await mockUnitOfWork.Received(1).RollbackAsync();
--- domains/authorization/Authorization.API/API/Authorization/[Features]/AcceptTermsCommand.cs
+++ domains/authorization/Authorization.API/API/Authorization/[Features]/AcceptTermsCommand.cs
@@ -1,21 +1,19 @@
-using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using API.Data;
 using API.Models;
 using API.Repository;
 using API.ValueObjects;
-using API.Data;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Authorization._Features_;
 
-public record AcceptTermsCommand(string OrgCvr, Guid UserId, string UserName, string OrgName ) : IRequest<bool>;
+public record AcceptTermsCommand(string OrgCvr, string OrgName ) : IRequest<bool>;
 
 public class AcceptTermsCommandHandler(
     IOrganizationRepository organizationRepository,
-    IUserRepository userRepository,
     ITermsRepository termsRepository,
     IUnitOfWork unitOfWork)
     : IRequestHandler<AcceptTermsCommand, bool>
@@ -26,14 +24,13 @@ public class AcceptTermsCommandHandler(
 
         try
         {
-            var orgTin = Tin.Create(request.OrgCvr);
+            var usersOrganizationsCvr = Tin.Create(request.OrgCvr);
             var organization = await organizationRepository.Query()
-                .Include(o => o.Affiliations)
-                .FirstOrDefaultAsync(o => o.Tin == orgTin, cancellationToken);
+                .FirstOrDefaultAsync(o => o.Tin == usersOrganizationsCvr, cancellationToken);
 
             if (organization == null)
             {
-                organization = Organization.Create(orgTin, OrganizationName.Create(request.OrgCvr));
+                organization = Organization.Create(usersOrganizationsCvr, OrganizationName.Create(request.OrgName));
                 await organizationRepository.AddAsync(organization, cancellationToken);
             }
 
@@ -43,28 +40,12 @@ public class AcceptTermsCommandHandler(
 
             if (latestTerms == null)
             {
-                throw new InvalidOperationException("No terms found in the system");
+                return false;
             }
 
             if (!organization.TermsAccepted || organization.TermsVersion != latestTerms.Version)
             {
                 organization.AcceptTerms(latestTerms);
-                organizationRepository.Update(organization);
-            }
-
-            var idpUserId = IdpUserId.Create(request.UserId);
-            var user = await userRepository.Query()
-                .Where(u => u.IdpUserId == idpUserId)
-                .FirstOrDefaultAsync(cancellationToken);
-
-            if (user == null)
-            {
-                user = User.Create(idpUserId, UserName.Create(request.UserName));
-                await userRepository.AddAsync(user, cancellationToken);
-
-                var affiliation = Affiliation.Create(user, organization);
-                organization.Affiliations.Add(affiliation);
-                organizationRepository.Update(organization);
             }
 
             await unitOfWork.CommitAsync();
