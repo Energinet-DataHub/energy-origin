fc1b93273d0b10f1735047b33cf9a6aa2a80be65
sahma19 <saiuan@msn.com>
2024-07-05 19:52:18 +0200

    remove using rabbitmq.client

--- domains/authorization/Authorization.API/API/Program.cs
+++ domains/authorization/Authorization.API/API/Program.cs
@@ -14,7 +14,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
