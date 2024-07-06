252f5f40bce39d8aa95fb0b0a110177c7fe8569d
sahma19 <saiuan@msn.com>
2024-07-06 22:00:09 +0200

    AddTermsMigrationTests.cs

---
+++ domains/authorization/Authorization.API/API.IntegrationTests/API.IntegrationTests/Migrations/AddTermsMigrationTests.cs
@@ -0,0 +1,58 @@
+﻿using API.IntegrationTests.Setup;
+using API.Models;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
+
+namespace API.IntegrationTests.Migrations;
+
+[Collection(IntegrationTestCollection.CollectionName)]
+public class AddTermsMigrationTests
+{
+    private readonly DbContextOptions<ApplicationDbContext> options;
+
+    public AddTermsMigrationTests(IntegrationTestFixture integrationTestFixture)
+    {
+        var newDatabaseInfo = integrationTestFixture.PostgresContainer.CreateNewDatabase().Result;
+        options = new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql(newDatabaseInfo.ConnectionString).Options;
+    }
+
+    [Fact]
+    public async Task AddTerms_Migration_SetsTermsAcceptedToFalseForExistingOrganizations()
+    {
+        await using var dbContext = new ApplicationDbContext(options);
+        var migrator = dbContext.GetService<IMigrator>();
+
+        await migrator.MigrateAsync("20240620115450_AddOrganizationTinUniqueIndex");
+
+        await InsertOldOrganization(dbContext, Guid.NewGuid(), "12345678", "Test Org");
+
+        var applyMigration = () => migrator.MigrateAsync("20240703143706_AddTerms");
+        await applyMigration.Should().NotThrowAsync();
+
+        var organizations = await dbContext.Organizations.ToListAsync();
+        organizations.Should().NotBeEmpty();
+        organizations.Should().AllSatisfy(org =>
+        {
+            org.TermsAccepted.Should().BeFalse();
+            org.TermsVersion.Should().BeNull();
+            org.TermsAcceptanceDate.Should().BeNull();
+        });
+    }
+
+    private static async Task InsertOldOrganization(ApplicationDbContext dbContext, Guid id, string tin, string name)
+    {
+        var organizationTable = dbContext.Model.FindEntityType(typeof(Organization))!.GetTableName();
+
+        var query = $@"
+            INSERT INTO ""{organizationTable}"" (""Id"", ""Tin"", ""Name"")
+            VALUES (@Id, @Tin, @Name)";
+
+        await dbContext.Database.ExecuteSqlRawAsync(query,
+            new NpgsqlParameter("Id", id),
+            new NpgsqlParameter("Tin", tin),
+            new NpgsqlParameter("Name", name));
+    }
+}
