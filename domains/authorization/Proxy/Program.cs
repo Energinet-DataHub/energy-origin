b2a7f829e7d0109510f3ed9a70e1767930463532
cfk <cfk@energinet.dk>
2024-07-31 10:55:00 +0200

    fix

--- domains/authorization/Proxy/Program.cs
+++ domains/authorization/Proxy/Program.cs
@@ -18,10 +18,10 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+        // Add services to the container.
 
         builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
 
         builder.Services.AddSwagger("Wallet API");
