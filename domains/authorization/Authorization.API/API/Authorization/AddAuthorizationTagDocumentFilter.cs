660f117624841097b9a3aff986cc2b0636d5a475
TopSwagCode <josh@topswagcode.com>
2024-09-11 17:29:55 +0200

    add api filters for documentation

---
+++ domains/authorization/Authorization.API/API/Authorization/AddAuthorizationTagDocumentFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace API.Authorization;
+
+public class AddAuthorizationTagDocumentFilter : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        swaggerDoc.Tags.Add(new OpenApiTag
+        {
+            Name = "Authorization",
+            Description = """
+                          Authorization in Energy Track & Trace provides endpoints for getting organization permissions for 3rd party clients and logged in users.
+                          Its also the service that is used a user can grant consent to a 3rd party client to access their data.
+                          Finally it also internal API endpoints for returning access rights that populates JWT Tokens during login.
+                          """
+        });
+    }
+}
