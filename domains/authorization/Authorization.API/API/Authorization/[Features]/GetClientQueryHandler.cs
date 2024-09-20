86cbfd4a658b01b9363fe9f6bdfa3388bfdc80f0
TopSwagCode <josh@topswagcode.com>
2024-09-20 12:20:49 +0200

    come back

---
+++ domains/authorization/Authorization.API/API/Authorization/[Features]/GetClientQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using API.Authorization.Exceptions;
+using API.Models;
+using API.Repository;
+using API.ValueObjects;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+namespace API.Authorization._Features_;
+public class GetClientQueryHandler(IClientRepository clientRepository)
+    : IRequestHandler<GetClientQuery, GetClientQueryResult>
+{
+    public async Task<GetClientQueryResult> Handle(GetClientQuery request, CancellationToken cancellationToken)
+    {
+        var client = await clientRepository
+                         .Query()
+                         .FirstOrDefaultAsync(client => client.IdpClientId == request.IdpClientId,
+                             cancellationToken: cancellationToken) ??
+                     throw new EntityNotFoundException(request.IdpClientId.Value.ToString(), nameof(Client));
+        return new GetClientQueryResult(client.IdpClientId, client.Name, client.RedirectUrl);
+    }
+}
+public record GetClientQuery(IdpClientId IdpClientId) : IRequest<GetClientQueryResult>;
+public record GetClientQueryResult(IdpClientId IdpClientId, ClientName Name, string RedirectUrl);
