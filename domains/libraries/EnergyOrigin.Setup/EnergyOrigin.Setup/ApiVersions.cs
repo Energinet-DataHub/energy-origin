8e08a02a1fc93061738e87566fc1ddd93e1ba16e
sahma19 <saiuan@msn.com>
2024-09-23 12:17:42 +0200

    remove redundant versions

--- domains/libraries/EnergyOrigin.Setup/EnergyOrigin.Setup/ApiVersions.cs
+++ domains/libraries/EnergyOrigin.Setup/EnergyOrigin.Setup/ApiVersions.cs
@@ -5,13 +5,6 @@ public static class ApiVersions
     public const string Version1 = "1"; // Used by frontend
     public const string Version20240515 = "20240515"; // Used by frontend
     public const string Version20230101 = "20230101"; // Used by frontend
-    public const string Version20240423 = "20240423";
-    public const string Version20240110 = "20240110";
-    public const string Version20240103 = "20240103";
 
     public const int Version1AsInt = 1;
-    public const int Version20230101AsInt = 20230101;
-    public const int Version20240423AsInt = 20240423;
-    public const int Version20240515AsInt = 20240515;
-    public const int Version20240103AsInt = 20240103;
 }
