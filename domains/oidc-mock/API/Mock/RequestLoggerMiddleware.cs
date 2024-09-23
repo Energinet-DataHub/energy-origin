2501c5eb245ed891956da74a8a0d3f09c6434bbd
sahma19 <saiuan@msn.com>
2024-09-23 18:26:05 +0200

    no more codeql errors

--- domains/oidc-mock/API/Mock/RequestLoggerMiddleware.cs
+++ domains/oidc-mock/API/Mock/RequestLoggerMiddleware.cs
@@ -1,22 +1,42 @@
-namespace Oidc.Mock;
+using System.Web;
 
-public class RequestLoggerMiddleware
+namespace Oidc.Mock
 {
-    private readonly RequestDelegate next;
-
-    public RequestLoggerMiddleware(RequestDelegate next) => this.next = next;
-
-    public async Task InvokeAsync(HttpContext httpContext, ILogger<RequestLoggerMiddleware> logger)
+    public class RequestLoggerMiddleware
     {
-        var req = httpContext.Request;
-        if (string.Equals(req.Path.Value, "/health", StringComparison.InvariantCultureIgnoreCase))
-        {
-            await next(httpContext);
-            return;
-        }
+        private readonly RequestDelegate next;
 
-        logger.LogDebug("Request - (Scheme: {Scheme} Host: {Host}) {Method} (PathBase: {PathBase}) {Location}", req.Scheme, req.Host, req.Method, req.PathBase, $"{req.Path}{req.QueryString}");
-        await next(httpContext);
-        logger.LogDebug("Response - {StatusCode}", httpContext.Response.StatusCode);
+        public RequestLoggerMiddleware(RequestDelegate next) => this.next = next;
+
+        public async Task InvokeAsync(HttpContext httpContext, ILogger<RequestLoggerMiddleware> logger)
+        {
+            var req = httpContext.Request;
+            if (string.Equals(req.Path.Value, "/health", StringComparison.InvariantCultureIgnoreCase))
+            {
+                await next(httpContext);
+                return;
+            }
+
+            // Sanitize all necessary parts of the request
+            var sanitizedScheme = HttpUtility.UrlEncode(req.Scheme);
+            var sanitizedHost = HttpUtility.UrlEncode(req.Host.Value);
+            var sanitizedMethod = HttpUtility.UrlEncode(req.Method);
+            var sanitizedPathBase = HttpUtility.UrlEncode(req.PathBase.Value);
+            var sanitizedPath = HttpUtility.UrlEncode(req.Path.Value);
+            var sanitizedQueryString = HttpUtility.UrlEncode(req.QueryString.Value);
+
+            // Log the sanitized request data
+            logger.LogDebug("Request - (Scheme: {Scheme} Host: {Host}) {Method} (PathBase: {PathBase}) {Location}",
+                sanitizedScheme,
+                sanitizedHost,
+                sanitizedMethod,
+                sanitizedPathBase,
+                $"{sanitizedPath}{sanitizedQueryString}");
+
+            await next(httpContext);
+
+            // Log the response status code
+            logger.LogDebug("Response - {StatusCode}", httpContext.Response.StatusCode);
+        }
     }
 }
