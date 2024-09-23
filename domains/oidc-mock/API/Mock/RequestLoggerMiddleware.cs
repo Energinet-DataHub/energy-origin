fd8ee57db44e732c2430584ed355c7b6729485fc
sahma19 <saiuan@msn.com>
2024-09-23 18:26:29 +0200

    namespace fix

--- domains/oidc-mock/API/Mock/RequestLoggerMiddleware.cs
+++ domains/oidc-mock/API/Mock/RequestLoggerMiddleware.cs
@@ -1,42 +1,40 @@
 using System.Web;
 
-namespace Oidc.Mock
+namespace Oidc.Mock;
+
+public class RequestLoggerMiddleware
 {
-    public class RequestLoggerMiddleware
+    private readonly RequestDelegate next;
+
+    public RequestLoggerMiddleware(RequestDelegate next) => this.next = next;
+
+    public async Task InvokeAsync(HttpContext httpContext, ILogger<RequestLoggerMiddleware> logger)
     {
-        private readonly RequestDelegate next;
-
-        public RequestLoggerMiddleware(RequestDelegate next) => this.next = next;
-
-        public async Task InvokeAsync(HttpContext httpContext, ILogger<RequestLoggerMiddleware> logger)
+        var req = httpContext.Request;
+        if (string.Equals(req.Path.Value, "/health", StringComparison.InvariantCultureIgnoreCase))
         {
-            var req = httpContext.Request;
-            if (string.Equals(req.Path.Value, "/health", StringComparison.InvariantCultureIgnoreCase))
-            {
-                await next(httpContext);
-                return;
-            }
-
-            // Sanitize all necessary parts of the request
-            var sanitizedScheme = HttpUtility.UrlEncode(req.Scheme);
-            var sanitizedHost = HttpUtility.UrlEncode(req.Host.Value);
-            var sanitizedMethod = HttpUtility.UrlEncode(req.Method);
-            var sanitizedPathBase = HttpUtility.UrlEncode(req.PathBase.Value);
-            var sanitizedPath = HttpUtility.UrlEncode(req.Path.Value);
-            var sanitizedQueryString = HttpUtility.UrlEncode(req.QueryString.Value);
-
-            // Log the sanitized request data
-            logger.LogDebug("Request - (Scheme: {Scheme} Host: {Host}) {Method} (PathBase: {PathBase}) {Location}",
-                sanitizedScheme,
-                sanitizedHost,
-                sanitizedMethod,
-                sanitizedPathBase,
-                $"{sanitizedPath}{sanitizedQueryString}");
-
             await next(httpContext);
-
-            // Log the response status code
-            logger.LogDebug("Response - {StatusCode}", httpContext.Response.StatusCode);
+            return;
         }
+
+        // Sanitize all necessary parts of the request
+        var sanitizedScheme = HttpUtility.UrlEncode(req.Scheme);
+        var sanitizedHost = HttpUtility.UrlEncode(req.Host.Value);
+        var sanitizedMethod = HttpUtility.UrlEncode(req.Method);
+        var sanitizedPathBase = HttpUtility.UrlEncode(req.PathBase.Value);
+        var sanitizedPath = HttpUtility.UrlEncode(req.Path.Value);
+        var sanitizedQueryString = HttpUtility.UrlEncode(req.QueryString.Value);
+
+        // Log the sanitized request data
+        logger.LogDebug("Request - (Scheme: {Scheme} Host: {Host}) {Method} (PathBase: {PathBase}) {Location}",
+            sanitizedScheme,
+            sanitizedHost,
+            sanitizedMethod,
+            sanitizedPathBase,
+            $"{sanitizedPath}{sanitizedQueryString}");
+
+        await next(httpContext);
+
+        logger.LogDebug("Response - {StatusCode}", httpContext.Response.StatusCode);
     }
 }
