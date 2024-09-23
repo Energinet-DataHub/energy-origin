62c38741b4ecdce26230530ec7c4fd4145ce6d57
sahma19 <saiuan@msn.com>
2024-09-23 14:10:09 +0200

    try now?

--- domains/libraries/EnergyOrigin.Setup/EnergyOrigin.Setup/Swagger/ConfigureSwaggerOptions.cs
+++ domains/libraries/EnergyOrigin.Setup/EnergyOrigin.Setup/Swagger/ConfigureSwaggerOptions.cs
@@ -1,6 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using Asp.Versioning;
 using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -48,11 +49,45 @@ public class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider, st
 
         foreach (var description in provider.ApiVersionDescriptions)
         {
+            var descriptionText = new StringBuilder();
+
+            descriptionText.Append(description.IsDeprecated
+                ? "This API version has been deprecated."
+                : "An example API.");
+
+            if (description.SunsetPolicy is { } policy)
+            {
+                if (policy.Date is { } when)
+                {
+                    descriptionText.Append(" The API will be sunset on ")
+                        .Append(when.Date.ToShortDateString())
+                        .Append('.');
+                }
+
+                if (policy.HasLinks)
+                {
+                    descriptionText.AppendLine();
+
+                    foreach (var link in policy.Links)
+                    {
+                        if (link.Type != "text/html") continue;
+                        descriptionText.AppendLine();
+
+                        if (link.Title.HasValue)
+                        {
+                            descriptionText.Append(link.Title.Value).Append(": ");
+                        }
+
+                        descriptionText.Append(link.LinkTarget.OriginalString);
+                    }
+                }
+            }
+
             options.SwaggerDoc(description.GroupName, new OpenApiInfo
             {
                 Title = $"{subsystemName.FirstCharToUpper()} API {description.ApiVersion}",
                 Version = description.ApiVersion.ToString(),
-                Description = description.IsDeprecated ? "This API version has been deprecated." : ""
+                Description = descriptionText.ToString()
             });
         }
     }
