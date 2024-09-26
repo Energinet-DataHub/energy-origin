02132b31bf787dc727870491db49e3648aeaed14
sahma19 <shd@energinet.dk>
2024-09-26 09:58:54 +0200

    Activity log and aggregate certificates backwards compatible (#1516)

--- domains/transfer/Transfer.API/API/Program.cs
+++ domains/transfer/Transfer.API/API/Program.cs
@@ -79,7 +79,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 var activityLogApiVersionSet = app.NewApiVersionSet("activitylog").Build();
-app.UseActivityLogWithB2CSupport().WithApiVersionSet(activityLogApiVersionSet).HasApiVersion(ApiVersions.Version1AsInt);
+app.UseActivityLogWithB2CSupport().WithApiVersionSet(activityLogApiVersionSet)
+    .HasApiVersion(ApiVersions.Version1AsInt)
+    .HasApiVersion(ApiVersions.Version20240515AsInt);
 
 app.Run();
 
