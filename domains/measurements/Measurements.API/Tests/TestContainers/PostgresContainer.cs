2971317430f2170f35ff75524617ac21bec03714
Martin <156190191+martinhenningjensen@users.noreply.github.com>
2024-10-10 12:29:50 +0200

    add claim automation integration test (#1555)

--- domains/measurements/Measurements.API/Tests/TestContainers/PostgresContainer.cs
+++ domains/measurements/Measurements.API/Tests/TestContainers/PostgresContainer.cs
@@ -1,7 +1,4 @@
 using System.Threading.Tasks;
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Configurations;
-using DotNet.Testcontainers.Containers;
 using Testcontainers.PostgreSql;
 using Xunit;
 
