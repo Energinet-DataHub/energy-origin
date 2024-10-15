d9f0ba4f26356e9f5cffcab58d7ce255d21ca69b
TopSwagCode <josh@topswagcode.com>
2024-10-15 19:46:40 +0200

    remove some client tests, since they are now more valid on organization

--- domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/Models/ClientTests.cs
+++ domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/Models/ClientTests.cs
@@ -1,7 +1,7 @@
 using API.Models;
 using API.ValueObjects;
 using FluentAssertions;
-/*
+
 namespace API.UnitTests.Models;
 
 public class ClientTests
@@ -19,34 +19,4 @@ public class ClientTests
         client.IdpClientId.Should().Be(idpClientId);
         client.ClientType.Should().Be(clientType);
     }
-
-    [Fact]
-    public void Client_CanExist_WithoutConsents()
-    {
-        var idpClientId = new IdpClientId(Guid.NewGuid());
-        var clientType = ClientType.External;
-
-        var client = Client.Create(idpClientId, new ClientName("Client"), clientType, "https://redirect.url");
-
-        client.Consents.Should().NotBeNull().And.BeEmpty();
-    }
-
-    [Fact]
-    public void Client_CanHave_Consents()
-    {
-        var idpClientId = new IdpClientId(Guid.NewGuid());
-        var clientType = ClientType.External;
-
-        var organizationTin = new Tin("12345678");
-        var organizationName = new OrganizationName("Test Organization");
-
-        var organization = Organization.Create(organizationTin, organizationName);
-
-        var client = Client.Create(idpClientId, new ClientName("Client"), clientType, "https://redirect.url");
-        var consentDate = DateTimeOffset.UtcNow;
-        var consent = Consent.Create(organization, client, consentDate);
-
-        client.Consents.Should().Contain(consent);
-    }
 }
-*/
