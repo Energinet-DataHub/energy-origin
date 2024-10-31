0b2b0684a3473877ee54ae95ece1879fada158b3
sahma19 <saiuan@msn.com>
2024-10-31 12:10:35 +0100

    no more side effects from changing options value

--- domains/certificates/Query.API/API.UnitTests/MeasurementsSyncer/MeasurementsSyncServiceTest.cs
+++ domains/certificates/Query.API/API.UnitTests/MeasurementsSyncer/MeasurementsSyncServiceTest.cs
@@ -42,6 +42,7 @@ public class MeasurementsSyncServiceTest
 
     public MeasurementsSyncServiceTest()
     {
+        _options.MinimumAgeThresholdHours = 0;
         var measurementSyncMetrics = Substitute.For<MeasurementSyncMetrics>();
         _service = new MeasurementsSyncService(_fakeLogger, _fakeSlidingWindowState, _fakeClient, new SlidingWindowService(measurementSyncMetrics, Options.Create(_options)),
             new MeasurementSyncMetrics(), _fakeMeasurementPublisher, _fakeMeteringPointsClient, Options.Create(_options));
--- domains/certificates/Query.API/API.UnitTests/MeasurementsSyncer/SlidingWindowServiceTest.cs
+++ domains/certificates/Query.API/API.UnitTests/MeasurementsSyncer/SlidingWindowServiceTest.cs
@@ -26,6 +26,7 @@ public class SlidingWindowServiceTest
 
     public SlidingWindowServiceTest()
     {
+        _options.MinimumAgeThresholdHours = 0;
         _sut = new SlidingWindowService(_measurementSyncMetrics, Options.Create(_options));
     }
 
