b32a477c4ace53f7f5bae6b3983378a14b9a48ea
sahma19 <saiuan@msn.com>
2024-11-15 20:10:46 +0100

    fix second migration test

--- domains/authorization/Authorization.API/API.IntegrationTests/API.IntegrationTests/Migrations/AddOrganizationConsentTests.cs
+++ domains/authorization/Authorization.API/API.IntegrationTests/API.IntegrationTests/Migrations/AddOrganizationConsentTests.cs
@@ -16,7 +16,9 @@ public class AddOrganizationConsentTests
     public AddOrganizationConsentTests(IntegrationTestFixture integrationTestFixture)
     {
         var newDatabaseInfo = integrationTestFixture.PostgresContainer.CreateNewDatabase().Result;
-        options = new DbContextOptionsBuilder<ApplicationDbContext>().UseNpgsql(newDatabaseInfo.ConnectionString).Options;
+        options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql(newDatabaseInfo.ConnectionString)
+            .Options;
     }
 
     [Fact]
@@ -26,6 +28,7 @@ public class AddOrganizationConsentTests
         await using var dbContext = new ApplicationDbContext(options);
         var migrator = dbContext.GetService<IMigrator>();
 
+        // Migrate to the state before the migration under test
         await migrator.MigrateAsync("20240730115826_AddDefaultTermsWithUniqueConstraint");
 
         var orgId = Guid.NewGuid();
@@ -39,18 +42,85 @@ public class AddOrganizationConsentTests
         var applyMigration = () => migrator.MigrateAsync("20241021074018_RemoveConsentTable");
         await applyMigration.Should().NotThrowAsync();
 
-        var organizations = await dbContext.Organizations.ToListAsync();
-        var organizationConsents = await dbContext.OrganizationConsents.ToListAsync();
-        var clients = await dbContext.Clients.ToListAsync();
+        // Use Npgsql to query the database directly
+        var connectionString = dbContext.Database.GetConnectionString();
+        await using var connection = new NpgsqlConnection(connectionString);
+        await connection.OpenAsync();
+
+        // Query Organizations
+        var organizations = new List<(Guid Id, string Name, string? Tin)>();
+        var orgCommandText = @"
+            SELECT ""Id"", ""Name"", ""Tin""
+            FROM ""Organizations""";
+
+        await using (var orgCommand = new NpgsqlCommand(orgCommandText, connection))
+        {
+            await using var orgReader = await orgCommand.ExecuteReaderAsync();
+
+            while (await orgReader.ReadAsync())
+            {
+                var id = orgReader.GetGuid(orgReader.GetOrdinal("Id"));
+                var name = orgReader.GetString(orgReader.GetOrdinal("Name"));
+                var tinOrdinal = orgReader.GetOrdinal("Tin");
+
+                string? tin = null;
+                if (!orgReader.IsDBNull(tinOrdinal))
+                {
+                    tin = orgReader.GetString(tinOrdinal);
+                }
+
+                organizations.Add((id, name, tin));
+            }
+        }
+
+        // Query Clients
+        var clients = new List<(Guid Id, string Name, Guid IdpClientId, Guid OrganizationId)>();
+        var clientCommandText = @"
+            SELECT ""Id"", ""Name"", ""IdpClientId"", ""OrganizationId""
+            FROM ""Clients""";
+
+        await using (var clientCommand = new NpgsqlCommand(clientCommandText, connection))
+        {
+            await using var clientReader = await clientCommand.ExecuteReaderAsync();
+
+            while (await clientReader.ReadAsync())
+            {
+                var id = clientReader.GetGuid(clientReader.GetOrdinal("Id"));
+                var name = clientReader.GetString(clientReader.GetOrdinal("Name"));
+                var idpClientId = clientReader.GetGuid(clientReader.GetOrdinal("IdpClientId"));
+                var organizationId = clientReader.GetGuid(clientReader.GetOrdinal("OrganizationId"));
+
+                clients.Add((id, name, idpClientId, organizationId));
+            }
+        }
+
+        // Query OrganizationConsents
+        var organizationConsents = new List<(Guid ConsentGiverOrganizationId, Guid ConsentReceiverOrganizationId)>();
+        var consentCommandText = @"
+            SELECT ""ConsentGiverOrganizationId"", ""ConsentReceiverOrganizationId""
+            FROM ""OrganizationConsents""";
+
+        await using (var consentCommand = new NpgsqlCommand(consentCommandText, connection))
+        {
+            await using var consentReader = await consentCommand.ExecuteReaderAsync();
+
+            while (await consentReader.ReadAsync())
+            {
+                var consentGiverOrganizationId = consentReader.GetGuid(consentReader.GetOrdinal("ConsentGiverOrganizationId"));
+                var consentReceiverOrganizationId = consentReader.GetGuid(consentReader.GetOrdinal("ConsentReceiverOrganizationId"));
+
+                organizationConsents.Add((consentGiverOrganizationId, consentReceiverOrganizationId));
+            }
+        }
 
         // Then organization, client and consent should be migrated
-        clients.Should().ContainSingle(c => c.Name.Value == "Test Client" && c.IdpClientId.Value == clientId);
-        var client = clients.First();
+        clients.Should().ContainSingle(c => c.Name == "Test Client" && c.IdpClientId == clientId);
+        var clientData = clients.First();
         organizations.Should().HaveCount(2);
-        organizations.Should().ContainSingle(o => o.Name.Value == "Test Client" && o.Id == client.OrganizationId);
-        organizations.Should().ContainSingle(o => o.Name.Value == "Test Org" && o.Id == orgId);
+        organizations.Should().ContainSingle(o => o.Name == "Test Client" && o.Id == clientData.OrganizationId);
+        organizations.Should().ContainSingle(o => o.Name == "Test Org" && o.Id == orgId && o.Tin == "12345678");
         organizationConsents.Should().ContainSingle(orgConsent =>
-            orgConsent.ConsentGiverOrganizationId == orgId && orgConsent.ConsentReceiverOrganizationId == client.OrganizationId);
+            orgConsent.ConsentGiverOrganizationId == orgId && orgConsent.ConsentReceiverOrganizationId == clientData.OrganizationId);
     }
 
     private static async Task InsertOldOrganization(ApplicationDbContext dbContext, Guid id, string tin, string name)
@@ -94,6 +164,6 @@ public class AddOrganizationConsentTests
         await dbContext.Database.ExecuteSqlRawAsync(query,
             new NpgsqlParameter("ClientId", clientId),
             new NpgsqlParameter("OrganizationId", organizationId),
-            new NpgsqlParameter("ConsentDate", DateTime.Now));
+            new NpgsqlParameter("ConsentDate", DateTime.UtcNow));
     }
 }
