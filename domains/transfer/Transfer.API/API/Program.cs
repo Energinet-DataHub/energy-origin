4c778e34eb650e8d2552799ca9768c0d5c916e65
TopSwagCode <josh@topswagcode.com>
2024-12-11 20:58:56 +0100

    bring back circuit breaker

--- domains/transfer/Transfer.API/API/Program.cs
+++ domains/transfer/Transfer.API/API/Program.cs
@@ -35,23 +35,23 @@ builder.Services.AddHttpClient<IAuthorizationClient, AuthorizationClient>(client
 {
     client.BaseAddress =
         new Uri(builder.Configuration["Authorization:BaseUrl"]!); // Do we want to fail in other way than nullpointer?
-});
-// .AddPolicyHandler(RetryPolicy())
-// .AddPolicyHandler(GetCircuitBreakerPolicy());
-//
-// AsyncRetryPolicy<HttpResponseMessage> RetryPolicy()
-// {
-//     return HttpPolicyExtensions
-//         .HandleTransientHttpError()
-//         .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
-// }
-//
-// IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
-// {
-//     return HttpPolicyExtensions
-//         .HandleTransientHttpError()
-//         .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
-// }
+})
+.AddPolicyHandler(RetryPolicy())
+.AddPolicyHandler(GetCircuitBreakerPolicy());
+
+AsyncRetryPolicy<HttpResponseMessage> RetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+}
+
+IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+}
 
 builder.Services.AddOpenTelemetryMetricsAndTracing("Transfer.API", otlpOptions.ReceiverEndpoint);
 
