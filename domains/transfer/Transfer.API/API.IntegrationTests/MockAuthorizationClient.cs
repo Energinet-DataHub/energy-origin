a79cbda023ce16421a11184ce86fb0980e18a152
sahma19 <saiuan@msn.com>
2025-01-20 21:46:34 +0100

    introduce OptionsMonitor

--- domains/transfer/Transfer.API/API.IntegrationTests/MockAuthorizationClient.cs
+++ domains/transfer/Transfer.API/API.IntegrationTests/MockAuthorizationClient.cs
@@ -2,19 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Transfer.Api.Clients;
-using EnergyOrigin.Domain.ValueObjects;
+using Microsoft.Extensions.Options;
 
 namespace API.IntegrationTests;
 
-public class MockAuthorizationClient : IAuthorizationClient
+public class MockAuthorizationClient(IOptionsMonitor<TestContext> testContext) : IAuthorizationClient
 {
-    public static List<UserOrganizationConsentsResponseItem> MockedConsents = new();
-
     public async Task<UserOrganizationConsentsResponse?> GetConsentsAsync()
     {
         await Task.Delay(1);
-        var response = new UserOrganizationConsentsResponse(MockedConsents);
 
-        return response;
+        var consentList = testContext.CurrentValue.ConsentList;
+
+        return consentList != null
+            ? new UserOrganizationConsentsResponse(consentList)
+            : new UserOrganizationConsentsResponse(new List<UserOrganizationConsentsResponseItem>());
     }
 }
+
+public class TestContext
+{
+    public List<UserOrganizationConsentsResponseItem>? ConsentList { get; set; }
+}
+
+public class TestContextMonitor : IOptionsMonitor<TestContext>
+{
+    private TestContext _currentValue = new();
+    public TestContext CurrentValue => _currentValue;
+
+    public TestContext Get(string? name) => _currentValue;
+    public IDisposable OnChange(Action<TestContext, string> listener) => throw new NotImplementedException();
+    public void Set(List<UserOrganizationConsentsResponseItem> consentList) => _currentValue = new TestContext { ConsentList = consentList };
+}
+
