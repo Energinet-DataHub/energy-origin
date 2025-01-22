53c5423a1d8a149565de08d60035282c5a99ae7e
sahma19 <saiuan@msn.com>
2025-01-22 22:49:18 +0100

    this should actually do it

--- domains/libraries/EnergyOrigin.Setup/EnergyOrigin.Setup/LoggerBuilder.cs
+++ domains/libraries/EnergyOrigin.Setup/EnergyOrigin.Setup/LoggerBuilder.cs
@@ -9,7 +9,8 @@ public static class LoggerBuilder
     {
         var loggerConfiguration = new LoggerConfiguration()
             .Filter.ByExcluding("RequestPath like '/health%'")
-            .Filter.ByExcluding("RequestPath like '/metrics%'");
+            .Filter.ByExcluding("RequestPath like '/metrics%'")
+            .MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", Serilog.Events.LogEventLevel.Warning);
 
         loggerConfiguration = loggerConfiguration.WriteTo.Console(new JsonFormatter());
 
