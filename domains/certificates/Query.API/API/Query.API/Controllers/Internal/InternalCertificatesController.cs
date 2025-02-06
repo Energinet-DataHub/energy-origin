a911b171593390711ba54ad39a32033a6cb9173e
sahma19 <saiuan@msn.com>
2025-02-06 13:34:48 +0100

    dotnet format

--- domains/certificates/Query.API/API/Query.API/Controllers/Internal/InternalCertificatesController.cs
+++ domains/certificates/Query.API/API/Query.API/Controllers/Internal/InternalCertificatesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -14,40 +14,40 @@ using MediatR;
 
 namespace API.Query.API.Controllers.Internal;
 
-    [Authorize(Policy = Policy.B2CInternal)]
-    [ApiController]
-    [Route("api/certificates/portal")]
-    public class InternalContractsController : ControllerBase
+[Authorize(Policy = Policy.B2CInternal)]
+[ApiController]
+[Route("api/certificates/portal")]
+public class InternalContractsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public InternalContractsController(IMediator mediator)
     {
-        private readonly IMediator _mediator;
-
-        public InternalContractsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-        [HttpGet]
-        [Route("internal-contracts")]
-        [ProducesResponseType(typeof(ContractsForAdminPortalResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(typeof(ContractsForAdminPortalResponse), StatusCodes.Status403Forbidden)]
-        public async Task<IActionResult> GetContractsForAdminPortal(
-            [FromServices] ILogger<InternalContractsController> logger, CancellationToken cancellationToken)
-        {
-            var queryResult = await _mediator.Send(new GetContractsForAdminPortalQuery(), cancellationToken);
-
-            var responseItems = queryResult.Result
-                .Select(c => new ContractsForAdminPortalResponseItem(
-                    c.GSRN,
-                    c.MeteringPointOwner,
-                    c.Created,
-                    c.StartDate,
-                    c.EndDate,
-                    c.MeteringPointType
-                )).ToList();
-
-            return Ok(new ContractsForAdminPortalResponse(responseItems));
-        }
+        _mediator = mediator;
     }
+
+    [HttpGet]
+    [Route("internal-contracts")]
+    [ProducesResponseType(typeof(ContractsForAdminPortalResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ContractsForAdminPortalResponse), StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> GetContractsForAdminPortal(
+        [FromServices] ILogger<InternalContractsController> logger, CancellationToken cancellationToken)
+    {
+        var queryResult = await _mediator.Send(new GetContractsForAdminPortalQuery(), cancellationToken);
+
+        var responseItems = queryResult.Result
+            .Select(c => new ContractsForAdminPortalResponseItem(
+                c.GSRN,
+                c.MeteringPointOwner,
+                c.Created,
+                c.StartDate,
+                c.EndDate,
+                c.MeteringPointType
+            )).ToList();
+
+        return Ok(new ContractsForAdminPortalResponse(responseItems));
+    }
+}
 
 
 public record ContractsForAdminPortalResponseItem(
