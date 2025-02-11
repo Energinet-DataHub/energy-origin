e9caa7f1f8dac636c6b75e809ebdb639b4659dc5
tnickelsen <TNE@energinet.dk>
2025-02-11 14:07:18 +0100

    Creates wallet if non exists on TA creation

--- domains/transfer/Transfer.API/API/Transfer/Api/[Features]/CreateTransferAgreementCommand.cs
+++ domains/transfer/Transfer.API/API/Transfer/Api/[Features]/CreateTransferAgreementCommand.cs
@@ -57,9 +57,21 @@ public class CreateTransferAgreementCommandHandler(IUnitOfWork UnitOfWork, IWall
 
         var wallets = await WalletClient.GetWallets(command.ReceiverOrganizationId, CancellationToken.None);
 
-        var walletId = wallets.Result.First().Id;
+        //This is needed in order for our preview environments to work, as we don't
+        //accept terms in preview environments. Can be removed when we accept terms
+        //in preview environments.
+        var walletId = wallets.Result.FirstOrDefault()?.Id;
+        if (walletId == null)
+        {
+            var createWalletResponse = await WalletClient.CreateWallet(command.ReceiverOrganizationId, cancellationToken);
 
-        var walletEndpoint = await WalletClient.CreateWalletEndpoint(walletId, command.ReceiverOrganizationId, CancellationToken.None);
+            if (createWalletResponse == null)
+                throw new ApplicationException("Failed to create wallet.");
+
+            walletId = createWalletResponse.WalletId;
+        }
+
+        var walletEndpoint = await WalletClient.CreateWalletEndpoint(walletId.Value, command.ReceiverOrganizationId, CancellationToken.None);
 
         var externalEndpoint = await WalletClient.CreateExternalEndpoint(command.SenderOrganizationId, walletEndpoint, SenderTin.Value, CancellationToken.None);
 
