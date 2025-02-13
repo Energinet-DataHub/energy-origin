9b4252610e3a2959ed0ea8d8a803cb9c770171b0
sahma19 <saiuan@msn.com>
2025-02-13 14:23:47 +0100

    done

--- domains/admin-portal/AdminPortal.API/Program.cs
+++ domains/admin-portal/AdminPortal.API/Program.cs
@@ -43,7 +43,6 @@ builder.Services.AddAuthentication(options =>
         options.ResponseType = OpenIdConnectResponseType.Code;
 
         options.SaveTokens = true;
-        options.CallbackPath = "/api/admin-portal/signin-oidc";
         options.GetClaimsFromUserInfoEndpoint = false;
 
         options.MapInboundClaims = false;
