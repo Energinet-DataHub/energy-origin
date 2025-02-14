e6fb7f9b4c34ca09e0c52d80056642daf31f73c2
sahma19 <saiuan@msn.com>
2025-02-14 12:25:19 +0100

    remove redundant code and packages

--- domains/admin-portal/AdminPortal.API/Services/AggregationService.cs
+++ domains/admin-portal/AdminPortal.API/Services/AggregationService.cs
@@ -52,7 +52,6 @@ public class AggregationService : IAggregationService
 
     private async Task<FirstPartyOrganizationsResponse> GetOrganizationsAsync(HttpClient client)
     {
-        // client.DefaultRequestHeaders.Remove("Authorization");
         var response = await client.GetAsync("first-party-organizations/");
         response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadFromJsonAsync<FirstPartyOrganizationsResponse>();
@@ -61,7 +60,6 @@ public class AggregationService : IAggregationService
 
     private async Task<ContractsForAdminPortalResponse> GetContractsAsync(HttpClient client)
     {
-        // client.DefaultRequestHeaders.Remove("Authorization");
         var response = await client.GetAsync("internal-contracts/");
         response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadFromJsonAsync<ContractsForAdminPortalResponse>();
