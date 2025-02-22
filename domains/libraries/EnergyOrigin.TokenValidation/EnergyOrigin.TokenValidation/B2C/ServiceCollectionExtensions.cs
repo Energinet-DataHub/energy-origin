e525709f375224d1f64a5791c112760b1adcb623
sahma19 <saiuan@msn.com>
2025-02-22 23:30:58 +0100

    live it

--- domains/libraries/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/B2C/ServiceCollectionExtensions.cs
+++ domains/libraries/EnergyOrigin.TokenValidation/EnergyOrigin.TokenValidation/B2C/ServiceCollectionExtensions.cs
@@ -98,7 +98,7 @@ public static class ServiceCollectionExtensions
 
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    ValidAudiences = new[] { "api://ett-internal", "12c3f3de-88a2-46d8-8122-8ea0db0f60bc" },
+                    ValidAudiences = new[] { "api://ett-internal" },
                     ValidateAudience = true,
                     ValidIssuers = new[] { $"https://login.microsoftonline.com/{tenantId}/v2.0" },
                     ValidateIssuer = true,
