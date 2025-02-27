6a38304197d81820529cf11a73091a20eb0fbf2c
sahma19 <saiuan@msn.com>
2025-02-27 11:30:30 +0100

    converted to options pattern

---
+++ domains/admin-portal/AdminPortal.Web/AdminPortal/Options/ClientOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminPortal.Options;
+
+public class ClientUriOptions
+{
+    public const string Prefix = "Endpoint";
+
+
+    public required string Authorization { get; set; }
+
+    public required string Certificates { get; set; }
+}
