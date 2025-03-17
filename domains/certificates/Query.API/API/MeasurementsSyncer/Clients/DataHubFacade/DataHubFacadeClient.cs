6db8bc0c608c4bdd41f7b085285dcfe5181a81fe
tnickelsen <TNE@energinet.dk>
2025-03-17 10:54:47 +0100

    log url

--- domains/certificates/Query.API/API/MeasurementsSyncer/Clients/DataHubFacade/DataHubFacadeClient.cs
+++ domains/certificates/Query.API/API/MeasurementsSyncer/Clients/DataHubFacade/DataHubFacadeClient.cs
@@ -5,6 +5,7 @@ using System.Net.Http.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using DataContext.ValueObjects;
+using Microsoft.Extensions.Logging;
 
 namespace API.MeasurementsSyncer.Clients.DataHubFacade;
 
@@ -16,15 +17,19 @@ public interface IDataHubFacadeClient
 public class DataHubFacadeClient : IDataHubFacadeClient
 {
     private readonly HttpClient _client;
+    private readonly ILogger<DataHubFacadeClient> _logger;
 
-    public DataHubFacadeClient(HttpClient client)
+    public DataHubFacadeClient(HttpClient client, ILogger<DataHubFacadeClient> logger)
     {
         _client = client;
+        _logger = logger;
     }
 
     public async Task<ListMeteringPointForCustomerCaResponse?> ListCustomerRelations(string owner, List<Gsrn> gsrns, CancellationToken cancellationToken)
     {
         var mpIdsUrl = string.Join("&meteringPointIds=", gsrns.Select(x => x.Value));
+        var url = $"/api/relation/meteringpoints/customer?subject={owner}&meteringPointIds={mpIdsUrl}";
+        _logger.LogInformation("Url: " + url);
         return await _client.GetFromJsonAsync<ListMeteringPointForCustomerCaResponse>($"/api/relation/meteringpoints/customer?subject={owner}&meteringPointIds={mpIdsUrl}", cancellationToken);
     }
 }
