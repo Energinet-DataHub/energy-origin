1f50b6acb0fbbf51dc432d5845420d2e0fb07427
Theis Nickelsen <130448180+tnickelsen@users.noreply.github.com>
2025-03-24 12:50:06 +0100

    4092 removed client logging (#1821)

--- domains/certificates/Query.API/API/MeasurementsSyncer/Clients/DataHub3/DataHub3Client.cs
+++ domains/certificates/Query.API/API/MeasurementsSyncer/Clients/DataHub3/DataHub3Client.cs
@@ -18,21 +18,16 @@ public interface IDataHub3Client
 public class DataHub3Client : IDataHub3Client
 {
     private readonly HttpClient _client;
-    private readonly ILogger<DataHub3Client> _logger;
 
-    public DataHub3Client(HttpClient client, ILogger<DataHub3Client> logger)
+    public DataHub3Client(HttpClient client)
     {
         _client = client;
-        _logger = logger;
     }
 
     public async Task<MeteringPointData[]?> GetMeasurements(List<Gsrn> gsrns, long dateFromEpoch, long dateToEpoch, CancellationToken cancellationToken)
     {
         var meteringPointIds = string.Join(",", gsrns.Select(x => x.Value));
         var url = $"ListAggregatedTimeSeries?meteringPointIds={meteringPointIds}&dateFromEpoch={dateFromEpoch}&dateToEpoch={dateToEpoch}&Aggregation=Hour";
-
-        var baseUrl = _client.BaseAddress;
-        _logger.LogError("Url: " + baseUrl + url);
 
         return await _client.GetFromJsonAsync<MeteringPointData[]>(url, cancellationToken: cancellationToken);
     }
