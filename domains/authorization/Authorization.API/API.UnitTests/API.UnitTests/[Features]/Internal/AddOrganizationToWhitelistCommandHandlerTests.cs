1c26b89b7a7b334a9439bd1ce6b81db2bbd01b5c
sahma19 <saiuan@msn.com>
2025-03-26 16:13:58 +0100

    Mock databases do not enforce unique constraints. deleted as we already have an integration test proving it works.

--- domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/[Features]/Internal/AddOrganizationToWhitelistCommandHandlerTests.cs
+++ domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/[Features]/Internal/AddOrganizationToWhitelistCommandHandlerTests.cs
@@ -22,19 +22,4 @@ public class AddOrganizationToWhitelistCommandHandlerTests
 
         _repository.Query().Should().ContainSingle(w => w.Tin == tin);
     }
-
-    [Fact]
-    public async Task Given_OrganizationAlreadyExists_WhenHandlingCommand_Then_DoesNotAdd()
-    {
-        var tin = Tin.Create("12345678");
-        var existing = Whitelisted.Create(tin);
-        await _repository.AddAsync(existing, CancellationToken.None);
-
-        var command = new AddOrganizationToWhitelistCommand(tin);
-        var handler = new AddOrganizationToWhitelistCommandHandler(_repository, _unitOfWork);
-
-        await handler.Handle(command, CancellationToken.None);
-
-        _repository.Query().Count(w => w.Tin == tin).Should().Be(1);
-    }
 }
