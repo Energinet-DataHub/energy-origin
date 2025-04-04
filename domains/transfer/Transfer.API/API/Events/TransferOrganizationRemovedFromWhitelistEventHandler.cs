2405c7938f081b5c5d57a3199e86661dabdff7a5
tnickelsen <TNE@energinet.dk>
2025-04-04 11:04:25 +0200

    Added logging

--- domains/transfer/Transfer.API/API/Events/TransferOrganizationRemovedFromWhitelistEventHandler.cs
+++ domains/transfer/Transfer.API/API/Events/TransferOrganizationRemovedFromWhitelistEventHandler.cs
@@ -5,22 +5,26 @@ using EnergyOrigin.Domain.ValueObjects;
 using EnergyOrigin.IntegrationEvents.Events.OrganizationRemovedFromWhitelist.V1;
 using MassTransit;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace API.Events;
 
 public class TransferOrganizationRemovedFromWhitelistEventHandler : IConsumer<OrganizationRemovedFromWhitelist>
 {
     private readonly IMediator _mediator;
+    private readonly ILogger<TransferOrganizationRemovedFromWhitelistEventHandler> _logger;
 
-    public TransferOrganizationRemovedFromWhitelistEventHandler(IMediator mediator)
+    public TransferOrganizationRemovedFromWhitelistEventHandler(IMediator mediator, ILogger<TransferOrganizationRemovedFromWhitelistEventHandler> logger)
     {
         _mediator = mediator;
+        _logger = logger;
     }
 
     public async Task Consume(ConsumeContext<OrganizationRemovedFromWhitelist> context)
     {
         var e = context.Message;
 
+        _logger.LogInformation("Organization {orgId} removed from whitelist, removing all transfer agreements", e.OrganizationId);
         var cmd = new DeleteTransferAgreementsCommand(OrganizationId.Create(e.OrganizationId));
         await _mediator.Send(cmd);
     }
