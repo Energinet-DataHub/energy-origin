476a5cea7291344f027c200bba28682e80bd329d
sahma19 <saiuan@msn.com>
2025-04-09 13:42:10 +0200

    reset

--- domains/libraries/EnergyTrackAndTrace.Testing/Testcontainers/PostgresContainer.cs
+++ domains/libraries/EnergyTrackAndTrace.Testing/Testcontainers/PostgresContainer.cs
@@ -10,14 +10,13 @@ namespace EnergyTrackAndTrace.Testing.Testcontainers;
 public class PostgresContainer : IAsyncLifetime
 {
     public PostgreSqlContainer TestContainer { get; } = new PostgreSqlBuilder()
-        .WithImage("postgres:15") // We use this version in production
-        .WithDatabase("postgres")
+        .WithImage("postgres:latest")
+        .WithDatabase("db")
         .WithUsername("postgres")
         .WithPassword("postgres")
-        .WithCleanUp(true)
-        .WithCommand("postgres", "-c", "fsync=off", "-c", "synchronous_commit=off", "-c", "full_page_writes=off") // Faster I/O for testing
         .WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted("pg_isready").UntilPortIsAvailable(5432))
         .WithPortBinding(PostgreSqlBuilder.PostgreSqlPort, true)
+        .WithCleanUp(true)
         .Build();
 
     public string ConnectionString => TestContainer.GetConnectionString();
@@ -43,12 +42,19 @@ public class DatabaseInfo
 {
     public string ConnectionString { get; private set; }
 
-    public DatabaseInfo(string connectionString) => ConnectionString = connectionString;
+    public DatabaseInfo(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
 
     public string Host => GetValue("Host");
+
     public string Port => GetValue("Port");
+
     public string Name => GetValue("Database");
+
     public string User => GetValue("Username");
+
     public string Password => GetValue("Password");
 
     private string GetValue(string key)
