8a5903caff6239bee14b14f9646635ecd5c159fd
Casper Olsen <cabol@energinet.dk>
2025-04-11 08:30:55 +0200

    Credential service tests

--- domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/Service/CredentialServiceTest.cs
+++ domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/Service/CredentialServiceTest.cs
@@ -1,6 +1,7 @@
 using API.Services;
 using EnergyOrigin.Setup.Exceptions;
 using Microsoft.Graph.Applications.Item.AddPassword;
+using Microsoft.Graph.Applications.Item.RemovePassword;
 using Microsoft.Graph.Models;
 using Microsoft.Graph.Models.ODataErrors;
 using NSubstitute;
@@ -221,6 +222,168 @@ public class CredentialServiceTest
         // Assert
         Assert.IsType<BusinessException>(exception);
         Assert.Contains("Not allowed to add 3 credentials", exception.Message);
+    }
+
+    [Fact]
+    public async Task GivenCreateCredential_PasswordAdded_ReturnsCredential()
+    {
+        // Arrange
+        var applicationId = Guid.NewGuid();
+        var application = new Application
+        {
+            Id = applicationId.ToString()
+        };
+
+        _graphServiceClientWrapper
+            .GetApplication(applicationId.ToString(), Arg.Any<CancellationToken>())
+            .Returns(application);
+
+        var startDateTime = new DateTimeOffset(2025, 4, 9, 0, 0, 0, new TimeSpan(0));
+        var passwordCredential = new PasswordCredential
+        {
+            KeyId = Guid.NewGuid(),
+            Hint = "hint",
+            StartDateTime = startDateTime,
+            EndDateTime = startDateTime.AddHours(1),
+            SecretText = "secret"
+        };
+
+        _graphServiceClientWrapper
+            .AddPassword(applicationId.ToString(), Arg.Any<AddPasswordPostRequestBody>(), Arg.Any<CancellationToken>())
+            .Returns(passwordCredential);
+
+        // Act
+        var result = await _sut.CreateCredential(applicationId, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(passwordCredential.Hint, result.Hint);
+        Assert.Equal(passwordCredential.KeyId, result.KeyId);
+        Assert.Equal(passwordCredential.StartDateTime, result.StartDateTime);
+        Assert.Equal(passwordCredential.EndDateTime, result.EndDateTime);
+        Assert.Equal(passwordCredential.SecretText, result.Secret);
+    }
+
+    [Fact]
+    public async Task GivenDeleteCredential_WithApplicationThatDoesNotExists_ThrowException()
+    {
+        // Arrange
+        var applicationId = Guid.NewGuid();
+        ReturnNullApplication(applicationId);
+
+        var keyId = Guid.NewGuid();
+
+        // Act
+        var exception = await Record.ExceptionAsync(
+            async () => await _sut.DeleteCredential(applicationId, keyId, CancellationToken.None));
+
+        // Assert
+        Assert.IsType<ResourceNotFoundException>(exception);
+    }
+
+    [Fact]
+    public async Task GivenDeleteCredential_WithODataError_ThrowException()
+    {
+        // Arrange
+        var applicationId = Guid.NewGuid();
+        ThrowODataErrorWhenGettingApplication(applicationId);
+
+        var keyId = Guid.NewGuid();
+
+        // Act
+        var exception = await Record.ExceptionAsync(
+            async () => await _sut.DeleteCredential(applicationId, keyId, CancellationToken.None));
+
+        // Assert
+        Assert.IsType<ResourceNotFoundException>(exception);
+    }
+
+    [Fact]
+    public async Task GivenDeleteCredential_NoPasswordCredentials_ThrowException()
+    {
+        // Arrange
+        var applicationId = Guid.NewGuid();
+        var application = new Application
+        {
+            Id = applicationId.ToString(),
+            PasswordCredentials = null
+        };
+
+        _graphServiceClientWrapper
+            .GetApplication(applicationId.ToString(), Arg.Any<CancellationToken>())
+            .Returns(application);
+
+        var keyId = Guid.NewGuid();
+
+        // Act
+        var exception = await Record.ExceptionAsync(
+            async () => await _sut.DeleteCredential(applicationId, keyId, CancellationToken.None));
+
+        // Assert
+        Assert.IsType<ResourceNotFoundException>(exception);
+    }
+
+    [Fact]
+    public async Task GivenDeleteCredential_ODataErrorWhenGettingPassword_ThrowException()
+    {
+        // Arrange
+        var applicationId = Guid.NewGuid();
+        var application = new Application
+        {
+            Id = applicationId.ToString(),
+            PasswordCredentials = []
+        };
+
+        _graphServiceClientWrapper
+            .GetApplication(applicationId.ToString(), Arg.Any<CancellationToken>())
+            .Returns(application);
+
+        _graphServiceClientWrapper
+            .RemovePassword(
+                applicationId.ToString(),
+                Arg.Any<RemovePasswordPostRequestBody>(),
+                Arg.Any<CancellationToken>())
+            .ThrowsAsync(new ODataError());
+
+        var keyId = Guid.NewGuid();
+
+        // Act
+        var exception = await Record.ExceptionAsync(
+            async () => await _sut.DeleteCredential(applicationId, keyId, CancellationToken.None));
+
+        // Assert
+        Assert.IsType<BusinessException>(exception);
+    }
+
+    [Fact]
+    public async Task GivenDeleteCredential_PasswordRemoved_DoesNotThrowException()
+    {
+        // Arrange
+        var applicationId = Guid.NewGuid();
+        var application = new Application
+        {
+            Id = applicationId.ToString(),
+            PasswordCredentials = []
+        };
+
+        _graphServiceClientWrapper
+            .GetApplication(applicationId.ToString(), Arg.Any<CancellationToken>())
+            .Returns(application);
+
+        _graphServiceClientWrapper
+            .RemovePassword(
+                applicationId.ToString(),
+                Arg.Any<RemovePasswordPostRequestBody>(),
+                Arg.Any<CancellationToken>())
+            .Returns(Task.CompletedTask);
+
+        var keyId = Guid.NewGuid();
+
+        // Act
+        var exception = await Record.ExceptionAsync(
+            async () => await _sut.DeleteCredential(applicationId, keyId, CancellationToken.None));
+
+        // Assert
+        Assert.Null(exception);
     }
 
     private void ReturnNullApplication(Guid applicationId)
