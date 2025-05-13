31d1ef3f4993bee2d78e9e4f3c92b37b00968b02
sahma19 <saiuan@msn.com>
2025-05-13 19:59:57 +0200

    Add migrations

---
+++ domains/certificates/Shared/DataContext/Migrations/Scripts/20250512-0031-AddSponsorshipsTable.sql
@@ -0,0 +1,21 @@
+﻿DO
+$$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+          FROM information_schema.tables
+         WHERE table_schema = 'public'
+           AND table_name   = 'Sponsorships'
+    ) THEN
+
+CREATE TABLE public."Sponsorships"
+(
+    "SponsorshipGSRN"           TEXT                     NOT NULL,
+    "SponsorshipEndDate"        timestamp with time zone NOT NULL,
+
+    CONSTRAINT "PK_Sponsorships" PRIMARY KEY ("SponsorshipGSRN")
+);
+
+END IF;
+END;
+$$;
