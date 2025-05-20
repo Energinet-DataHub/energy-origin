01cd1ad55ac4b62b146521cd3ed26ac4ec814ef7
cfk <cfk@energinet.dk>
2025-05-20 14:54:06 +0200

    update proxy

--- domains/authorization/Proxy/Controllers/ClaimsController.cs
+++ domains/authorization/Proxy/Controllers/ClaimsController.cs
@@ -95,6 +95,11 @@ public class ClaimsController : ProxyBase
     }
 }
 
+public enum TimeMatch
+{
+    Hourly,
+    All,
+}
 
 public record GetClaimsQueryParameters
 {
@@ -118,6 +123,14 @@ public record GetClaimsQueryParameters
     /// </summary>
     [DefaultValue(0)]
     public int Skip { get; init; }
+
+    /// <summary>
+    /// Fetch all or hourly claims.
+    /// Values:
+    /// All
+    /// Hourly (default)
+    /// </summary>
+    public TimeMatch TimeMatch { get; init; } = TimeMatch.Hourly;
 }
 
 public record AggregateClaimsQueryParameters
