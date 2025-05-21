f3c60f96db5595092f7a355f81279a727281ae45
sahma19 <shd@energinet.dk>
2025-05-21 11:56:13 +0200

    atart report generation endpoint (#1914)

---
+++ domains/transfer/Shared/DataContext/Migrations/Scripts/20251505-0031-AddReportsTable.sql
@@ -0,0 +1,22 @@
+﻿DO
+$$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM information_schema.tables
+     WHERE table_schema = 'public' AND table_name = 'Reports'
+  ) THEN
+CREATE TABLE public."Reports" (
+                                  "Id"              uuid NOT NULL,
+                                  "CreatedAt"       timestamp with time zone DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC') NOT NULL,
+                                  "StartDate"       timestamp with time zone NOT NULL,
+                                  "EndDate"         timestamp with time zone NOT NULL,
+                                  "Status"          text NOT NULL,
+                                  "Content"         bytea,
+                                  "OrganizationId"  uuid NOT NULL,
+                                  CONSTRAINT "PK_Reports" PRIMARY KEY ("Id")
+);
+CREATE INDEX "IX_Reports_OrganizationId"
+    ON public."Reports" ("OrganizationId");
+END IF;
+END;
+$$;
--- domains/transfer/Transfer.API/API/Shared/ChangeAction.cs
+++
@@ -1,3 +0,0 @@
-namespace API.Shared;
-
-public enum ChangeAction { Created = 1, Updated = 2, Deleted = 3 }
