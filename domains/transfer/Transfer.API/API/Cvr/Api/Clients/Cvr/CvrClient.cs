908070e361ce6ef64745af256d2bc56f06f99e91
Casper Olsen <cabol@energinet.dk>
2025-05-26 10:04:47 +0200

    Batch requests to cvr

--- domains/transfer/Transfer.API/API/Cvr/Api/Clients/Cvr/CvrClient.cs
+++ domains/transfer/Transfer.API/API/Cvr/Api/Clients/Cvr/CvrClient.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
@@ -13,41 +14,65 @@ public interface ICvrClient
     Task<Root?> CvrNumberSearch(IEnumerable<CvrNumber> cvrNumbers);
 }
 
-public class CvrClient : ICvrClient
+public class CvrClient(HttpClient client) : ICvrClient
 {
-    private readonly HttpClient client;
-
-    public CvrClient(HttpClient client)
-    {
-        this.client = client;
-    }
+    private const int BatchSize = 25;
 
     public async Task<Root?> CvrNumberSearch(IEnumerable<CvrNumber> cvrNumbers)
     {
-        var cvrNumbersArray = JsonSerializer.Serialize(cvrNumbers);
-
         var from = 0;
-        var size = 5000;
-        var postBody = $@"{{
-                            ""from"": {from},
-                            ""size"": {size},
-                            ""query"": {{
-                                ""bool"": {{
-                                    ""must"": [
-                                        {{
-                                            ""terms"": {{
-                                                ""Vrvirksomhed.cvrNummer"": {cvrNumbersArray}
-                                            }}
-                                        }}
-                                    ]
+        var cvrArray = cvrNumbers.ToArray();
+        var tasks = new List<Task<HttpResponseMessage>>();
+        for (int i = 0; i < cvrNumbers.Count(); i += BatchSize)
+        {
+            var batch = cvrArray.Skip(i).Take(BatchSize);
+            var serializedBatch = JsonSerializer.Serialize(batch);
+            var body = GetBody(serializedBatch, from, BatchSize);
+            var content = new StringContent(body, Encoding.UTF8, "application/json");
+
+            tasks.Add(client.PostAsync("cvr-permanent/virksomhed/_search", content));
+
+        }
+
+        var responses = await Task.WhenAll(tasks);
+
+        var result = new Root
+        {
+            hits = new HitsRoot
+            {
+                hits = []
+            }
+        };
+
+        foreach (var response in responses)
+        {
+            var root = await response.Content.ReadFromJsonAsync<Root>();
+            if (root?.hits?.hits is not null)
+            {
+                result.hits.hits.AddRange(root.hits.hits);
+            }
+        }
+
+        return result;
+
+    }
+
+    private static string GetBody(string cvrNumbersArray, int from, int size)
+    {
+        return $@"{{
+                    ""from"": {from},
+                    ""size"": {size},
+                    ""query"": {{
+                        ""bool"": {{
+                            ""must"": [
+                                {{
+                                    ""terms"": {{
+                                        ""Vrvirksomhed.cvrNummer"": {cvrNumbersArray}
+                                    }}
                                 }}
-                            }}
-                        }}";
-
-        var content = new StringContent(postBody, Encoding.UTF8, "application/json");
-
-        var res = await client.PostAsync("cvr-permanent/virksomhed/_search", content);
-
-        return await res.Content.ReadFromJsonAsync<Root>();
+                            ]
+                        }}
+                    }}
+                }}";
     }
 }
