f0dad539927c6ab1ab9ab6000dfecba7eb03dcea
sahma19 <shd@energinet.dk>
2025-05-29 10:22:44 +0200

    revert client (#1957)

--- domains/libraries/EnergyOrigin.Datahub3/EnergyOrigin.Datahub3/DataHub3Client.cs
+++ domains/libraries/EnergyOrigin.Datahub3/EnergyOrigin.Datahub3/DataHub3Client.cs
@@ -1,6 +1,5 @@
 using System.Net.Http.Json;
 using EnergyOrigin.Domain.ValueObjects;
-using Microsoft.Extensions.Options;
 
 namespace EnergyOrigin.Datahub3;
 
@@ -12,57 +11,17 @@ public interface IDataHub3Client
 public class DataHub3Client : IDataHub3Client
 {
     private readonly HttpClient _client;
-    private readonly bool _useMock;
 
-    public DataHub3Client(
-        HttpClient client,
-        IOptions<DataHub3Options> opts)
+    public DataHub3Client(HttpClient client)
     {
         _client = client;
-        _useMock = opts.Value.EnableMock;
     }
 
-    public async Task<MeteringPointData[]?> GetMeasurements(
-        List<Gsrn> gsrns,
-        long dateFromEpoch,
-        long dateToEpoch,
-        CancellationToken cancellationToken)
+    public async Task<MeteringPointData[]?> GetMeasurements(List<Gsrn> gsrns, long dateFromEpoch, long dateToEpoch, CancellationToken cancellationToken)
     {
-        if (_useMock)
-        {
-            var allResults = new List<MeteringPointData>();
-            foreach (var g in gsrns)
-            {
-                var url = $"/ListAggregatedTimeSeries"
-                          + $"?meteringPointIds={g.Value}"
-                          + $"&dateFromEpoch={dateFromEpoch}"
-                          + $"&dateToEpoch={dateToEpoch}"
-                          + $"&Aggregation=Hour";
+        var meteringPointIds = string.Join(",", gsrns.Select(x => x.Value));
+        var url = $"ListAggregatedTimeSeries?meteringPointIds={meteringPointIds}&dateFromEpoch={dateFromEpoch}&dateToEpoch={dateToEpoch}&Aggregation=Hour";
 
-                var single = await _client
-                    .GetFromJsonAsync<MeteringPointData[]?>(
-                        url,
-                        cancellationToken: cancellationToken);
-
-                if (single != null)
-                    allResults.AddRange(single);
-            }
-
-            return allResults.ToArray();
-        }
-        else
-        {
-            var meteringPointIds = string.Join(",", gsrns.Select(x => x.Value));
-            var url = $"/ListAggregatedTimeSeries"
-                      + $"?meteringPointIds={meteringPointIds}"
-                      + $"&dateFromEpoch={dateFromEpoch}"
-                      + $"&dateToEpoch={dateToEpoch}"
-                      + $"&Aggregation=Hour";
-
-            return await _client
-                .GetFromJsonAsync<MeteringPointData[]?>(
-                    url,
-                    cancellationToken: cancellationToken);
-        }
+        return await _client.GetFromJsonAsync<MeteringPointData[]>(url, cancellationToken: cancellationToken);
     }
 }
