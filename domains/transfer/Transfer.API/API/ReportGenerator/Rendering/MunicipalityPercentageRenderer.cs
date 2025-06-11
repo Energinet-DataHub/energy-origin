f9ea495b0c0e091ed4e3013c6fda3a9e8edb549d
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-11 20:31:02 +0200

    try another approach

--- domains/transfer/Transfer.API/API/ReportGenerator/Rendering/MunicipalityPercentageRenderer.cs
+++ domains/transfer/Transfer.API/API/ReportGenerator/Rendering/MunicipalityPercentageRenderer.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using API.ReportGenerator.Domain;
 using EnergyOrigin.Domain.ValueObjects.Mappers;
@@ -15,24 +15,32 @@ public class MunicipalityPercentageRenderer : IMunicipalityPercentageRenderer
 {
     public string Render(List<MunicipalityDistribution> municipalities)
     {
+        if (municipalities.Count == 0)
+            return "<p>Ingen kommunedata tilgængelig</p>";
+
         var ordered = municipalities.OrderByDescending(x => x.Percentage).ToList();
-        var top3Municipalities = ordered.Take(3);
+        var top3 = ordered.Take(3);
         var rest = ordered.Skip(3);
 
-        var html2 = @"<h6 class=""section-title"">Kommuner</h6>
-                    <ul>";
+        var html = @"<h6 class=""section-title"">Kommuner</h6><ul>";
 
-        foreach (var municipality in top3Municipalities)
+        foreach (var m in top3)
         {
-            var name = MunicipalityCodeMapper.GetMunicipalityName(municipality.Municipality);
-            if (name != null)
-                html2 += $"<li>{name}: {municipality.Percentage:0}%</li>";
+            var name = MunicipalityCodeMapper.GetMunicipalityName(m.Municipality);
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                var safeName = System.Net.WebUtility.HtmlEncode(name);
+                var formattedPct = m.Percentage.ToString("0.0", CultureInfo.InvariantCulture);
+                html += $"<li>{safeName}: {formattedPct}%</li>";
+            }
         }
 
-        html2 += $"<li>Andre kommuner: {rest.Sum(x => x.Percentage):0}%</li>";
+        var restSum = rest.Sum(x => x.Percentage);
+        var formattedRest = restSum.ToString("0.0", CultureInfo.InvariantCulture);
+        html += $"<li>Andre kommuner: {formattedRest}%</li>";
 
-        html2 += "</ul>";
+        html += "</ul>";
 
-        return html2.Trim();
+        return html.Trim();
     }
 }
