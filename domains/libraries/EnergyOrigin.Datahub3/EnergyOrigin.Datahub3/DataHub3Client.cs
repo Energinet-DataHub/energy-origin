4965c35b0e60b2be9cae12e9ddb904e24b2dcc5c
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-11 23:35:48 +0200

    it's time

--- domains/libraries/EnergyOrigin.Datahub3/EnergyOrigin.Datahub3/DataHub3Client.cs
+++ domains/libraries/EnergyOrigin.Datahub3/EnergyOrigin.Datahub3/DataHub3Client.cs
@@ -17,11 +17,27 @@ public class DataHub3Client : IDataHub3Client
         _client = client;
     }
 
-    public async Task<MeteringPointData[]?> GetMeasurements(List<Gsrn> gsrns, long dateFromEpoch, long dateToEpoch, CancellationToken cancellationToken)
+    public async Task<MeteringPointData[]?> GetMeasurements(
+        List<Gsrn> gsrns,
+        long dateFromEpoch,
+        long dateToEpoch,
+        CancellationToken cancellationToken)
     {
-        var meteringPointIds = string.Join(",", gsrns.Select(x => x.Value));
-        var url = $"ListAggregatedTimeSeries?meteringPointIds={meteringPointIds}&dateFromEpoch={dateFromEpoch}&dateToEpoch={dateToEpoch}&Aggregation=Hour";
+        var allResults = new List<MeteringPointData>();
+        foreach (var g in gsrns)
+        {
+            var url = $"/ListAggregatedTimeSeries?meteringPointIds={g.Value}"
+                      + $"&dateFromEpoch={dateFromEpoch}"
+                      + $"&dateToEpoch={dateToEpoch}"
+                      + $"&Aggregation=Hour";
 
-        return await _client.GetFromJsonAsync<MeteringPointData[]>(url, cancellationToken: cancellationToken);
+            var single = await _client
+                .GetFromJsonAsync<MeteringPointData[]?>(url, cancellationToken: cancellationToken);
+
+            if (single != null)
+                allResults.AddRange(single);
+        }
+
+        return allResults.ToArray();
     }
 }
