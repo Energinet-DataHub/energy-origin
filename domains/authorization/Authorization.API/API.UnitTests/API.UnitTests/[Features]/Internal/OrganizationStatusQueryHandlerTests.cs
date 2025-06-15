54f73e71304c520f95026d9cc1e174eda6b2160f
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-15 16:04:47 +0200

    final auth

---
+++ domains/authorization/Authorization.API/API.UnitTests/API.UnitTests/[Features]/Internal/OrganizationStatusQueryHandlerTests.cs
@@ -0,0 +1,76 @@
+using API.Authorization._Features_.Internal;
+using API.Models;
+using API.UnitTests.Repository;
+using EnergyOrigin.Domain.ValueObjects;
+
+namespace API.UnitTests._Features_.Internal;
+
+public class GetOrganizationStatusQueryHandlerTests
+{
+    private readonly GetOrganizationStatusQueryHandler _sut;
+
+    public GetOrganizationStatusQueryHandlerTests()
+    {
+        var fakeOrgRepo = new FakeOrganizationRepository();
+        _sut = new GetOrganizationStatusQueryHandler(fakeOrgRepo);
+
+        Dictionary<string, Organization> orgMap = new()
+        {
+            ["12345678"] = Any.Organization(tin: Tin.Create("12345678")),
+            ["87654321"] = Any.TrialOrganization(tin: Tin.Create("87654321")),
+            ["69696969"] = Any.DeactivatedOrganization(tin: Tin.Create("69696969"))
+        };
+
+        foreach (var org in orgMap.Values)
+            fakeOrgRepo.AddAsync(org, CancellationToken.None).Wait();
+    }
+
+    [Theory]
+    [InlineData("00000000", "trial", true, OrganizationStatus.Trial)]
+    [InlineData("00000000", "normal", true, OrganizationStatus.Normal)]
+    [InlineData("12345678", "normal", true, OrganizationStatus.Normal)]
+    [InlineData("87654321", "trial", true, OrganizationStatus.Trial)]
+    [InlineData("87654321", "normal", false, OrganizationStatus.Trial)]
+    [InlineData("12345678", "trial", false, OrganizationStatus.Normal)]
+    [InlineData("69696969", "trial", false, OrganizationStatus.Deactivated)]
+    [InlineData("69696969", "normal", false, OrganizationStatus.Deactivated)]
+    [InlineData("87654321", "TrIaL", true, OrganizationStatus.Trial)]
+    public async Task LoginTypeValidation_ReturnsExpectedResult(string tin, string loginType, bool expectedValid, OrganizationStatus? expectedStatus)
+    {
+        var query = new GetOrganizationStatusQuery(tin, loginType);
+        var result = await _sut.Handle(query, CancellationToken.None);
+
+        Assert.Equal(expectedValid, result.IsValid);
+        Assert.Equal(expectedStatus, result.OrgStatus);
+    }
+
+    [Theory]
+    [InlineData("", "trial")]
+    [InlineData("", "normal")]
+    public async Task EmptyTin_ThrowsArgumentException(string tin, string loginType)
+    {
+        var query = new GetOrganizationStatusQuery(tin, loginType);
+        await Assert.ThrowsAsync<ArgumentException>(() => _sut.Handle(query, CancellationToken.None));
+    }
+
+    [Theory]
+    [InlineData("12345678", "vip")]
+    [InlineData("12345678", "")]
+    [InlineData("12345678", " ")]
+    [InlineData("12345678", "\t")]
+    [InlineData("12345678", "\n")]
+    [InlineData("87654321", "")]
+    [InlineData("87654321", " ")]
+    [InlineData("87654321", "\t")]
+    [InlineData("87654321", "\n")]
+    [InlineData("69696969", "")]
+    [InlineData("69696969", " ")]
+    [InlineData("69696969", "\t")]
+    [InlineData("69696969", "\n")]
+    public async Task InvalidLoginTypes_ReturnExpected(string tin, string loginType)
+    {
+        var query = new GetOrganizationStatusQuery(tin, loginType);
+        var result = await _sut.Handle(query, CancellationToken.None);
+        Assert.False(result.IsValid);
+    }
+}
---
+++ domains/authorization/Authorization.API/API/Authorization/[Features]/Internal/OrganizationStatusQueryHandler.cs
@@ -0,0 +1,35 @@
+using System.Threading;
+using System.Threading.Tasks;
+using API.Models;
+using API.Repository;
+using EnergyOrigin.Domain.ValueObjects;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Authorization._Features_.Internal;
+
+public class GetOrganizationStatusQueryHandler(IOrganizationRepository organizationRepository)
+    : IRequestHandler<GetOrganizationStatusQuery, LoginTypeValidationResult>
+{
+    public async Task<LoginTypeValidationResult> Handle(GetOrganizationStatusQuery request, CancellationToken cancellationToken)
+    {
+        var tin = Tin.Create(request.Tin);
+        var organization = await organizationRepository.Query()
+            .FirstOrDefaultAsync(w => w.Tin == tin, cancellationToken);
+
+        var status = organization?.Status;
+        var loginType = request.LoginType.ToLowerInvariant();
+
+        return (loginType, status) switch
+        {
+            ("normal", OrganizationStatus.Normal) => new LoginTypeValidationResult(true, OrganizationStatus.Normal),
+            ("normal", null) => new LoginTypeValidationResult(true, OrganizationStatus.Normal),
+            ("trial", OrganizationStatus.Trial) => new LoginTypeValidationResult(true, OrganizationStatus.Trial),
+            ("trial", null) => new LoginTypeValidationResult(true, OrganizationStatus.Trial),
+            _ => new LoginTypeValidationResult(false, status)
+        };
+    }
+}
+
+public record GetOrganizationStatusQuery(string Tin, string LoginType) : IRequest<LoginTypeValidationResult>;
+public record LoginTypeValidationResult(bool IsValid, OrganizationStatus? OrgStatus);
