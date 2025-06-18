28fda4a29ea754509d77545d7b74b72ab69b1b5d
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-18 14:43:19 +0200

    ship it

--- domains/authorization/Authorization.API/API.IntegrationTests/API.IntegrationTests/API/ValidateLoginTypeIntegrationTests.cs
+++ domains/authorization/Authorization.API/API.IntegrationTests/API.IntegrationTests/API/ValidateLoginTypeIntegrationTests.cs
@@ -47,49 +47,6 @@ public class ValidateLoginTypeIntegrationTest : IntegrationTestBase
         Assert.Equal("trial", json.GetProperty("org_status").GetString());
     }
 
-    [Theory]
-    [InlineData("normal", "trial", "a1b2c3d4-e111-4444-aaaa-aaaaaaaaaaaa - Trial Organization is not allowed to log in as a Normal Organization - Please log in as Trial Organization, or contact support, if you think this is an error")]
-    [InlineData("trial", "normal", "b2c3d4e5-e222-5555-bbbb-bbbbbbbbbbbb - Normal Organization is not allowed to log in as a Trial organization - Please log in as Normal Organization, or contact support, if you think this is an error")]
-    [InlineData("trial", "deactivated", "c3d4e5f6-e333-6666-cccc-cccccccccccc - Organization is deactivated - Please contact support, if you think this is an error")]
-    [InlineData("trial", "", "d4e5f6g7-e999-8888-eeee-eeeeeeeeeeee - Unhandled Exception")]
-    [InlineData("normal", "", "d4e5f6g7-e999-8888-eeee-eeeeeeeeeeee - Unhandled Exception")]
-    [InlineData("deactivated", "", "d4e5f6g7-e999-8888-eeee-eeeeeeeeeeee - Unhandled Exception")]
-    [InlineData("vip", "", "d4e5f6g7-e999-8888-eeee-eeeeeeeeeeee - Unhandled Exception")]
-    [InlineData("vip", "normal", "d4e5f6g7-e999-8888-eeee-eeeeeeeeeeee - Unhandled Exception")]
-    public async Task GivenLoginTypeAndOrgStatusMismatch_WhenQueryingEndpoint_Then_ReturnHttp409WithB2cStatus409_And_ExpectedFailureReason(string loginTypeRequestParameter, string? orgStatusQueryHandlerResult, string expectedReason)
-    {
-        var tin = orgStatusQueryHandlerResult switch
-        {
-            "trial" => "87654321",
-            "normal" => "12345678",
-            "deactivated" => "69696969",
-            _ => throw new ArgumentException("Unsupported org status")
-        };
-
-        var org = orgStatusQueryHandlerResult switch
-        {
-            "trial" => Any.TrialOrganization(tin: Tin.Create(tin)),
-            "normal" => Any.Organization(tin: Tin.Create(tin)),
-            "deactivated" => Any.DeactivatedOrganization(tin: Tin.Create(tin)),
-            _ => throw new ArgumentException("Unsupported org status")
-        };
-
-        await using (var dbContext = new ApplicationDbContext(_options))
-        {
-            await dbContext.Organizations.AddAsync(org, CancellationToken.None);
-            await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
-        }
-
-        var request = new DoesOrganizationStatusMatchLoginTypeRequest(org.Tin!.Value, loginTypeRequestParameter);
-        var response = await _api.GetDoesLoginTypeMatch(request);
-
-        Assert.Equal(HttpStatusCode.Conflict, response.StatusCode);
-        var json = await response.Content.ReadFromJsonAsync<JsonElement>(cancellationToken: TestContext.Current.CancellationToken);
-        Assert.Equal(expectedReason, json.GetProperty("userMessage").GetString());
-        Assert.Equal("1.0", json.GetProperty("version").GetString());
-        Assert.Equal(409, json.GetProperty("status").GetInt32());
-    }
-
     [Fact]
     public async Task GivenTrialLoginType_WhenOrganizationDoesNotExist_Then_Return200Ok_And_OrganizationStatusTrial()
     {
