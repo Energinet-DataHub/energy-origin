bf5ec50a410032e4b028fb765ad28b2afe3c4b93
Casper Olsen <cabol@energinet.dk>
2025-06-19 14:12:52 +0200

    Mock data fetched one at a time

--- domains/libraries/EnergyOrigin.Datahub3/EnergyOrigin.Datahub3/MeasurementClient.cs
+++ domains/libraries/EnergyOrigin.Datahub3/EnergyOrigin.Datahub3/MeasurementClient.cs
@@ -2,6 +2,8 @@ using Energinet.DataHub.Measurements.Abstractions.Api.Models;
 using Energinet.DataHub.Measurements.Abstractions.Api.Queries;
 using Energinet.DataHub.Measurements.Client;
 using EnergyOrigin.Domain.ValueObjects;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using NodaTime;
 
 namespace EnergyOrigin.Datahub3;
@@ -11,10 +13,19 @@ public interface IMeasurementClient
     Task<IEnumerable<MeasurementAggregationByPeriodDto>> GetMeasurements(IList<Gsrn> gsrn, long dateFromEpoch, long dateToEpoch, CancellationToken cancellationToken);
 }
 
-public class MeasurementClient(IMeasurementsClient client) : IMeasurementClient
+public class MeasurementClient(IMeasurementsClient client, IOptions<DataHub3Options> options, ILogger<MeasurementClient> logger) : IMeasurementClient
 {
-    public async Task<IEnumerable<MeasurementAggregationByPeriodDto>> GetMeasurements(IList<Gsrn> gsrns, long dateFromEpoch, long dateToEpoch, CancellationToken cancellationToken)
+    public async Task<IEnumerable<MeasurementAggregationByPeriodDto>> GetMeasurements(
+            IList<Gsrn> gsrns,
+            long dateFromEpoch,
+            long dateToEpoch,
+            CancellationToken cancellationToken)
     {
+        if (options.Value.EnableMock)
+        {
+            return await GetAggregatedByPeriodForMockData(gsrns, dateFromEpoch, dateToEpoch, cancellationToken);
+        }
+
         var query = new GetAggregateByPeriodQuery(
             [.. gsrns.Select(x => x.Value)],
             Instant.FromUnixTimeSeconds(dateFromEpoch),
@@ -22,5 +33,34 @@ public class MeasurementClient(IMeasurementsClient client) : IMeasurementClient
             Aggregation.Hour);
 
         return await client.GetAggregatedByPeriodAsync(query, cancellationToken);
+    }
+
+    private async Task<IEnumerable<MeasurementAggregationByPeriodDto>> GetAggregatedByPeriodForMockData(IList<Gsrn> gsrns, long dateFromEpoch, long dateToEpoch, CancellationToken cancellationToken)
+    {
+        var tasks = new List<Task<IEnumerable<MeasurementAggregationByPeriodDto>>>();
+        foreach (var gsrn in gsrns)
+        {
+            var singleGsrnQuery = new GetAggregateByPeriodQuery(
+                [gsrn.Value],
+                Instant.FromUnixTimeSeconds(dateFromEpoch),
+                Instant.FromUnixTimeSeconds(dateToEpoch),
+                Aggregation.Hour);
+
+            tasks.Add(client.GetAggregatedByPeriodAsync(singleGsrnQuery, cancellationToken));
+        }
+
+
+        var responses = await Task.WhenAll(tasks);
+
+        List<MeasurementAggregationByPeriodDto> combinedAggregations = [];
+        foreach (var aggregations in responses)
+        {
+            if (aggregations.Any())
+            {
+                combinedAggregations.Add(aggregations.First());
+            }
+        }
+
+        return combinedAggregations;
     }
 }
