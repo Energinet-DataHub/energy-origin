380060e9ed4d07c666f074787c9d83fd18a4839c
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-30 13:51:32 +0200

    escape dangerous stuff :O

--- domains/admin-portal/AdminPortal.Web/AdminPortal/wwwroot/lib/jquery-validation/dist/jquery.validate.js
+++ domains/admin-portal/AdminPortal.Web/AdminPortal/wwwroot/lib/jquery-validation/dist/jquery.validate.js
@@ -1083,7 +1083,7 @@ $.extend( $.validator, {
 			}
 
 			// Always apply ignore filter
-			return $( element ).not( this.settings.ignore )[ 0 ];
+            return $( this.escapeCssMeta( element ) ).not( this.settings.ignore )[ 0 ];
 		},
 
 		checkable: function( element ) {
