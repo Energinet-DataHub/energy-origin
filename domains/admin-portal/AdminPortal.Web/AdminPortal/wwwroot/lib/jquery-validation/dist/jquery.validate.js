9aa3c49997eed1b907979865072d93d7ee8cae58
sahma19 <55732914+sahma19@users.noreply.github.com>
2025-06-30 13:57:22 +0200

    remove xss dependency

--- domains/admin-portal/AdminPortal.Web/AdminPortal/wwwroot/lib/jquery-validation/dist/jquery.validate.js
+++ domains/admin-portal/AdminPortal.Web/AdminPortal/wwwroot/lib/jquery-validation/dist/jquery.validate.js
@@ -393,9 +393,9 @@ $.extend( $.validator, {
 	prototype: {
 
 		init: function() {
-			this.labelContainer = $( this.settings.errorLabelContainer );
+            this.labelContainer = $( document ).find( this.settings.errorLabelContainer );
 			this.errorContext = this.labelContainer.length && this.labelContainer || $( this.currentForm );
-			this.containers = $( this.settings.errorContainer ).add( this.settings.errorLabelContainer );
+            this.containers = $( document ).find( this.settings.errorContainer ).add( this.settings.errorLabelContainer );
 			this.submitted = {};
 			this.valueCache = {};
 			this.pendingRequest = 0;
