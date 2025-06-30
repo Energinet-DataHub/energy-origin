3e1662f0851d9603c65dc0b71ea80544441c8c84
Christoffer Frydkjær <christofferfk@gmail.com>
2025-06-30 14:52:52 +0200

    align text

--- domains/transfer/ClaimAutomation/Worker/Automation/ClaimWorker.cs
+++ domains/transfer/ClaimAutomation/Worker/Automation/ClaimWorker.cs
@@ -43,7 +43,7 @@ public class ClaimWorker : BackgroundService
             }
             catch (OperationCanceledException)
             {
-                logger.LogWarning("ClaimService was cancelled");
+                logger.LogWarning("ClaimWorker has been cancelled");
             }
             catch (Exception e)
             {
