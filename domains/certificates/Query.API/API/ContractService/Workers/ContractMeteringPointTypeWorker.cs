b07af79612908aa561898ea7d9dea8a699f6bb75
Casper Olsen <cabol@energinet.dk>
2025-07-11 12:36:28 +0200

    Add contract worker with logging

---
+++ domains/certificates/Query.API/API/ContractService/Workers/ContractMeteringPointTypeWorker.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using API.ContractService.Clients;
+using API.UnitOfWork;
+using DataContext.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API.ContractService.Workers;
+
+public class ContractMeteringPointTypeWorker(
+        IUnitOfWork unitOfWork,
+        IMeteringPointsClient meteringPointsClient,
+        ILogger<ContractMeteringPointTypeWorker> logger)
+    : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            var contracts = await unitOfWork
+                .CertificateIssuingContractRepo
+                .Query()
+                .ToListAsync(stoppingToken);
+
+            var distinctMeteringPointOwners = contracts
+                .DistinctBy(x => x.MeteringPointOwner)
+                .Select(x => x.MeteringPointOwner);
+
+            var meteringPoints = new Dictionary<string, List<MeteringPoint>>();
+            foreach (var meteringPointOwner in distinctMeteringPointOwners)
+            {
+                var meteringPointsResponse = await meteringPointsClient.GetMeteringPoints(meteringPointOwner, stoppingToken);
+                if (meteringPointsResponse is not null)
+                {
+                    meteringPoints.Add(meteringPointOwner, meteringPointsResponse.Result);
+                }
+            }
+
+            foreach (var contract in contracts)
+            {
+                if (contract.MeteringPointType == MeteringPointType.Production) continue;
+
+                var meteringPointsForContractOwner = meteringPoints
+                    .FirstOrDefault(x => x.Key == contract.MeteringPointOwner);
+
+                if (meteringPointsForContractOwner.Value.Count == 0)
+                {
+                    logger.LogWarning(
+                            "Contract job: Contracts MeteringPointOwner does not have any metering points, for {MeteringPointOwner}",
+                            contract.MeteringPointOwner);
+
+                    continue;
+                }
+
+                var meteringPoint = meteringPointsForContractOwner.Value.FirstOrDefault(x => x.Gsrn == contract.GSRN);
+                if (meteringPoint is null)
+                {
+
+                    logger.LogWarning("Contract job: No MeteringPoint found for GSRN {GSRN}", contract.GSRN);
+                    continue;
+                }
+
+                if (meteringPoint.MeteringPointType == MeterType.Production || meteringPoint.MeteringPointType == MeterType.Child)
+                {
+
+                    logger.LogWarning(
+                            "Contract job: Contract has MeteringPointType {MeteringPointTypeContract} and MeteringPoint has MeteringPointType {MeteringPointTypeMeteringPoint}",
+                            contract.MeteringPointType.ToString(), meteringPoint.MeteringPointType.ToString());
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Contract job: Something went wrong with the ContractMeteringPointTypeWorker");
+        }
+    }
+}
