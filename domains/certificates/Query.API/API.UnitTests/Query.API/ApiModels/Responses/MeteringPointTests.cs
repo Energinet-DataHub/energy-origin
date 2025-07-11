348bdffbcc105e987c5152e9acf9553f2d0d730c
Casper Olsen <cabol@energinet.dk>
2025-07-11 09:42:07 +0200

    Test that property names don't change

---
+++ domains/certificates/Query.API/API.UnitTests/Query.API/ApiModels/Responses/MeteringPointTests.cs
@@ -0,0 +1,22 @@
+using API.ContractService.Clients;
+using Xunit;
+
+namespace API.UnitTests.Query.API.ApiModels.Responses;
+
+public class MeteringPointTests
+{
+    [Fact]
+    public void MeteringPoint_ResponsePropertyNames_ShouldBeStable()
+    {
+        Assert.Equal("Gsrn", nameof(MeteringPoint.Gsrn));
+        Assert.Equal("GridArea", nameof(MeteringPoint.GridArea));
+        Assert.Equal("Type", nameof(MeteringPoint.Type));
+        Assert.Equal("SubMeterType", nameof(MeteringPoint.SubMeterType));
+        Assert.Equal("Address", nameof(MeteringPoint.Address));
+        Assert.Equal("Technology", nameof(MeteringPoint.Technology));
+        Assert.Equal("ConsumerCvr", nameof(MeteringPoint.ConsumerCvr));
+        Assert.Equal("CanBeUsedForIssuingCertificates", nameof(MeteringPoint.CanBeUsedForIssuingCertificates));
+        Assert.Equal("Capacity", nameof(MeteringPoint.Capacity));
+        Assert.Equal("BiddingZone", nameof(MeteringPoint.BiddingZone));
+    }
+}
