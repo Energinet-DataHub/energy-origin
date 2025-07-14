f545b192d9e54c14f278972cc1c1663bcb81eaf1
tnickelsen <TNE@energinet.dk>
2025-07-14 09:12:57 +0200

    sum

--- domains/transfer/Transfer.API/API/Transfer/Api/[Features]/PopulateReportCommand.cs
+++ domains/transfer/Transfer.API/API/Transfer/Api/[Features]/PopulateReportCommand.cs
@@ -84,8 +84,8 @@ public class PopulateReportCommandHandler
 
             var (totalConsumptionRaw, averageHourConsumptionRaw, claims) = await _dataFetcher.GetAsync(report.OrganizationId, from, to, cancellationToken);
 
-            _logger.LogInformation("totalConsumptionRaw " + totalConsumptionRaw.Sum(x => x.KwhQuantity));
-            _logger.LogInformation("averageHourConsumptionRaw " + averageHourConsumptionRaw.Sum(x => x.KwhQuantity));
+            _logger.LogInformation("totalConsumptionRaw " + totalConsumptionRaw.Sum(x => x.KwhQuantity) * 1000);
+            _logger.LogInformation("averageHourConsumptionRaw " + averageHourConsumptionRaw.Sum(x => x.KwhQuantity) * 1000);
             _logger.LogInformation("claims " + claims.Sum(x => x.Quantity));
 
             var coverage = _coverageProcessor.Calculate(claims, totalConsumptionRaw, from, to);
