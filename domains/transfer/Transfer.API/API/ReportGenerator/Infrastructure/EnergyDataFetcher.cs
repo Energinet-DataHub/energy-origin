f1e78febffdc42230a1e2f066df8daf910c9a713
Casper Olsen <cabol@energinet.dk>
2025-07-23 13:53:52 +0200

    Log claims

--- domains/transfer/Transfer.API/API/ReportGenerator/Infrastructure/EnergyDataFetcher.cs
+++ domains/transfer/Transfer.API/API/ReportGenerator/Infrastructure/EnergyDataFetcher.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.Transfer.Api.Services;
 using EnergyOrigin.Domain.ValueObjects;
 using EnergyOrigin.WalletClient;
+using Microsoft.Extensions.Logging;
 
 namespace API.ReportGenerator.Infrastructure;
 
@@ -18,14 +19,18 @@ public sealed class EnergyDataFetcher : IEnergyDataFetcher
 {
     private readonly IConsumptionService _consumptionService;
     private readonly IWalletClient _walletClient;
+    private readonly ILogger<EnergyDataFetcher> _logger;
 
     public EnergyDataFetcher(
         IConsumptionService consumptionService,
-        IWalletClient walletClient)
+        IWalletClient walletClient,
+        ILogger<EnergyDataFetcher> logger)
     {
         _consumptionService = consumptionService ?? throw new ArgumentNullException(nameof(consumptionService));
         _walletClient = walletClient ?? throw new ArgumentNullException(nameof(walletClient));
+        _logger = logger;
     }
+
 
     public async Task<(List<ConsumptionHour> totalHourConsumption, List<ConsumptionHour> averageHourConsumption, List<Claim> claims)> GetAsync(
         OrganizationId orgId,
@@ -39,11 +44,18 @@ public sealed class EnergyDataFetcher : IEnergyDataFetcher
         var allConsumptionFetchedFromDatahub = _consumptionService.GetTotalAndAverageHourlyConsumption(orgId, from, to, ct);
         var allClamsFetchedFromWallet = _walletClient.GetClaimsAsync(orgId.Value, from, to, TimeMatch.All, ct);
 
+
         await Task.WhenAll(allConsumptionFetchedFromDatahub, allClamsFetchedFromWallet);
 
         var (totalHourConsumption, averageHourConsumption) = await allConsumptionFetchedFromDatahub;
         var allClaims = (await allClamsFetchedFromWallet)?.Result.ToList() ?? new List<Claim>();
 
+        foreach (var claim in allClaims)
+        {
+            _logger.LogInformation(
+                    "Fetched claims: Quantity {Quantity}, ConsumptionStart {StartConsumption}, ProductionStart {StartProduction}",
+                    claim.Quantity, claim.ConsumptionCertificate.Start, claim.ProductionCertificate.Start);
+        }
         var filteredClaims = allClaims.Where(claim => claim.IsTrialClaim() == isTrial).ToList();
 
         return (totalHourConsumption, averageHourConsumption, claims: filteredClaims);
