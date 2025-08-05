61149270cac70e0cf4cc91214423435841c39053
Casper Olsen <cabol@energinet.dk>
2025-08-05 15:14:03 +0200

    fix: Set authorization header when calling measurements

--- domains/certificates/Query.API/API/ContractService/Clients/Internal/AdminPortalMeteringPointsClient.cs
+++ domains/certificates/Query.API/API/ContractService/Clients/Internal/AdminPortalMeteringPointsClient.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -20,6 +21,7 @@ public class AdminPortalMeteringPointsClient(HttpClient httpClient, IHttpContext
     public async Task<MeteringPointsResponse?> GetMeteringPoints(string owner, CancellationToken cancellationToken)
     {
         ValidateHttpContext();
+        SetAuthorizationHeader();
 
         var meteringPointsUrl = $"/api/measurements/admin-portal/internal-meteringpoints?organizationId={owner}";
 
@@ -33,6 +35,12 @@ public class AdminPortalMeteringPointsClient(HttpClient httpClient, IHttpContext
         {
             throw new HttpRequestException($"No HTTP context found. {nameof(MeteringPointsClient)} must be used as part of a request");
         }
+    }
+
+    private void SetAuthorizationHeader()
+    {
+        httpClient.DefaultRequestHeaders.Authorization =
+            AuthenticationHeaderValue.Parse(httpContextAccessor.HttpContext!.Request.Headers.Authorization!);
     }
 }
 
