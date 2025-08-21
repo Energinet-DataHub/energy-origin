f358963cf8a0af7fce93bb8a4b4634336a55695a
Mathias Sejrup Plougmann <masep@energinet.dk>
2025-08-21 14:33:56 +0200

    chore: corrects font

--- domains/transfer/Transfer.API/API/ReportGenerator/Rendering/HeadlinePercentageRenderer.cs
+++ domains/transfer/Transfer.API/API/ReportGenerator/Rendering/HeadlinePercentageRenderer.cs
@@ -21,7 +21,7 @@ public class HeadlinePercentageRenderer : IHeadlinePercentageRenderer
                                 <span style="color: #002433; font-size: 12px; font-family: OpenSansNormal; font-weight: 400; text-transform: uppercase; letter-spacing: 0.60px; word-wrap: break-word">For perioden<br/></span>
                                 <span style="color: #002433; font-size: 12px; font-family: OpenSansNormal; font-weight: 800; text-transform: uppercase; letter-spacing: 0.60px; word-wrap: break-word">{periodLabel}</span>
                             </div>
-                            <div data-layer="{pct}%" style="width: 117px; height: 66px; left: 16px; top: 28px; position: absolute; color: #002433; font-size: 44px; font-family: OpenSansBold; font-weight: 800; word-wrap: break-word">{pct}%</div>
+                            <div data-layer="{pct}%" style="width: 117px; height: 66px; left: 16px; top: 28px; position: absolute; color: #002433; font-size: 44px; font-family: OpenSansBolder; font-weight: 800; word-wrap: break-word">{pct}%</div>
                             <div data-layer="Timedækning" class="TimedKning" style="width: 209.87px; left: 16px; top: 19px; position: absolute; color: #002433; font-size: 12px; font-family: OpenSansNormal; font-weight: 400; text-transform: uppercase; letter-spacing: 0.60px; word-wrap: break-word">Timedækning</div>
                         </div>
                     """;
