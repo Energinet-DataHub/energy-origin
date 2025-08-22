f7f2daa43082c4bba21a88102177c8eb6e5315db
cfk <cfk@energinet.dk>
2025-08-22 11:11:30 +0200

    more logging

--- domains/admin-portal/AdminPortal.Web/AdminPortal/Services/MeasurementsService.cs
+++ domains/admin-portal/AdminPortal.Web/AdminPortal/Services/MeasurementsService.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using AdminPortal.Dtos.Response;
+using Microsoft.Extensions.Logging;
+
 namespace AdminPortal.Services;
 
 public interface IMeasurementsService
@@ -10,12 +12,13 @@ public interface IMeasurementsService
     Task<GetMeteringpointsResponse> GetMeteringPointsHttpRequestAsync(Guid organizationId);
 }
 
-public class MeasurementsService(HttpClient client) : IMeasurementsService
+public class MeasurementsService(HttpClient client, ILogger<MeasurementsService> logger) : IMeasurementsService
 {
     public async Task<GetMeteringpointsResponse> GetMeteringPointsHttpRequestAsync(Guid organizationId)
     {
         var response = await client.GetAsync($"internal-meteringpoints?organizationId={organizationId}");
         response.EnsureSuccessStatusCode();
+        logger.LogInformation("CLIENT; " + await response.Content.ReadAsStringAsync());
         var result = await response.Content.ReadFromJsonAsync<GetMeteringpointsResponse>();
         return result ?? throw new InvalidOperationException("The API could not be reached or returned null.");
     }
