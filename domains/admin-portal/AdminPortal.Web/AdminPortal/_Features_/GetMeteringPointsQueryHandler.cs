6cfb6822e173db25e5a98a8a14725ce7af62c1f0
Christoffer Frydkjær <christofferfk@gmail.com>
2025-08-25 10:02:07 +0200

    remove logging

--- domains/admin-portal/AdminPortal.Web/AdminPortal/_Features_/GetMeteringPointsQueryHandler.cs
+++ domains/admin-portal/AdminPortal.Web/AdminPortal/_Features_/GetMeteringPointsQueryHandler.cs
@@ -100,7 +100,6 @@ public class GetMeteringPointsQueryHandler(
                     }
                 )
                 .ToList();
-            logger.LogInformation("QUERY: " + System.Text.Json.JsonSerializer.Serialize(result));
             return new GetMeteringPointsQueryResult(new GetMeteringPointsQueryResultViewModel(selectedOrganization.OrganizationId, selectedOrganization.OrganizationName, selectedOrganization.Tin, result));
 
         }
